5623e07ed5a0a83247ebbe8b6e1476fae83c1832
Dmitry Ivanov <karward94@gmail.com>
2022-03-17 20:07:24 +0200

    Optimization to Python 3.8

--- services/movie.py
+++ services/movie.py
@@ -1,7 +1,9 @@
+from typing import List
+
 from db.models import Movie
 
 
-def get_movies(genres_ids: list[int] = None, actors_ids: list[int] = None):
+def get_movies(genres_ids: List[int] = None, actors_ids: List[int] = None):
     queryset = Movie.objects.all()
     if genres_ids is not None:
         queryset = queryset.filter(genres__id__in=genres_ids)
@@ -15,7 +17,7 @@ def get_movie_by_id(movie_id: int):
 
 
 def create_movie(movie_title: str, movie_description: str,
-                 genres_ids: list[int] = None, actors_ids: list[int] = None):
+                 genres_ids: List[int] = None, actors_ids: List[int] = None):
     add_movie = Movie.objects.create(title=movie_title,
                                      description=movie_description)
     if genres_ids:
