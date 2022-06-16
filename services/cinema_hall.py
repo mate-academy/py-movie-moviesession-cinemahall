0adc5592ae3b611240ffef9e6a37ce5101bf6c0e
Denis <sairustv228@gmail.com>
2022-06-16 13:02:33 +0300

    v1.1.2

--- services/cinema_hall.py
+++ services/cinema_hall.py
@@ -5,9 +5,10 @@ def get_cinema_halls():
     return CinemaHall.objects.all()
 
 
-def create_cinema_hall(hall_name: str,
-                       hall_rows: int,
-                       hall_seats_in_row: int
+def create_cinema_hall(
+        hall_name: str,
+        hall_rows: int,
+        hall_seats_in_row: int
                        ):
     return CinemaHall.objects.create(
         name=hall_name,
--- services/movie.py
+++ services/movie.py
@@ -1,38 +1,33 @@
 from db.models import Movie
 
 
-def get_movies(genres_ids: list[int] = None,
-               actors_ids: list[int] = None
-               ):
+def get_movies(genres_ids=None, actors_ids=None):
     queryset = Movie.objects.all()
 
     if genres_ids is not None:
         queryset = queryset.filter(genres__id__in=genres_ids)
 
-    if genres_ids is not None:
+    if actors_ids is not None:
         queryset = queryset.filter(actors__id__in=actors_ids)
 
     return queryset
 
 
-def get_movie_by_id(movie_id: int):
+def get_movie_by_id(movie_id):
     return Movie.objects.get(id=movie_id)
 
 
-def create_movie(movie_title: str,
-                 movie_description: str,
-                 genres_ids: list[int] = None,
-                 actors_ids: list[int] = None
-                 ):
+def create_movie(movie_title, movie_description, genres_ids=None,
+                 actors_ids=None):
     new_movie = Movie.objects.create(
         title=movie_title,
         description=movie_description,
     )
 
-    if genres_ids:
+    if genres_ids is not None:
         new_movie.genres.set(genres_ids)
 
-    if actors_ids:
+    if actors_ids is not None:
         new_movie.actors.set(actors_ids)
 
     return new_movie
