47c3234178eef6ba9ea2c4ad852bccda339c2f41
Flashmobber <ufinaid@gmail.com>
2022-06-21 22:24:44 +0300

    f-strings removed

--- db/models.py
+++ db/models.py
@@ -23,7 +23,7 @@ class Movie(models.Model):
     genres = models.ManyToManyField(Genre, related_name="movies")
 
     def __str__(self):
-        return f"{self.title}"
+        return self.title
 
 
 class CinemaHall(models.Model):
@@ -32,7 +32,7 @@ class CinemaHall(models.Model):
     seats_in_row = models.IntegerField()
 
     def __str__(self):
-        return f"{self.name}"
+        return self.name
 
     def capacity(self):
         return self.rows * self.seats_in_row
