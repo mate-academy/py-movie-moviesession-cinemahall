0ab51e6c3ce4f58b0127979e64816e6fc1f77465
Serhiy Zhyhadlo <sergeyz.dev1996@gmail.com>
2022-06-21 16:08:24 +0300

    'Solution

---
+++ db/services/movies_session.py
@@ -0,0 +1,32 @@
+from db.models import MovieSession
+
+
+def create_movie_session(movie_show_time, movie_id, cinema_hall_id):
+    MovieSession.objects.create(show_time=movie_show_time, movie_id=movie_id, cinema_hall_id=cinema_hall_id)
+
+
+def get_movies_sessions(session_date=None):
+    sessions = MovieSession.objects.all()
+    if session_date:
+        sessions.filter(show_time=session_date)
+    return sessions
+
+
+def get_movie_session_by_id(movie_session_id):
+    movie = MovieSession.objects.filter(id=movie_session_id).all()
+    return movie
+
+
+def update_movie_session(session_id, show_time=None, movie_id=None, cinema_hall_id=None):
+    session = MovieSession.objects.filter(id=session_id)
+    if show_time:
+        session.update(show_time=show_time)
+
+    if movie_id:
+        session.update(movie_id=movie_id)
+
+    if cinema_hall_id:
+        session.update(cinema_hall_id=cinema_hall_id)
+
+def delete_movie_session_by_id(session_id):
+    MovieSession.objects.filter(id = session_id).delete()
---
+++ main.py
@@ -0,0 +1,13 @@
+from db.models import Genre, Actor, Movie, MovieSession, CinemaHall
+
+
+
+
+def create():
+    CinemaHall.objects.create(name="Blue", rows=10, seats_in_row=12)
+    CinemaHall.objects.create(name="VIP", rows=3, seats_in_row=5)
+    Movie.objects.create(title="Matrix", description="Matrix description")
+    Movie.objects.create(title="Batman", description="Batman description")
+
+
+create()
