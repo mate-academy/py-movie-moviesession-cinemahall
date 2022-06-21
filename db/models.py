5022e8967806871989dc2d4e13ae4d609cdb987e
Daria <darizhur.vol2@gmail.com>
2022-06-21 19:16:05 +0300

    'NewSolution'

--- db/models.py
+++ db/models.py
@@ -29,7 +29,7 @@ class Movie(models.Model):
 class CinemaHall(models.Model):
     name = models.CharField(max_length=255)
     rows = models.IntegerField()
-    seats_in_row= models.IntegerField()
+    seats_in_row = models.IntegerField()
 
     def __str__(self):
         return self.name
