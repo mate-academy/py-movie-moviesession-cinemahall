b7e499faedb02d7ee24ec893277b794ede6ed21c
Roma Tsybulia <Romalonstejn@gmail.com>
2022-06-22 08:45:18 +0300

    Indentation was refactored

--- db/models.py
+++ db/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+
+
 class Genre(models.Model):
     name = models.CharField(max_length=255, unique=True)
+
     def __str__(self):
         return self.name
+
+
 class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
