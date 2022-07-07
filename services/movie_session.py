d384c5537264ca90bef407eac64d70137281bc7d
Oleg Fadeev <drivemax26@gmail.com>
2022-07-07 15:38:43 +0300

    Solution

---
+++ services/movie_session.py
@@ -0,0 +1,44 @@
+from db.models import MovieSession
+
+
+def create_movie_session(movie_show_time, movie_id, cinema_hall_id):
+    return MovieSession.objects.create(
+        show_time=movie_show_time,
+        cinema_hall_id=cinema_hall_id,
+        movie_id=movie_id
+    )
+
+
+def get_movies_sessions(session_date=None):
+    queryset = MovieSession.objects.all()
+
+    if session_date is not None:
+        queryset = queryset.filter(show_time__date=session_date)
+
+    return queryset
+
+
+def get_movie_session_by_id(movie_session_id):
+
+    return MovieSession.objects.get(id=movie_session_id)
+
+
+def update_movie_session(
+        session_id,
+        show_time=None,
+        movie_id=None,
+        cinema_hall_id=None
+):
+    queryset = MovieSession.objects.filter(id=session_id)
+    if show_time:
+        queryset.update(show_time=show_time)
+
+    if movie_id:
+        queryset.update(movie_id=movie_id)
+
+    if cinema_hall_id:
+        queryset.update(cinema_hall_id=cinema_hall_id)
+
+
+def delete_movie_session_by_id(session_id):
+    MovieSession.objects.filter(id=session_id).delete()
