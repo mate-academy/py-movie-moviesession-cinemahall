48a58761e0b29cf59661b5388bbe910cd47da26e
Roman <coderc770@gmail.com>
2022-08-09 12:02:11 +0300

    fixed

--- services/movie_session.py
+++ services/movie_session.py
@@ -16,24 +16,24 @@ def get_movies_sessions(session_date=None):
 
 
 def get_movie_session_by_id(movie_session_id):
-    return MovieSession.objects.all().get(id=movie_session_id)
+    if MovieSession.objects.all().filter(id=movie_session_id).exists():
+        return MovieSession.objects.all().get(id=movie_session_id)
 
 
 def update_movie_session(session_id,
                          show_time=None,
                          movie_id=None,
                          cinema_hall_id=None):
-    if MovieSession.objects.all().filter(id=session_id).exists():
-        updating = MovieSession.objects.all().filter(id=session_id)
-        if show_time is not None:
-            updating.update(
-                show_time=show_time)
+    updating = MovieSession.objects.all().filter(id=session_id)
+    if show_time is not None:
+        updating.update(
+            show_time=show_time)
 
-        if movie_id is not None:
-            updating.update(movie_id=movie_id)
-        if cinema_hall_id is not None:
-            updating.update(
-                cinema_hall_id=cinema_hall_id)
+    if movie_id is not None:
+        updating.update(movie_id=movie_id)
+    if cinema_hall_id is not None:
+        updating.update(
+            cinema_hall_id=cinema_hall_id)
 
 
 def delete_movie_session_by_id(session_id):
