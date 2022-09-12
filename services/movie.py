bae39ee00f83cbf0084f11ae3cbd671aae6e8014
Anastasia Martyniuk <anastasia.20.12.1995@gmail.com>
2022-09-12 15:27:31 +0300

    Solution

--- services/movie.py
+++ services/movie.py
@@ -3,15 +3,11 @@ from db.models import Movie
 
 def get_movies(genres_ids: list[int] = None, actors_ids: list[int] = None):
     query_set = Movie.objects.all()
-    if genres_ids is None and actors_ids is None:
-        return query_set
-    if genres_ids and actors_ids:
-        return query_set.filter(
-            genres__id__in=genres_ids,
-            actors__id__in=actors_ids)
-    if genres_ids is not None and actors_ids is None:
-        return query_set.filter(genres__id__in=genres_ids)
-    return query_set.filter(actors__id__in=actors_ids)
+    if genres_ids:
+        query_set = query_set.filter(genres__id__in=genres_ids)
+    if actors_ids:
+        query_set = query_set.filter(actors__id__in=actors_ids)
+    return query_set
 
 
 def get_movie_by_id(movie_id: int):
