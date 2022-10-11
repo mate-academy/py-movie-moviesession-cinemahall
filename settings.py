406147f9d739880cbdb9d15f185af9555f49410b
mltdll <ilya.vyplavin@gmail.com>
2022-10-11 23:49:03 +0300

    Solution

--- settings.py
+++ settings.py
@@ -21,6 +21,7 @@ TIME_ZONE = "Europe/Kiev"
 
 USE_I18N = True
 
-USE_TZ = True
+# Tests seem to use naive datetime.
+USE_TZ = False
 
-INSTALLED_APPS = ("db",)
+INSTALLED_APPS = ("db", "services", )
