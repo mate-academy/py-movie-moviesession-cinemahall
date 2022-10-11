6fe60941be2befe0958eac90b622540ff3c15513
Davyd Kryvko <davidkrivko@gmail.com>
2022-10-11 13:33:18 +0200

    'Solution'

--- db/models.py
+++ db/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -14,3 +15,25 @@ class Actor(models.Model):
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    actors = models.ManyToManyField(Actor, related_name="movies")
+    genres = models.ManyToManyField(Genre, related_name="movies")
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    def capacity(self):
+        return self.rows * self.seats_in_row
+
+
+class MovieSession(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.SET_NULL)
+    cinema_hall = models.ForeignKey(CinemaHall, on_delete=models.CASCADE)
+    show_time = models.DateTimeField()
