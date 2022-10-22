5a806dd623bd51723f4301d7412b3749f751f65c
Vadym Shkarbul <vadim.shkarbul@gmail.com>
2022-10-22 18:53:41 +0300

    Fixed flake8

--- services/movie.py
+++ services/movie.py
@@ -16,7 +16,7 @@ def get_movies(genres_ids: list = None,
     return queryset
 
 
-def get_movie_by_id(movie_id) -> Movie:
+def get_movie_by_id(movie_id: int) -> Movie:
     return Movie.objects.get(id=movie_id)
 
 
