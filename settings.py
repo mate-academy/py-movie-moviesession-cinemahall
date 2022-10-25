13052b94c2fde52df121847f7e6190ce55cbc520
Yevhenii Davydenko <akazhulich@gmail.com>
2022-10-25 23:14:02 +0300

    Solution

--- settings.py
+++ settings.py
@@ -17,10 +17,10 @@ DATABASES = {
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "Europe/Kiev"
+#TIME_ZONE = "Europe/Kiev"
 
-USE_I18N = True
+#USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 INSTALLED_APPS = ("db",)
