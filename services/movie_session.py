72218bea77caad01c7d059d58fec1317b7848658
Yaroslav Stetsko <yar.stetsko@gmail.com>
2022-10-25 12:02:17 +0300

    Fix for return annotations and create_create_movie

--- services/movie_session.py
+++ services/movie_session.py
@@ -10,12 +10,12 @@ def create_movie_session(
 ) -> None:
     MovieSession.objects.create(
         show_time=movie_show_time,
-        movie=Movie.objects.get(id=movie_id),
-        cinema_hall=CinemaHall.objects.get(id=cinema_hall_id)
+        movie_id=movie_id,
+        cinema_hall_id=cinema_hall_id
     )
 
 
-def get_movies_sessions(session_date: DateTimeField = None) -> MovieSession:
+def get_movies_sessions(session_date: DateTimeField = None) -> QuerySet:
     queryset = MovieSession.objects.all()
 
     if session_date:
@@ -24,7 +24,7 @@ def get_movies_sessions(session_date: DateTimeField = None) -> MovieSession:
     return queryset
 
 
-def get_movie_session_by_id(movie_session_id: int) -> QuerySet:
+def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
     return MovieSession.objects.get(id=movie_session_id)
 
 
