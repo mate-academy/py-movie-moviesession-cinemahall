d81e606eda4493234ee77abaabb3c2931bd45319
Yaroslav Stetsko <yar.stetsko@gmail.com>
2022-10-25 11:44:21 +0300

    Fix for return annotation in movie.py

--- services/cinema_hall.py
+++ services/cinema_hall.py
@@ -11,7 +11,7 @@ def create_cinema_hall(
         hall_name: str,
         hall_rows: str,
         hall_seats_in_row: int
-) -> QuerySet:
+) -> CinemaHall:
     new_hall = CinemaHall.objects.create(
         name=hall_name,
         rows=hall_rows,
