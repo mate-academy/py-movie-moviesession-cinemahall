ba1f87042205cf4079c314a5be655d94c0da88bb
Alexander <alexzubkoff123@gmail.com>
2022-10-26 16:23:43 +0300

    Refactored code for optimizing DB requests

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,17 +1,15 @@
 from datetime import datetime
 from django.db.models import QuerySet
-from db.models import Movie, CinemaHall, MovieSession
+from db.models import MovieSession
 
 
 def create_movie_session(movie_show_time: datetime,
                          movie_id: int,
                          cinema_hall_id: int) -> MovieSession:
-    cinema_hall_id = CinemaHall.objects.get(id=cinema_hall_id)
-    movie_id = Movie.objects.get(id=movie_id)
     movie_session = MovieSession.objects.create(
         show_time=movie_show_time,
-        cinema_hall=cinema_hall_id,
-        movie=movie_id
+        cinema_hall_id=cinema_hall_id,
+        movie_id=movie_id
     )
     return movie_session
 
@@ -19,7 +17,7 @@ def create_movie_session(movie_show_time: datetime,
 def get_movies_sessions(session_date: str = None) -> QuerySet:
     query_set = MovieSession.objects.all()
     if session_date:
-        query_set = MovieSession.objects.filter(show_time__date=session_date)
+        query_set = query_set.filter(show_time__date=session_date)
     return query_set
 
 
@@ -35,12 +33,10 @@ def update_movie_session(session_id: int, show_time: str = None,
         movie_session.show_time = show_time
 
     if movie_id:
-        movie = Movie.objects.get(id=movie_id)
-        movie_session.movie = movie
+        movie_session.movie_id = movie_id
 
     if cinema_hall_id:
-        cinema_hall_id = CinemaHall.objects.get(id=cinema_hall_id)
-        movie_session.cinema_hall = cinema_hall_id
+        movie_session.cinema_hall_id = cinema_hall_id
     movie_session.save()
     return movie_session
 
