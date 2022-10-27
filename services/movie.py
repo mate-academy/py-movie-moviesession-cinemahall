020159693ca31a7d464c8c3554b6ecc30181faf6
graandda <graanddadays@gmail.com>
2022-10-27 23:20:18 +0300

    solution - with related_name, and reformat

--- services/movie.py
+++ services/movie.py
@@ -3,8 +3,9 @@ from db.models import Movie
 from django.db.models import QuerySet
 
 
-def get_movies(genres_ids: list[int] = None,
-               actors_ids: list[int] = None) -> QuerySet:
+def get_movies(
+    genres_ids: list[int] = None, actors_ids: list[int] = None
+) -> QuerySet[Movie]:
     queryset = Movie.objects.all()
     if genres_ids:
         queryset = queryset.filter(genres__id__in=genres_ids)
@@ -18,10 +19,12 @@ def get_movie_by_id(movie_id: int) -> Movie:
     return Movie.objects.get(id=movie_id)
 
 
-def create_movie(movie_title: str,
-                 movie_description: str,
-                 genres_ids: list[int] = None,
-                 actors_ids: list[int] = None) -> QuerySet:
+def create_movie(
+    movie_title: str,
+    movie_description: str,
+    genres_ids: list[int] = None,
+    actors_ids: list[int] = None,
+) -> Movie:
     new_movie = Movie.objects.create(
         title=movie_title,
         description=movie_description,
