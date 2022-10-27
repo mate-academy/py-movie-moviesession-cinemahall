aaada543de359ec9f5ec03aa2ca01af35189a63e
Katya Vasylieva <katevvasylyeva@gmail.com>
2022-10-27 13:02:30 +0300

    Solution

---
+++ services/movie.py
@@ -0,0 +1,29 @@
+from db.models import Movie
+
+
+def get_movies(genres_ids: list[int] = None, actors_ids: list[int] = None):
+    query_set = Movie.objects.all()
+
+    if genres_ids:
+        query_set = query_set.filter(genres__id__in=genres_ids)
+
+    if actors_ids:
+        query_set = query_set.filter(actors__id__in=actors_ids)
+
+    return query_set
+
+
+def get_movie_by_id(movie_id: int) -> str:
+    return Movie.objects.get(id=movie_id)
+
+
+def create_movie(movie_title: str, movie_description: str,
+                 genres_ids: list[int] = None,
+                 actors_ids: list[int] = None):
+    new_movie = Movie.objects.create(title=movie_title, description=movie_description)
+
+    if genres_ids:
+        new_movie.genres.set(genres_ids)
+
+    if actors_ids:
+        new_movie.actors.set(actors_ids)
