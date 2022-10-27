ee43b8733f80d6924a74558766f2767e3f61b9bc
Dima Ovcharenko <kukuku381@gmail.com>
2022-10-27 18:27:32 +0300

    Solution

--- services/movie.py
+++ services/movie.py
@@ -18,14 +18,14 @@ def get_movies(genres_ids: List[int] = None,
     return queryset
 
 
-def get_movie_by_id(movie_id: int) -> QuerySet:
+def get_movie_by_id(movie_id: int) -> Movie:
     return Movie.objects.get(id=movie_id)
 
 
 def create_movie(movie_title: str,
                  movie_description: str,
                  genres_ids: List[int] = None,
-                 actors_ids: List[int] = None) -> QuerySet:
+                 actors_ids: List[int] = None) -> Movie:
     new_movie = Movie.objects.create(
         title=movie_title,
         description=movie_description)
--- services/movie_session.py
+++ services/movie_session.py
@@ -8,7 +8,7 @@ from db.models import MovieSession, CinemaHall, Movie
 def create_movie_session(movie_show_time: datetime,
                          movie_id: int,
                          cinema_hall_id: int) -> None:
-    MovieSession.objects.create(
+    return MovieSession.objects.create(
         show_time=movie_show_time,
         cinema_hall=CinemaHall.objects.get(id=cinema_hall_id),
         movie=Movie.objects.get(id=movie_id))
@@ -27,7 +27,7 @@ def get_movie_session_by_id(movie_session_id: int) -> QuerySet:
 
 
 def update_movie_session(session_id: int,
-                         show_time: datetime = None,
+                         show_time: str = None,
                          movie_id: int = None,
                          cinema_hall_id: int = None) -> None:
     queryset = MovieSession.objects.get(id=session_id)
