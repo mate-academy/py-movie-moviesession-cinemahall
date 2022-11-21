13fb53a6423035a78e02ee501814e631abf5d1fc
Nataliia <amirammina@gmail.com>
2022-11-21 15:41:05 +0200

    Fixed tests errors

--- services/movie_session.py
+++ services/movie_session.py
@@ -10,8 +10,8 @@ def create_movie_session(
 ) -> MovieSession:
     return MovieSession.objects.create(
         show_time=movie_show_time,
-        cinema_hall=cinema_hall_id,
-        movie=movie_id
+        cinema_hall_id=cinema_hall_id,
+        movie_id=movie_id
     )
 
 
