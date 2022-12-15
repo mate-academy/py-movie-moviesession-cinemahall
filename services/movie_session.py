1ac536ef9312bb05a90dbbd70a4ce53c9976c215
Taras Zelinskyi <taras.sv89@gmail.com>
2022-12-15 10:31:27 +0100

    fixed

--- services/movie_session.py
+++ services/movie_session.py
@@ -32,13 +32,11 @@ def update_movie_session(session_id: int,
     movie_session = MovieSession.objects.get(id=session_id)
     if show_time is not None:
         movie_session.show_time = show_time
-        movie_session.save()
     if movie_id is not None:
         movie_session.movie_id = movie_id
-        movie_session.save()
     if cinema_hall_id is not None:
         movie_session.cinema_hall_id = cinema_hall_id
-        movie_session.save()
+    movie_session.save()
 
 
 def delete_movie_session_by_id(session_id: int) -> MovieSession:
