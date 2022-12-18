e4c23ab5f3107f3b0f4d42555cd275f1501c1903
vani@linux <winfkill@gmail.com>
2022-12-18 16:28:10 +0200

    Solution

---
+++ services/movie.py
@@ -0,0 +1,35 @@
+from db.models import Movie
+
+
+def get_movies(genres_ids: list = None, actors_ids: list = None) -> Movie:
+    all_movie = Movie.objects.all()
+    if genres_ids is None and actors_ids is None:
+        return all_movie
+    if genres_ids is not None and actors_ids is None:
+        return all_movie.filter(genres__id__in=genres_ids)
+    if actors_ids is not None and genres_ids is None:
+        return all_movie.filter(actors__id__in=actors_ids)
+    if genres_ids is not None and actors_ids is not None:
+        return all_movie.filter(genres__id__in=genres_ids).filter(
+            actors__id__in=actors_ids
+        )
+
+
+def get_movie_by_id(movie_id: int) -> Movie:
+    return Movie.objects.get(id=movie_id)
+
+
+def create_movie(
+        movie_title: str,
+        movie_description: str,
+        genres_ids: list = None,
+        actors_ids: list = None) -> None:
+    new_movie = Movie.objects.create(
+        title=movie_title,
+        description=movie_description,
+    )
+    if genres_ids:
+        new_movie.genres.set(genres_ids)
+
+    if actors_ids:
+        new_movie.actors.set(actors_ids)
---
+++ test.py
@@ -0,0 +1,5 @@
+import datetime
+
+show_time = datetime.datetime(year=2021, month=11, day=29, hour=16, minute=40)
+
+print(show_time)
