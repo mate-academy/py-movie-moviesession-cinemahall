c858403a54bfd30ce35eb5d9facc582e024e8265
Andrew.H <john.riderr9@gmail.com>
2023-01-05 16:17:31 +0200

    flake8 fix

--- services/movie_session.py
+++ services/movie_session.py
@@ -6,21 +6,24 @@ from django.db.models.query import QuerySet
 
 # from datetime import time, date
 
-def create_movie_session(movie_show_time: datetime.time,
-                         movie_id: int,
-                         cinema_hall_id: int) -> None:
-    MovieSession.objects.create(show_time=movie_show_time,
-                                movie_id=movie_id,
-                                cinema_hall_id=cinema_hall_id)
+
+def create_movie_session(
+    movie_show_time: datetime.time, movie_id: int, cinema_hall_id: int
+) -> None:
+    MovieSession.objects.create(
+        show_time=movie_show_time,
+        movie_id=movie_id,
+        cinema_hall_id=cinema_hall_id,
+    )
 
 
 def get_movies_sessions(session_date: str = None) -> QuerySet:
     queryset = MovieSession.objects.all()
     if session_date:
         year, month, day = map(int, session_date.split("-"))
-        queryset = queryset.filter(show_time__year=year,
-                                   show_time__month=month,
-                                   show_time__day=day)
+        queryset = queryset.filter(
+            show_time__year=year, show_time__month=month, show_time__day=day
+        )
     return queryset
 
 
@@ -28,8 +31,12 @@ def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
     return MovieSession.objects.get(id=movie_session_id)
 
 
-def update_movie_session(session_id: int, show_time: datetime.time = None,
-                         movie_id: int = None, cinema_hall_id: int = None) -> None:
+def update_movie_session(
+    session_id: int,
+    show_time: datetime.time = None,
+    movie_id: int = None,
+    cinema_hall_id: int = None,
+) -> None:
     params = {
         "show_time": show_time,
         "movie_id": movie_id,
@@ -39,5 +46,5 @@ def update_movie_session(session_id: int, show_time: datetime.time = None,
     MovieSession.objects.filter(id=session_id).update(**not_none_params)
 
 
-def delete_movie_session_by_id(session_id: int):
+def delete_movie_session_by_id(session_id: int) -> None:
     MovieSession.objects.filter(id=session_id).delete()
