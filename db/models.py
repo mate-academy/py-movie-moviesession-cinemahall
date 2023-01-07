f5dec56263918e3078e7820c56651713a4f96679
Ivan Mayor <maramorosh2@gmail.com>
2023-01-07 16:13:40 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 
 
-import datetime
-
-
 class Genre(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
@@ -22,8 +19,11 @@ class Actor(models.Model):
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    actors = models.ManyToManyField(Actor)
-    genre = models.ManyToManyField(Genre)
+    actors = models.ManyToManyField(Actor, related_name="movies")
+    genres = models.ManyToManyField(Genre, related_name="movies")
+
+    def __str__(self) -> str:
+        return self.title
 
 
 class CinemaHall(models.Model):
@@ -31,15 +31,27 @@ class CinemaHall(models.Model):
     rows = models.IntegerField()
     seats_in_row = models.IntegerField()
 
-    def capacity(self):
+    @property
+    def capacity(self) -> int:
         return self.rows * self.seats_in_row
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class MovieSession(models.Model):
     show_time = models.DateTimeField()
     cinema_hall = models.ForeignKey(
         CinemaHall,
-        on_delete=models.SET_NULL,
-        null=True
+        on_delete=models.CASCADE,
+        null=True,
+        related_name="movie_session"
     )
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    movie = models.ForeignKey(
+        Movie,
+        on_delete=models.CASCADE,
+        related_name="movie_session"
+    )
+
+    def __str__(self) -> str:
+        return f"{self.movie.title} {self.show_time}"
--- db/services/movie.py
+++
@@ -1,2 +0,0 @@
-def get_movies(genre_ids, actor_ids):
-    pass
