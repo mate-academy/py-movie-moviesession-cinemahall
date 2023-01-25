0c4c32897fce36a7ce31ed76ede09d3ae5b11a75
Roman Pisarskiy <darkvader.ukr.net@gmail.com>
2023-01-25 14:03:28 +0200

    after refactoring in models.py and set USE_TZ=True - coz i have a error: "ValueError: Problem installing fixture '/Users/soobig666/Documents/Projects/py-movie-moviesession-cinemahall/cinema_db_data.json': Could not load db.MovieSession(pk=1): SQLite backend does not support timezone-aware datetimes when USE_TZ is False."

--- db/models.py
+++ db/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from typing import Any
 
 
 class Genre(models.Model):
@@ -35,14 +34,16 @@ class CinemaHall(models.Model):
     def __str__(self) -> str:
         return self.name
 
+    @property
     def capacity(self) -> int:
-        return self.seats_in_row
+        return self.rows * self.seats_in_row
 
 
 class MovieSession(models.Model):
-    show_time = models.DateTimeField(auto_now_add=True)
+    show_time = models.DateTimeField()
     cinema_hall = models.ForeignKey(CinemaHall, on_delete=models.CASCADE)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
 
-    def __str__(self) -> Any:
-        return self.show_time
+    def __str__(self) -> str:
+        return f"{self.movie.title} " \
+               f"{self.show_time.strftime('%Y-%m-%d %H:%M:%S')}"
