03b815c8b44059776d1f78d4d9d7f21dd9c61aa1
Artur Leleiko <artur_98@ukr.net>
2023-02-03 12:37:52 +0200

    'Solution'

---
+++ services/movie_session.py
@@ -0,0 +1,69 @@
+import datetime
+
+from django.db.models import QuerySet
+
+from db.models import MovieSession, Movie, CinemaHall
+
+
+def create_movie_session(
+        movie_show_time: datetime,
+        movie_id: int,
+        cinema_hall_id: int
+) -> None:
+    movie = Movie.objects.get(id=movie_id)
+    cinema_hall = CinemaHall.objects.get(
+        id=cinema_hall_id
+    )
+
+    MovieSession.objects.create(
+        show_time=movie_show_time,
+        movie=movie,
+        cinema_hall=cinema_hall
+    )
+
+
+def get_movies_sessions(
+        session_date: str = None
+) -> QuerySet:
+    queryset = MovieSession.objects.all()
+
+    if session_date is not None:
+        queryset = queryset.filter(
+            show_time__date=session_date
+        )
+
+    return queryset
+
+
+def get_movie_session_by_id(
+        movie_session_id: int
+) -> MovieSession:
+    return MovieSession.objects.get(
+        id=movie_session_id
+    )
+
+
+def update_movie_session(
+        session_id: int,
+        show_time: str = None,
+        movie_id: int = None,
+        cinema_hall_id: int = None
+) -> None:
+    updated_movie_session = get_movie_session_by_id(
+        session_id
+    )
+
+    if show_time is not None:
+        updated_movie_session.show_time = show_time
+
+    if movie_id is not None:
+        updated_movie_session.movie_id = movie_id
+
+    if cinema_hall_id is not None:
+        updated_movie_session.cinema_hall_id = cinema_hall_id
+
+    updated_movie_session.save()
+
+
+def delete_movie_session_by_id(session_id: int) -> None:
+    MovieSession.objects.filter(id=session_id).delete()
