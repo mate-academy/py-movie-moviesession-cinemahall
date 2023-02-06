de4246fe2ed3e12e201e7d85bb94cc69ba4a1baf
yura.vataschuk <yura.vataschuk@gmail.com>
2023-02-06 22:52:43 +0200

    develop

--- services/movie.py
+++ services/movie.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import init_django_orm  # noqa: F401
 from django.db.models.query import QuerySet
 
@@ -5,8 +7,8 @@ from db.models import Movie
 
 
 def get_movies(
-        genres_ids: list[int] = None,
-        actors_ids: list[int] = None
+        genres_ids: Optional[list[int]] = None,
+        actors_ids: Optional[list[int]] = None
 ) -> QuerySet:
     queryset = Movie.objects.all()
     if genres_ids:
@@ -24,8 +26,8 @@ def get_movie_by_id(movie_id: int) -> Movie:
 def create_movie(
         movie_title: str,
         movie_description: str,
-        genres_ids: list[int] = None,
-        actors_ids: list[int] = None
+        genres_ids: Optional[list[int]] = None,
+        actors_ids: Optional[list[int]] = None
 ) -> None:
     new_film = Movie.objects.create(
         title=movie_title,
