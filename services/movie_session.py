de7cb4347f6d84ef359a3275485354a6ddc6293e
Oleksiy Liubchenko <alexeylubchenko75@gmail.com>
2023-02-08 17:24:01 +0200

    'Solution_upd'

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,6 +1,7 @@
 from db.models import MovieSession
 from django.db.models import QuerySet
 import datetime
+from django.shortcuts import get_object_or_404
 
 
 def create_movie_session(
@@ -23,7 +24,7 @@ def get_movies_sessions(session_date: datetime = None) -> QuerySet:
 
 
 def get_movie_session_by_id(movie_session_id: int) -> object:
-    return MovieSession.objects.get(id=movie_session_id)
+    return get_object_or_404(MovieSession, id=movie_session_id)
 
 
 def update_movie_session(
