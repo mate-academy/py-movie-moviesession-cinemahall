e51104fb25ea6bf2a2c6514c3a533d9170f18e09
Serhii Terletckiy <serhii.terletskiy.ua@gmail.com>
2023-02-16 13:44:41 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -14,3 +14,57 @@ class Actor(models.Model):
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    actors = models.ManyToManyField(
+        Actor,
+        related_name="movies"
+    )
+    genres = models.ManyToManyField(
+        Genre,
+        related_name="genres"
+
+    )
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+
+class MovieSession(models.Model):
+    show_time = models.DateTimeField()
+    cinema_hall = models.ForeignKey(
+        CinemaHall,
+        on_delete=models.CASCADE,
+        related_name="cinema_halls"
+    )
+    movie = models.ForeignKey(
+        Movie,
+        on_delete=models.CASCADE,
+        related_name="movies"
+    )
+
+    def __str__(self) -> str:
+        year = self.show_time.year
+        month = self.show_time.month
+        day = self.show_time.day
+        hour = self.show_time.hour
+        minute = self.show_time.minute
+        return f"{self.movie.title} " \
+               f"{year}-{month:02}-{day:02} " \
+               f"{hour:02}:{minute:02}:00"
