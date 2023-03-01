623088e21532e366d2c3de69c0e25af99b5082fa
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-03-01 16:19:47 +0200

    add CASCADE in models

--- db/models.py
+++ db/models.py
@@ -42,7 +42,7 @@ class CinemaHall(models.Model):
 class MovieSession(models.Model):
     show_time = models.DateTimeField()
     cinema_hall = models.ForeignKey(CinemaHall, on_delete=models.CASCADE)
-    movie = models.ForeignKey(Movie, on_delete=models.SET_NULL, null=True)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return f"{self.movie} {self.show_time}"
