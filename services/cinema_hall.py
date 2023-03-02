63e512f2ae405285c0cfd7f470eb01b61441631b
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2023-03-02 17:19:27 +0200

    Solution_2

--- services/cinema_hall.py
+++ services/cinema_hall.py
@@ -1,7 +1,7 @@
 from db.models import CinemaHall
 
 
-def get_cinema_halls() -> list:
+def get_cinema_halls() -> CinemaHall:
     return CinemaHall.objects.all()
 
 
--- services/movie_session.py
+++ services/movie_session.py
@@ -9,35 +9,35 @@ def create_movie_session(
     new_session = MovieSession.objects.create(
         show_time=movie_show_time,
         cinema_hall_id=cinema_hall_id,
-        movie_id=movie_id)
+        movie_id=movie_id
+    )
     return new_session
 
 
-def get_movies_sessions(session_date: str = None) -> list:
+def get_movies_sessions(session_date: str = None) -> MovieSession:
     queryset = MovieSession.objects.all()
     if session_date:
         queryset = queryset.filter(show_time__date=session_date)
     return queryset
 
 
-def get_movie_session_by_id(movie_session_id: int) -> list:
+def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
     return MovieSession.objects.get(id=movie_session_id)
 
 
 def update_movie_session(session_id: int,
                          show_time: str = None,
                          movie_id: int = None,
-                         cinema_hall_id: int = None) -> MovieSession:
-    old_movie_session = MovieSession.objects.filter(id=session_id)
-    new_session = []
+                         cinema_hall_id: int = None) -> None:
+    session = MovieSession.objects.get(id=session_id)
     if show_time:
-        new_session = old_movie_session.update(show_time=show_time)
+        session.show_time = show_time
     if movie_id:
-        new_session = old_movie_session.update(movie_id=movie_id)
+        session.movie_id = movie_id
     if cinema_hall_id:
-        new_session = old_movie_session.update(cinema_hall_id=cinema_hall_id)
-    return new_session
+        session.cinema_hall_id = cinema_hall_id
+    session.save()
 
 
 def delete_movie_session_by_id(session_id: int) -> None:
-    return MovieSession.objects.get(id=session_id).delete()
+    MovieSession.objects.get(id=session_id).delete()
