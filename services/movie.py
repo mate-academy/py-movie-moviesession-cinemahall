ba435860f8f519d64e5b09652da1951c4cc3f81a
Andriy <andriykrav@gmail.com>
2023-03-07 21:22:49 +0200

    refactored

--- services/movie.py
+++ services/movie.py
@@ -1,24 +1,23 @@
+from typing import Optional
+
 from django.db.models import QuerySet
 
 from db.models import Movie, Genre, Actor
 
 
 def get_movies(
-    genres_ids: int = None, actors_ids: int = None
+        genres_ids: Optional[list] = None,
+        actors_ids: Optional[list] = None
 ) -> QuerySet:
     movies = Movie.objects.all()
 
-    if genres_ids and actors_ids:
-        movies = movies.filter(
-            genres__id__in=genres_ids,
-            actors__id__in=actors_ids
-        )
-    elif genres_ids:
+    if genres_ids:
         movies = movies.filter(genres__id__in=genres_ids)
-    elif actors_ids:
+
+    if actors_ids:
         movies = movies.filter(actors__id__in=actors_ids)
 
-    return movies
+    return movies.distinct()
 
 
 def get_movie_by_id(movie_id: int) -> QuerySet:
@@ -28,8 +27,8 @@ def get_movie_by_id(movie_id: int) -> QuerySet:
 def create_movie(
         movie_title: str,
         movie_description: str,
-        genres_ids: list = None,
-        actors_ids: list = None
+        genres_ids: Optional[list] = None,
+        actors_ids: Optional[list] = None
 ) -> QuerySet:
     movie = Movie.objects.create(
         title=movie_title, description=movie_description
