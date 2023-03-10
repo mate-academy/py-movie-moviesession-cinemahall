2bccd90f338bee952ed5091bae328eead74dd6e7
Illia Smyrnov <ilushasm@gmail.com>
2023-03-10 15:29:35 +0200

    Solution2

--- services/movie.py
+++ services/movie.py
@@ -1,9 +1,11 @@
+from typing import Optional
+
 from db.models import Movie
 
 
 def get_movies(
-    genres_ids: list[int] = None,
-    actors_ids: list[int] = None
+    genres_ids: Optional[list] = None,
+    actors_ids: Optional[list] = None
 ) -> list:
     queryset = Movie.objects.all()
 
@@ -23,8 +25,8 @@ def get_movie_by_id(movie_id: int) -> Movie:
 def create_movie(
     movie_title: str,
     movie_description: str,
-    genres_ids: list[int] = None,
-    actors_ids: list[int] = None
+    genres_ids: Optional[list] = None,
+    actors_ids: Optional[list] = None
 ) -> None:
     new_movie = Movie.objects.create(
         title=movie_title,
--- services/movie_session.py
+++ services/movie_session.py
@@ -1,10 +1,11 @@
-import datetime
+from datetime import datetime
+from typing import Optional
 
 from db.models import MovieSession
 
 
 def create_movie_session(
-    movie_show_time: datetime.datetime,
+    movie_show_time: datetime,
     movie_id: int,
     cinema_hall_id: int,
 ) -> MovieSession:
@@ -16,7 +17,7 @@ def create_movie_session(
     return new_session
 
 
-def get_movies_sessions(session_date: str = None) -> list:
+def get_movies_sessions(session_date: Optional[str] = None) -> list:
     queryset = MovieSession.objects.all()
 
     if session_date:
@@ -31,9 +32,9 @@ def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
 
 def update_movie_session(
     session_id: int,
-    show_time: datetime = None,
-    movie_id: int = None,
-    cinema_hall_id: int = None
+    show_time: Optional[datetime] = None,
+    movie_id: Optional[int] = None,
+    cinema_hall_id: Optional[int] = None
 ) -> None:
     session = MovieSession.objects.get(id=session_id)
 
