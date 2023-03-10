3fdb529d7e8bb12817b4bcd5c88085f9f9f80285
Sergii Machulin <machulinsv@gmail.com>
2023-03-10 21:48:53 +0200

    Update solution

--- services/movie_session.py
+++ services/movie_session.py
@@ -2,24 +2,19 @@ import datetime
 
 from django.db.models import QuerySet
 
-from db.models import MovieSession, Movie, CinemaHall
+from db.models import MovieSession
 
 
 def create_movie_session(
         movie_show_time: datetime.datetime,
         movie_id: int,
         cinema_hall_id: int
-) -> MovieSession:
-    movie_key = Movie.objects.get(id=movie_id)
-    cinema_hall_key = CinemaHall.objects.get(id=cinema_hall_id)
-
-    new_movie_session = MovieSession.objects.create(
+) -> None:
+    MovieSession.objects.create(
         show_time=movie_show_time,
-        movie=movie_key,
-        cinema_hall=cinema_hall_key
+        movie_id=movie_id,
+        cinema_hall_id=cinema_hall_id
     )
-
-    return new_movie_session
 
 
 def get_movies_sessions(session_date: str = None) -> QuerySet:
@@ -38,21 +33,17 @@ def update_movie_session(
         show_time: datetime.datetime = None,
         movie_id: int = None,
         cinema_hall_id: int = None
-) -> MovieSession:
-    session_to_update = MovieSession.objects.filter(id=session_id)
+) -> None:
+    session_to_update = MovieSession.objects.get(id=session_id)
 
     if show_time:
-        session_to_update.update(show_time=show_time)
-
+        session_to_update.show_time = show_time
     if movie_id:
-        movie_key = Movie.objects.get(id=movie_id)
-        session_to_update.update(movie=movie_key)
-
+        session_to_update.movie_id = movie_id
     if cinema_hall_id:
-        cinema_hall_key = CinemaHall.objects.get(id=cinema_hall_id)
-        session_to_update.update(cinema_hall=cinema_hall_key)
+        session_to_update.cinema_hall_id = cinema_hall_id
 
-    return session_to_update
+    session_to_update.save()
 
 
 def delete_movie_session_by_id(session_id: int) -> None:
