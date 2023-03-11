ac48e3583bad5ff57814865cee293f2cf2715511
Sergii Machulin <machulinsv@gmail.com>
2023-03-11 23:13:36 +0200

    Upgrade solution

--- services/movie_session.py
+++ services/movie_session.py
@@ -9,7 +9,7 @@ def create_movie_session(
         movie_show_time: datetime.datetime,
         movie_id: int,
         cinema_hall_id: int
-) -> None:
+) -> MovieSession:
     MovieSession.objects.create(
         show_time=movie_show_time,
         movie_id=movie_id,
