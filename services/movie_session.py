0366d66017f25ae4940d24926a1eceea8b4dff06
Paul Savchuk <nekrjkeee@gmail.com>
2023-03-11 09:15:57 +0200

    flake 83 > 79

--- services/movie_session.py
+++ services/movie_session.py
@@ -20,7 +20,9 @@ def create_movie_session(
     )
 
 
-def get_movies_sessions(session_date: str = None) -> Union[MovieSession, QuerySet]:
+def get_movies_sessions(
+        session_date: str = None
+) -> Union[MovieSession, QuerySet]:
     if session_date:
         return MovieSession.objects.filter(show_time__date=session_date)
     return MovieSession.objects.all()
