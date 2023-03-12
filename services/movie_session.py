a8d438e7be858850149451c71df23f0351af4ff8
VladyslavCherkashyn <Sparrowq2@gmail.com>
2023-03-12 20:04:07 +0200

    Solution

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,11 +1,12 @@
 from db.models import MovieSession
+from django.db.models import QuerySet
 
 
 def create_movie_session(
         movie_show_time: int,
         movie_id: int,
         cinema_hall_id: int
-) -> None:
+) -> MovieSession:
 
     new_session = MovieSession.objects.create(
         show_time=movie_show_time,
@@ -16,7 +17,7 @@ def create_movie_session(
     return new_session
 
 
-def get_movies_sessions(session_date: str = None) -> MovieSession:
+def get_movies_sessions(session_date: str = None) -> QuerySet:
 
     if session_date:
         return MovieSession.objects.filter(show_time__date=session_date)
@@ -32,18 +33,20 @@ def update_movie_session(
         show_time: int = None,
         movie_id: int = None,
         cinema_hall_id: int = None
-) -> None:
+) -> MovieSession:
 
-    update_movie = MovieSession.objects.filter(id=session_id)
+    update_session = MovieSession.objects.filter(id=session_id)
 
     if show_time is not None:
-        update_movie.update(show_time=show_time)
+        update_session.update(show_time=show_time)
 
     if movie_id is not None:
-        update_movie.update(movie_id=movie_id)
+        update_session.update(movie_id=movie_id)
 
     if cinema_hall_id is not None:
-        update_movie.update(cinema_hall_id=cinema_hall_id)
+        update_session.update(cinema_hall_id=cinema_hall_id)
+
+    return update_session
 
 
 def delete_movie_session_by_id(session_id: int) -> None:
