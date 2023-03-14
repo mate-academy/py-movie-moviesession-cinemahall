ceda43c8a4a94bb3b7696bf56e0f5f454bfc152a
Dmytry <donchykd@gmail.com>
2023-03-14 15:04:20 +0200

    Solution

--- services/movie_session.py
+++ services/movie_session.py
@@ -36,15 +36,14 @@ def update_movie_session(session_id: int,
 
     if show_time:
         movie_session.show_time = show_time
-        movie_session.save()
 
     if movie_id:
         movie_session.movie_id = movie_id
-        movie_session.save()
 
     if cinema_hall_id:
         movie_session.cinema_hall_id = cinema_hall_id
-        movie_session.save()
+
+    movie_session.save()
 
 
 def delete_movie_session_by_id(session_id: int) -> None:
