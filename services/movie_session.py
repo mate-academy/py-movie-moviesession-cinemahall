da3abefad2191621321eecd288c86e56f92533ac
Yulia <Mala>
2023-03-14 18:25:18 +0200

    right import sequence

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,7 +1,8 @@
+from datetime import datetime
+
 from django.db.models import QuerySet
 
 from db.models import MovieSession
-from datetime import datetime
 
 
 def create_movie_session(movie_show_time: datetime,
@@ -13,10 +14,10 @@ def create_movie_session(movie_show_time: datetime,
 
 
 def get_movies_sessions(session_date: str = None) -> QuerySet:
-    result = MovieSession.objects.all()
+    queryset = MovieSession.objects.all()
     if session_date:
-        result = result.filter(show_time__date=session_date)
-    return result
+        queryset = queryset.filter(show_time__date=session_date)
+    return queryset
 
 
 def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
