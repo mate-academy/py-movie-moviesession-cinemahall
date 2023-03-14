e9ca4b809b861370efdb65bc354b098b40518d30
VladyslavCherkashyn <Sparrowq2@gmail.com>
2023-03-14 18:40:42 +0200

    Solution

--- services/movie_session.py
+++ services/movie_session.py
@@ -31,21 +31,21 @@ def update_movie_session(
         show_time: int = None,
         movie_id: int = None,
         cinema_hall_id: int = None
-) -> MovieSession:
+) -> None:
 
-    update_session = MovieSession.objects.filter(id=session_id)
+    update_session = MovieSession.objects.get(id=session_id)
 
     if show_time is not None:
-        update_session.update(show_time=show_time)
+        update_session.show_time = show_time
 
     if movie_id is not None:
-        update_session.update(movie_id=movie_id)
+        update_session.movie_id = movie_id
 
     if cinema_hall_id is not None:
-        update_session.update(cinema_hall_id=cinema_hall_id)
+        update_session.cinema_hall_id = cinema_hall_id
 
-    return update_session
+    update_session.save()
 
 
 def delete_movie_session_by_id(session_id: int) -> None:
-    MovieSession.objects.filter(id=session_id).delete()
+    MovieSession.objects.get(id=session_id).delete()
