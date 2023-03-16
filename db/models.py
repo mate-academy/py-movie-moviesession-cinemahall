f1065e347cb68b97567ea2b456ff18f0c8b6391f
Yevhen Kaskov <kaskov.e@gmail.com>
2023-03-16 13:53:28 +0200

    'unnecessary_condition_deleted'

--- db/models.py
+++ db/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+git commit
 
 class Genre(models.Model):
     name = models.CharField(max_length=255, unique=True)
@@ -19,8 +19,8 @@ class Actor(models.Model):
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    actors = models.ManyToManyField(Actor)
-    genres = models.ManyToManyField(Genre)
+    actors = models.ManyToManyField(Actor, related_name="movies")
+    genres = models.ManyToManyField(Genre, related_name="movies")
 
     def __str__(self) -> str:
         return f"{self.title}"
--- services/movie.py
+++ services/movie.py
@@ -11,9 +11,6 @@ def get_movies(
 ) -> QuerySet:
     movies = Movie.objects.all()
 
-    if genres_ids is None and actors_ids is None:
-        return movies
-
     if genres_ids:
         movies = movies.filter(genres__id__in=genres_ids)
     if actors_ids:
