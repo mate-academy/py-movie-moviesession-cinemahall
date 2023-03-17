99b8131556d91c545970b30803584eb7a39fbb81
Bohdan Kovalenko <bog7807@gmail.com>
2023-03-17 05:27:00 +0200

    Solution

--- services/movie_session.py
+++ services/movie_session.py
@@ -13,30 +13,26 @@ def create_movie_session(
 ) -> None:
     MovieSession.objects.create(
         show_time=movie_show_time,
-        cinema_hall=CinemaHall.objects.get(id=cinema_hall_id),
+        cinema_hall_id=cinema_hall_id,
         movie=get_movie_by_id(movie_id)
     )
 
 
-def get_movies_sessions(session_date: (list[str], None) = None) -> QuerySet:
+def get_movies_sessions(session_date: str = None) -> QuerySet:
     queryset = MovieSession.objects.all()
     if session_date:
-        queryset = queryset.filter(
-            show_time__date=datetime.strptime(
-                session_date, "%Y-%m-%d"
-            )
-        )
+        queryset = queryset.filter(show_time__date=session_date)
     return queryset
 
 
 def get_movie_session_by_id(
-        movie_session_id: (list[int], None) = None
+        movie_session_id: list[int] = None
 ) -> MovieSession:
     return MovieSession.objects.get(id=movie_session_id)
 
 
 def update_movie_session(
-        session_id: int,
+        session_id: list[int],
         show_time: (list[datetime], None) = None,
         movie_id: (list[int], None) = None,
         cinema_hall_id: (list[int], None) = None
@@ -47,9 +43,9 @@ def update_movie_session(
     if movie_id:
         session.movie = get_movie_by_id(movie_id)
     if cinema_hall_id:
-        session.cinema_hall = CinemaHall.objects.get(id=cinema_hall_id)
+        session.cinema_hall_id = cinema_hall_id
     session.save()
 
 
-def delete_movie_session_by_id(session_id: int) -> None:
+def delete_movie_session_by_id(session_id: list[int]) -> None:
     get_movie_session_by_id(session_id).delete()
