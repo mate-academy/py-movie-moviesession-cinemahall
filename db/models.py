df0deb11cbb1d05f1a8e5ab84836530b8ff4ee18
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-03-17 17:20:15 +0200

    Fixed imports, added 'related_name'

--- db/models.py
+++ db/models.py
@@ -50,10 +50,12 @@ class MovieSession(models.Model):
     cinema_hall = models.ForeignKey(
         CinemaHall,
         on_delete=models.CASCADE,
+        related_name="sessions"
     )
     movie = models.ForeignKey(
         Movie,
         on_delete=models.CASCADE,
+        related_name="sessions"
     )
 
     def __str__(self) -> str:
