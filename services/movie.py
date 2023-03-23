f5ac2aa886f43d30f2139feb9769c020bdaa2efd
Tetiana Kozakova <kleotan901@gmail.com>
2023-03-23 17:55:42 +0100

    solution

---
+++ services/movie.py
@@ -0,0 +1,37 @@
+from django.db.models import QuerySet
+
+import init_django_orm # noqa: F401
+from db.models import Movie
+
+
+def get_movies(
+        genres_ids: list[int] = None,
+        actors_ids: list[int] = None
+) -> QuerySet:
+    movies_queryset = Movie.objects.all()
+    if genres_ids is not None:
+        movies_queryset = movies_queryset.filter(genres__id__in=genres_ids)
+    if actors_ids is not None:
+        movies_queryset = movies_queryset.filter(actors__id__in=actors_ids)
+    return movies_queryset
+
+
+def get_movie_by_id(movie_id: int) -> str:
+    return Movie.objects.filter(id=movie_id).first()
+
+
+def create_movie(
+        movie_title: str,
+        movie_description: str,
+        genres_ids: list[int] = None,
+        actors_ids: list[int] = None
+) -> QuerySet:
+    new_movie = Movie.objects.create(
+        title=movie_title,
+        description=movie_description
+    )
+    if genres_ids:
+        new_movie.genres.set(genres_ids)
+    if actors_ids:
+        new_movie.actors.set(actors_ids)
+    return new_movie
---
+++ services/movie_session.py
@@ -0,0 +1,67 @@
+from django.db.models import QuerySet
+
+import init_django_orm # noqa: F401
+import datetime
+#from django.db.models import QuerySet
+
+from db.models import MovieSession
+
+
+def create_movie_session(
+        movie_show_time: datetime,
+        movie_id: int,
+        cinema_hall_id: int
+):
+    new_movie_session = MovieSession.objects.create(
+        show_time=movie_show_time,
+        cinema_hall_id=cinema_hall_id,
+        movie_id=movie_id
+    )
+    return new_movie_session
+
+
+def get_movies_sessions(session_date: str = None) -> QuerySet:
+    queryset = MovieSession.objects.all()
+    if session_date is not None:
+        queryset = queryset.filter(
+            show_time__year=session_date.split("-")[0],
+            show_time__month=session_date.split("-")[1],
+            show_time__day=session_date.split("-")[2]
+        )
+
+    return queryset
+
+
+def get_movie_session_by_id(movie_session_id: int) -> str:
+    return MovieSession.objects.filter(id=movie_session_id).first()
+
+
+def update_movie_session(
+        session_id: int,
+        show_time: datetime = None,
+        movie_id: int = None,
+        cinema_hall_id: int = None
+):
+    queryset_session_for_update = MovieSession.objects.filter(
+        id=session_id
+    )
+
+    if show_time is not None:
+        queryset_session_for_update.update(
+            show_time=show_time
+        )
+    if movie_id is not None:
+        queryset_session_for_update.update(
+            movie_id=movie_id
+        )
+    if cinema_hall_id is not None:
+        queryset_session_for_update.update(
+            cinema_hall_id=cinema_hall_id
+        )
+
+    return queryset_session_for_update
+
+
+def delete_movie_session_by_id(session_id: int) -> None:
+    movie_session = MovieSession.objects.filter(id=session_id).first()
+    movie_session.delete()
