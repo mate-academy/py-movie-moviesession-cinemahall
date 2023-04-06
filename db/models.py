3684f9e8ea9f9babefb8d2a954e659b8fc602d6f
Anatoliy Pilipey <pilipeytolya@gmail.com>
2023-04-06 09:06:59 +0300

    Solution

--- db/models.py
+++ db/models.py
@@ -14,3 +14,48 @@ class Actor(models.Model):
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    actors = models.ManyToManyField(
+        Actor,
+        related_name="actor_in_movie"
+    )
+    genres = models.ManyToManyField(
+        Genre,
+        related_name="genre_movie"
+    )
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class MovieSession(models.Model):
+    show_time = models.DateTimeField()
+    cinema_hall = models.ForeignKey(
+        CinemaHall,
+        on_delete=models.CASCADE,
+        related_name="hall_with_movie"
+    )
+    movie = models.ForeignKey(
+        Movie,
+        on_delete=models.CASCADE,
+        related_name="movie_in_hall"
+    )
+
+    def __str__(self) -> str:
+        return f"{self.movie} {self.show_time}"
