c4021cec60ad577f90f327bf37cba2c87e441b91
kostya-kononenko <kkononenko3@gmail.com>
2023-04-11 14:13:28 +0300

    Solution2

--- services/movie.py
+++ services/movie.py
@@ -5,8 +5,8 @@ from django.db.models import QuerySet
 from db.models import Movie
 
 
-def get_movies(genres_ids: list[int] = None,
-               actors_ids: list[int] = None) -> QuerySet[Movie]:
+def get_movies(genres_ids: list[int] | None = None,
+               actors_ids: list[int] | None = None) -> QuerySet[Movie]:
     queryset = Movie.objects.all()
     if genres_ids is not None:
         queryset = queryset.filter(genres__id__in=genres_ids)
@@ -21,8 +21,8 @@ def get_movie_by_id(movie_id: int) -> Optional[Movie]:
 
 def create_movie(movie_title: str,
                  movie_description: str,
-                 genres_ids: list = None,
-                 actors_ids: list = None) -> None:
+                 genres_ids: list[int] | None = None,
+                 actors_ids: list[int] | None = None) -> None:
     new_movie = Movie.objects.create(
         title=movie_title,
         description=movie_description
--- services/movie_session.py
+++ services/movie_session.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from typing import Optional
 
 from django.db.models import QuerySet
+from django.shortcuts import get_object_or_404
 
 from db.models import MovieSession
 
@@ -14,7 +15,7 @@ def create_movie_session(movie_show_time: datetime,
                                               cinema_hall_id=cinema_hall_id)
 
 
-def get_movies_sessions(session_date: datetime
+def get_movies_sessions(session_date: datetime | None
                         = None) -> QuerySet[MovieSession]:
     queryset = MovieSession.objects.all()
     if session_date is not None:
@@ -26,17 +27,18 @@ def get_movie_session_by_id(movie_session_id: int) -> Optional[MovieSession]:
     return MovieSession.objects.get(id=movie_session_id)
 
 
-def update_movie_session(session_id: int = None,
-                         show_time: str = None,
-                         movie_id: int = None,
-                         cinema_hall_id: int = None) -> None:
-    movie_session = MovieSession.objects.filter(id=session_id)
-    if show_time is not None:
-        movie_session.update(show_time=show_time)
-    if movie_id is not None:
-        movie_session.update(movie_id=movie_id)
-    if cinema_hall_id is not None:
-        movie_session.update(cinema_hall_id=cinema_hall_id)
+def update_movie_session(session_id: int | None = None,
+                         show_time: str | None = None,
+                         movie_id: int | None = None,
+                         cinema_hall_id: int | None = None) -> None:
+    movie_session = get_object_or_404(MovieSession, id=session_id)
+    if show_time:
+        movie_session.show_time = show_time
+    if movie_id:
+        movie_session.movie_id = movie_id
+    if cinema_hall_id:
+        movie_session.cinema_hall_id = cinema_hall_id
+    movie_session.save()
 
 
 def delete_movie_session_by_id(session_id: int) -> None:
