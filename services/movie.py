75b90033eeabb5d8892b81a40fd0091a26f1368f
Alexandr <olexandrzhezheria@gmail.com>
2023-04-25 12:45:55 +0300

    Solution

--- services/movie.py
+++ services/movie.py
@@ -1,33 +1,30 @@
+from typing import Optional, List
+
 from django.db.models import QuerySet
 from db.models import Movie
 
 
 def get_movies(
-    genres_ids: list[int] = None,
-    actors_ids: list[int] = None,
+    genres_ids: Optional[List[int]] = None,
+    actors_ids: Optional[List[int]] = None,
 ) -> QuerySet:
-    match (genres_ids, actors_ids):
-        case (None, None):
-            return Movie.objects.all()
-        case (_, None):
-            return Movie.objects.filter(genres__id__in=genres_ids).distinct()
-        case (None, _):
-            return Movie.objects.filter(actors__id__in=actors_ids).distinct()
-        case _:
-            return Movie.objects.filter(
-                genres__id__in=genres_ids, actors__id__in=actors_ids
-            ).distinct()
+    movies = Movie.objects.all()
+    if genres_ids:
+        movies = movies.filter(genres__id__in=genres_ids).distinct()
+    if actors_ids:
+        movies = movies.filter(actors__id__in=actors_ids).distinct()
+    return movies
 
 
-def get_movie_by_id(movie_id: list[int]) -> Movie:
+def get_movie_by_id(movie_id: int) -> Movie:
     return Movie.objects.get(pk=movie_id)
 
 
 def create_movie(
     movie_title: str,
     movie_description: str,
-    genres_ids: list[int] = None,
-    actors_ids: list[int] = None,
+    genres_ids: Optional[List[int]] = None,
+    actors_ids: Optional[List[int]] = None,
 ) -> None:
     movie = Movie.objects.create(
         title=movie_title,
