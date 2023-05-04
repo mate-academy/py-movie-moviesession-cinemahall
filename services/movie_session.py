5ab741acaf3a9da7af1520d4f90acbfac2b67192
Nochvai Maksym <123680608+MaksNochvai@users.noreply.github.com>
2023-05-04 10:20:54 +0300

    Solution

--- services/movie_session.py
+++ services/movie_session.py
@@ -5,7 +5,7 @@ from db.models import MovieSession
 
 def create_movie_session(movie_show_time: str,
                          movie_id: int,
-                         cinema_hall_id: int) -> QuerySet:
+                         cinema_hall_id: int) -> MovieSession:
     return MovieSession.objects.create(
         show_time=movie_show_time,
         cinema_hall_id=cinema_hall_id,
@@ -22,7 +22,7 @@ def get_movies_sessions(session_date: Optional[str] = None) -> QuerySet:
     return movie_session
 
 
-def get_movie_session_by_id(movie_session_id: int) -> QuerySet:
+def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
     return MovieSession.objects.get(
         id=movie_session_id
     )
@@ -32,13 +32,14 @@ def update_movie_session(session_id: int,
                          show_time: Optional[str] = None,
                          movie_id: Optional[int] = None,
                          cinema_hall_id: Optional[int] = None) -> None:
-    movie_session_to_update = MovieSession.objects.filter(id=session_id)
+    update_fields = {}
     if show_time is not None:
-        movie_session_to_update.update(show_time=show_time)
+        update_fields["show_time"] = show_time
     if movie_id is not None:
-        movie_session_to_update.update(movie_id=movie_id)
+        update_fields["movie_id"] = movie_id
     if cinema_hall_id is not None:
-        movie_session_to_update.update(cinema_hall_id=cinema_hall_id)
+        update_fields["cinema_hall_id"] = cinema_hall_id
+    MovieSession.objects.filter(id=session_id).update(**update_fields)
 
 
 def delete_movie_session_by_id(session_id: int) -> None:
