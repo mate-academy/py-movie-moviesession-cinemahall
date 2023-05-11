01f903b09889ca4485053d7302f24e7fa628266f
Mykola <nickkozlov90@gmail.com>
2023-05-11 11:34:37 +0300

    corrections adjusted

--- services/movie.py
+++ services/movie.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from django.db.models import QuerySet
 
 import init_django_orm  # noqa: F401
@@ -7,8 +9,8 @@ from db.models import Movie
 
 
 def get_movies(
-        genres_ids: list[int] = None,
-        actors_ids: list[int] = None
+        genres_ids: Optional[list[int]] = None,
+        actors_ids: Optional[list[int]] = None
 ) -> QuerySet:
     queryset = Movie.objects.all()
     if genres_ids is not None:
@@ -25,8 +27,8 @@ def get_movie_by_id(movie_id: int) -> Movie:
 def create_movie(
     movie_title: str,
     movie_description: str,
-    genres_ids: list[str] = None,
-    actors_ids: list[str] = None
+    genres_ids: Optional[list[str]] = None,
+    actors_ids: Optional[list[str]] = None
 ) -> Movie:
     new_movie = Movie.objects.create(
         title=movie_title,
--- services/movie_session.py
+++ services/movie_session.py
@@ -1,11 +1,13 @@
 from datetime import datetime
-from xmlrpc.client import DateTime
+from typing import Optional
+
+from django.db.models import QuerySet
 
 from db.models import MovieSession, Movie, CinemaHall
 
 
 def create_movie_session(
-    movie_show_time: DateTime,
+    movie_show_time: datetime,
     movie_id: int,
     cinema_hall_id: int
 ) -> MovieSession:
@@ -16,7 +18,7 @@ def create_movie_session(
     )
 
 
-def get_movies_sessions(session_date: str = None) -> MovieSession:
+def get_movies_sessions(session_date: Optional[str] = None) -> QuerySet:
     if session_date:
         return MovieSession.objects.filter(
             show_time__date=datetime.strptime(session_date, "%Y-%m-%d")
@@ -30,9 +32,9 @@ def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
 
 def update_movie_session(
         session_id: int,
-        show_time: DateTime = None,
-        movie_id: int = None,
-        cinema_hall_id: int = None
+        show_time: Optional[datetime] = None,
+        movie_id: Optional[int] = None,
+        cinema_hall_id: Optional[int] = None
 ) -> None:
     session = MovieSession.objects.get(id=session_id)
     if show_time:
