7d11c81e55b748cd7fb1ffe72b0dbebfbf32c717
“Ruslan <callogan217@gmail.com>
2023-05-26 18:55:34 +0300

    Typehints have been added

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,6 +1,7 @@
 from django.db.models import QuerySet
 
 from db.models import MovieSession
+from typing import Optional
 
 
 def create_movie_session(
@@ -15,7 +16,7 @@ def create_movie_session(
     )
 
 
-def get_movies_sessions(session_date: str = None) -> QuerySet:
+def get_movies_sessions(session_date: Optional[str] = None) -> QuerySet:
     queryset = MovieSession.objects.all()
 
     if session_date:
@@ -29,9 +30,9 @@ def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
 
 
 def update_movie_session(
-        session_id: int,
-        show_time: str = None,
-        movie_id: int = None,
+        session_id: Optional[int],
+        show_time: Optional[str] = None,
+        movie_id: Optional[int] = None,
         cinema_hall_id: int = None
 ) -> None:
     update_params = {
