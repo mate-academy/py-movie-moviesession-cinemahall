3965aec8858154d8460e7800df822806bf122efb
OleksandrLeliukh <alexander.leluh@gmail.com>
2023-05-27 12:04:33 +0300

    'Solution'

--- services/movie_session.py
+++ services/movie_session.py
@@ -6,10 +6,11 @@ from django.db.models import QuerySet
 from db.models import MovieSession
 
 
-def create_movie_session(movie_show_time: datetime,
-                         movie_id: int,
-                         cinema_hall_id: int,
-                         ) -> None:
+def create_movie_session(
+    movie_show_time: datetime,
+    movie_id: int,
+    cinema_hall_id: int,
+) -> None:
     MovieSession.objects.create(
         show_time=movie_show_time,
         cinema_hall_id=cinema_hall_id,
@@ -27,11 +28,12 @@ def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
     return MovieSession.objects.get(id=movie_session_id)
 
 
-def update_movie_session(session_id: int,
-                         show_time: Optional[datetime] = None,
-                         movie_id: Optional[int] = None,
-                         cinema_hall_id: Optional[int] = None
-                         ) -> None:
+def update_movie_session(
+    session_id: int,
+    show_time: Optional[datetime] = None,
+    movie_id: Optional[int] = None,
+    cinema_hall_id: Optional[int] = None
+) -> None:
     queryset = MovieSession.objects.get(id=session_id)
 
     if show_time:
