a7b60637d4278d16d47ff9f2ed91533a810774f1
Vladyslav <vladyslavkhokhlov@gmail.com>
2023-06-04 13:05:18 +0300

    Solution

--- services/movie_session.py
+++ services/movie_session.py
@@ -17,11 +17,13 @@ def create_movie_session(movie_show_time: str,
 
 
 def get_movies_sessions(session_date: Optional[str] = None) -> QuerySet:
+    movie_sessions = MovieSession.objects.all()
     if session_date:
-        return MovieSession.objects.filter(
-            show_time__date=datetime.strptime(session_date, "%Y-%m-%d")
+        movie_sessions = movie_sessions.filter(
+            show_time__date=session_date
         )
-    return MovieSession.objects.all()
+
+    return movie_sessions
 
 
 def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
@@ -31,19 +33,25 @@ def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
 def update_movie_session(session_id: int,
                          show_time: datetime = None,
                          movie_id: int = None,
-                         cinema_hall_id: int = None) -> QuerySet:
-    queryset = MovieSession.objects.filter(id=session_id)
+                         cinema_hall_id: int = None) -> MovieSession:
+    session = MovieSession.objects.get(id=session_id)
+    update_information = {}
 
     if show_time is not None:
-        queryset.update(show_time=show_time)
+        update_information["show_time"] = show_time
 
     if movie_id is not None:
-        queryset.update(movie_id=movie_id)
+        update_information["movie_id"] = movie_id
 
     if cinema_hall_id is not None:
-        queryset.update(cinema_hall_id=cinema_hall_id)
+        update_information["cinema_hall_id"] = cinema_hall_id
 
-    return queryset
+    for key, value in update_information.items():
+        setattr(session, key, value)
+
+    session.save()
+
+    return session
 
 
 def delete_movie_session_by_id(session_id: int) -> None:
