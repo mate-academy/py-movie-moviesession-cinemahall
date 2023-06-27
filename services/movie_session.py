b7602340a6f76eddfe90b40b0fd477670506f4c5
Munkoor <eee1lvlboss@gmail.com>
2023-06-27 21:56:38 +0300

    fixed a code

--- services/movie_session.py
+++ services/movie_session.py
@@ -41,4 +41,4 @@ def update_movie_session(session_id: int, show_time: str = None,
 
 
 def delete_movie_session_by_id(session_id: int) -> None:
-    MovieSession.objects.filter(id=session_id).delete()
+    get_movie_session_by_id(session_id).delete()
