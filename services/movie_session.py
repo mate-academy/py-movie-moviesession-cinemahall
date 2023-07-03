9980ca66e5ce1422b82dacc88f5ace41e2b5c87b
Oleksii Kolii <igravkompik@gmail.com>
2023-07-03 16:56:21 +0300

    solution 3

--- services/movie_session.py
+++ services/movie_session.py
@@ -24,7 +24,7 @@ def get_movie_session_by_id(movie_session_id: int) -> QuerySet:
     return MovieSession.objects.get(id=movie_session_id)
 
 
-def update_movie_sessions(session_id: int,
+def get_movies_sessions(session_id: int,
                           show_time: datetime = None,
                           movie_id: int = None,
                           cinema_hall_id: int = None) -> None:
