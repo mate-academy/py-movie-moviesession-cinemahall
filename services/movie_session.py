b25e6418d1cffb3729b6646cbf1627b074809530
Olha Tryhub <olha.tryhub@nure.ua>
2023-07-04 09:04:54 +0300

    Solution for Cinema

---
+++ services/movie_session.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+from django.db.models import QuerySet
+
+from db.models import MovieSession
+
+
+def create_movie_session(movie_show_time: datetime,
+                         movie_id: int,
+                         cinema_hall_id: int) -> None:
+    MovieSession.objects.create(
+        show_time=movie_show_time,
+        movie_id=movie_id,
+        cinema_hall_id=cinema_hall_id)
+
+
+def get_movies_sessions(session_date: str = None) -> QuerySet:
+    movie_sessions_set = MovieSession.objects.all()
+
+    if session_date:
+        session_year, session_month, session_day = session_date.split("-")
+        movie_sessions_set = movie_sessions_set.filter(
+            show_time__year=int(session_year),
+            show_time__month=int(session_month),
+            show_time__day=int(session_day),
+        )
+
+    return movie_sessions_set
+
+
+def get_movie_session_by_id(movie_session_id: int) -> QuerySet:
+    return MovieSession.objects.get(id=movie_session_id)
+
+
+def update_movie_session(session_id: int,
+                         show_time: datetime = None,
+                         movie_id: int = None,
+                         cinema_hall_id: int = None) -> None:
+    movie_session = MovieSession.objects.get(id=session_id)
+
+    if show_time:
+        movie_session.show_time = show_time
+
+    if movie_id:
+        movie_session.movie_id = movie_id
+
+    if cinema_hall_id:
+        movie_session.cinema_hall_id = cinema_hall_id
+
+    movie_session.save()
+
+
+def delete_movie_session_by_id(session_id: int) -> None:
+    MovieSession.objects.filter(id=session_id).delete()
