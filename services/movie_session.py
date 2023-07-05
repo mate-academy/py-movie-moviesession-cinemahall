ad9e22ce84d75c2dc3bb2a5a0cdfbc93074330ca
kovaliskoveronika <veronika.kovalisko.knm.2020@lpnu.ua>
2023-07-05 15:50:32 +0300

    tried to fix problems

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,19 +1,21 @@
 from datetime import datetime
 
+from django.db.models import QuerySet
+
 from db.models import MovieSession
 
 
-def create_movie_session(movie_show_time: datetime,
-                         movie_id: int,
-                         cinema_hall_id: int) -> MovieSession:
-    new_movie_session = MovieSession.objects.create(
+def create_movie_session(
+        movie_show_time: datetime,
+        movie_id: int,
+        cinema_hall_id: int) -> None:
+    MovieSession.objects.create(
         show_time=movie_show_time,
         movie_id=movie_id,
         cinema_hall_id=cinema_hall_id)
-    return new_movie_session
 
 
-def get_movies_sessions(session_date: str = None) -> MovieSession:
+def get_movies_sessions(session_date: str = None) -> QuerySet[MovieSession]:
     session = MovieSession.objects.all()
     if session_date:
         session = session.filter(show_time__date=session_date)
@@ -27,7 +29,7 @@ def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
 def update_movie_session(session_id: int,
                          show_time: datetime = None,
                          movie_id: int = None,
-                         cinema_hall_id: int = None) -> MovieSession:
+                         cinema_hall_id: int = None) -> None:
     session_to_update = MovieSession.objects.get(id=session_id)
     if show_time:
         session_to_update.show_time = show_time
@@ -37,7 +39,6 @@ def update_movie_session(session_id: int,
         session_to_update.cinema_hall_id = cinema_hall_id
 
     session_to_update.save()
-    return session_to_update
 
 
 def delete_movie_session_by_id(session_id: int) -> None:
