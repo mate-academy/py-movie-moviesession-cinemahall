1773600e9f1742b38f962175c7d0d7529787a6db
Oleksii Kolii <igravkompik@gmail.com>
2023-07-05 18:27:26 +0300

    changed return annotations

--- services/movie.py
+++ services/movie.py
@@ -13,14 +13,14 @@ def get_movies(genres_ids: list = None, actors_ids: list = None) -> QuerySet:
     return query
 
 
-def get_movie_by_id(movie_id: int) -> None:
+def get_movie_by_id(movie_id: int) -> Movie:
     return Movie.objects.get(id=movie_id)
 
 
 def create_movie(movie_title: str,
                  movie_description: str,
                  genres_ids: list = None,
-                 actors_ids: list = None) -> Movie:
+                 actors_ids: list = None) -> None:
     movie = Movie.objects.create(title=movie_title,
                                  description=movie_description)
     if genres_ids:
