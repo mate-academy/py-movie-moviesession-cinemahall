8bdf971b3f8f477024a13c65ba98eb3c424df196
DmytroTsehelnyk <dmytrotsehelnyk@yahoo.com>
2023-07-06 16:16:17 +0300

    Solution fix

--- services/cinema_hall.py
+++ services/cinema_hall.py
@@ -4,16 +4,20 @@ from db.models import CinemaHall
 
 
 def get_cinema_halls() -> QuerySet:
-    return CinemaHall.objects.all()
+    return CinemaHall.objects
 
 
 def create_cinema_hall(
     hall_name: str,
     hall_rows: int,
     hall_seats_in_row: int
-) -> CinemaHall:
-    return CinemaHall.objects.create(
+) -> None:
+    CinemaHall.objects.create(
         name=hall_name,
         rows=hall_rows,
         seats_in_row=hall_seats_in_row
     )
+
+
+if __name__ == '__main__':
+    get_cinema_halls()
