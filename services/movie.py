124656576191264f27d36036fdb17563eadc8dcc
Mariia Horbova <gorbova21@gmail.com>
2023-07-06 19:00:02 +0300

    fixed indentation

--- services/movie.py
+++ services/movie.py
@@ -22,11 +22,12 @@ def get_movie_by_id(movie_id: int) -> Movie:
     return Movie.objects.get(pk=movie_id)
 
 
-def create_movie(movie_title: str,
-                 movie_description: str,
-                 genres_ids: list[int] = None,
-                 actors_ids: list[int] = None
-                 ) -> None:
+def create_movie(
+        movie_title: str,
+        movie_description: str,
+        genres_ids: list[int] = None,
+        actors_ids: list[int] = None
+) -> None:
     movie = Movie.objects.create(
         title=movie_title,
         description=movie_description,
