170f5bd7ab54803f441999c7b0ee1300a6640546
Mariia Horbova <gorbova21@gmail.com>
2023-07-06 15:47:06 +0300

    fixed indentation and other bad practices

--- services/movie.py
+++ services/movie.py
@@ -1,13 +1,11 @@
-from typing import Optional
-
 from django.db.models import QuerySet
 
 from db.models import Movie
 
 
 def get_movies(
-        genres_ids: Optional[list] = None,
-        actors_ids: Optional[list] = None
+        genres_ids: list[int] = None,
+        actors_ids: list[int] = None
 ) -> QuerySet:
     movies = Movie.objects.all()
 
@@ -26,8 +24,8 @@ def get_movie_by_id(movie_id: int) -> Movie:
 
 def create_movie(movie_title: str,
                  movie_description: str,
-                 genres_ids: Optional[list] = None,
-                 actors_ids: Optional[list] = None
+                 genres_ids: list[int] = None,
+                 actors_ids: list[int] = None
                  ) -> None:
     movie = Movie.objects.create(
         title=movie_title,
--- services/movie_session.py
+++ services/movie_session.py
@@ -1,23 +1,22 @@
-from typing import Optional
-
 from django.db.models import QuerySet
 
 from db.models import CinemaHall, Movie, MovieSession
 
 
-def create_movie_session(movie_show_time: str,
-                         movie_id: int,
-                         cinema_hall_id: int
-                         ) -> None:
+def create_movie_session(
+        movie_show_time: str,
+        movie_id: int,
+        cinema_hall_id: int
+) -> None:
     MovieSession.objects.create(
         show_time=movie_show_time,
         movie_id=movie_id,
-        cinema_hall=CinemaHall(pk=cinema_hall_id)
+        cinema_hall_id=cinema_hall_id
     )
 
 
 def get_movies_sessions(
-        session_date: Optional[str] = None
+        session_date: str = None
 ) -> QuerySet:
     movies_sessions = MovieSession.objects.all()
     if session_date:
@@ -29,11 +28,12 @@ def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
     return MovieSession.objects.get(pk=movie_session_id)
 
 
-def update_movie_session(session_id: int,
-                         show_time: Optional[str] = None,
-                         movie_id: Optional[int] = None,
-                         cinema_hall_id: Optional[int] = None
-                         ) -> None:
+def update_movie_session(
+        session_id: int,
+        show_time: str = None,
+        movie_id: int = None,
+        cinema_hall_id: int = None
+) -> None:
     movie_session = get_movie_session_by_id(session_id)
 
     if show_time:
