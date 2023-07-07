a0f46761f4b80425780b052729c8efa7482c855d
Oksana Makieieva <131467999+muriakula@users.noreply.github.com>
2023-07-07 11:47:43 +0200

    Update services/movie.py
    Co-authored-by: Bohdan Salamakha <86322722+Bohdan-Salamakha@users.noreply.github.com>

--- services/movie.py
+++ services/movie.py
@@ -6,7 +6,7 @@ from db.models import Movie
 def get_movies(
         genres_ids: Optional[list[int]] = None,
         actors_ids: Optional[list[int]] = None
-) -> tuple[Movie]:
+) -> Queryset[Movie]:
     if not genres_ids and not actors_ids:
         return Movie.objects.all()
     if genres_ids and not actors_ids:
