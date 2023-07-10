a9d7aaff6bf10d2e1850bf655de1f47ada73bfa0
Nazar Lytvyniuk <nazarltvnk@icloud.com>
2023-07-10 17:41:25 +0200

    Change related names

--- db/models.py
+++ db/models.py
@@ -44,12 +44,12 @@ class MovieSession(models.Model):
     cinema_hall = models.ForeignKey(
         CinemaHall,
         on_delete=models.CASCADE,
-        related_name="movies_sessions"
+        related_name="movie_sessions"
     )
     movie = models.ForeignKey(
         Movie,
         on_delete=models.CASCADE,
-        related_name="movies_sessions"
+        related_name="sessions"
     )
 
     def __str__(self) -> str:
