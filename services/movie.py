72c5bee31711a1249e43d86ae8e11c9bf84dc53a
Vlad Larin <lordgadyka71@gmail.com>
2023-07-21 19:38:05 +0300

    solution2

--- services/movie.py
+++ services/movie.py
@@ -1,15 +1,17 @@
+from typing import Optional
+
 from db.models import Movie
 from django.db.models.query import QuerySet
 
 
 def get_movies(
-        genres_ids: list[int] = None,
-        actors_ids: list[int] = None
+        genres_ids: Optional[list[int]] = None,
+        actors_ids: Optional[list[int]] = None
 ) -> QuerySet:
     queryset = Movie.objects.all()
-    if genres_ids:
+    if genres_ids is not None:
         queryset = queryset.filter(genres__id__in=genres_ids).distinct()
-    if actors_ids:
+    if actors_ids is not None:
         queryset = queryset.filter(actors__id__in=actors_ids).distinct()
     return queryset
 
@@ -21,12 +23,12 @@ def get_movie_by_id(movie_id: int) -> Movie:
 def create_movie(
         movie_title: str,
         movie_description: str,
-        genres_ids: list[int] = None,
-        actors_ids: list[int] = None
+        genres_ids: Optional[list[int]] = None,
+        actors_ids: Optional[list[int]] = None
 ) -> None:
     new_movie = Movie.objects.create(title=movie_title,
                                      description=movie_description)
-    if genres_ids:
+    if genres_ids is not None:
         new_movie.genres.set(genres_ids)
-    if actors_ids:
+    if actors_ids is not None:
         new_movie.actors.set(actors_ids)
--- services/movie_session.py
+++ services/movie_session.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Optional
 
 from db.models import MovieSession
 
@@ -15,9 +16,11 @@ def create_movie_session(
     )
 
 
-def get_movies_sessions(session_date: datetime = None) -> MovieSession:
+def get_movies_sessions(
+        session_date: Optional[datetime] = None
+) -> MovieSession:
     queryset = MovieSession.objects.all()
-    if session_date:
+    if session_date is not None:
         queryset = queryset.filter(show_time__date=session_date)
     return queryset
 
@@ -28,16 +31,16 @@ def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
 
 def update_movie_session(
         session_id: int,
-        show_time: datetime = None,
-        movie_id: int = None,
-        cinema_hall_id: int = None
+        show_time: Optional[datetime] = None,
+        movie_id: Optional[int] = None,
+        cinema_hall_id: Optional[int] = None
 ) -> None:
     movie_session = get_movie_session_by_id(session_id)
-    if show_time:
+    if show_time is not None:
         movie_session.show_time = show_time
-    if movie_id:
+    if movie_id is not None:
         movie_session.movie_id = movie_id
-    if cinema_hall_id:
+    if cinema_hall_id is not None:
         movie_session.cinema_hall_id = cinema_hall_id
     movie_session.save()
 
