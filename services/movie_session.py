90347ad9ae0537f6bf3b26f5cf118882af9b1368
Oleksandr <@gmail.com>
2023-07-24 16:24:39 +0300

    'Fixed'

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,4 +1,5 @@
 import datetime
+from typing import Optional
 
 from db.models import models
 from db.models import MovieSession, Movie, CinemaHall
@@ -9,20 +10,16 @@ def create_movie_session(
         movie_id: int,
         cinema_hall_id: int
 ) -> None:
-
-    movie_ = Movie.objects.get(id=movie_id)
-    hall = CinemaHall.objects.get(id=cinema_hall_id)
     MovieSession.objects.create(
         show_time=movie_show_time,
-        movie=movie_,
-        cinema_hall=hall
+        movie_id=movie_id,
+        cinema_hall_id=cinema_hall_id
     )
 
 
 def get_movies_sessions(
-        session_date: str = None
+        session_date: Optional[str] = None
 ) -> models.QuerySet:
-
     queryset = MovieSession.objects.all()
     if session_date:
         queryset = queryset.filter(show_time__date=session_date)
@@ -35,11 +32,10 @@ def get_movie_session_by_id(movie_session_id: int) -> models.QuerySet:
 
 def update_movie_session(
         session_id: int,
-        show_time: datetime.datetime = None,
-        movie_id: int = None,
-        cinema_hall_id: int = None
+        show_time: Optional[datetime.datetime] = None,
+        movie_id: Optional[int] = None,
+        cinema_hall_id: Optional[int] = None
 ) -> None:
-
     session = MovieSession.objects.get(id=session_id)
 
     if show_time:
