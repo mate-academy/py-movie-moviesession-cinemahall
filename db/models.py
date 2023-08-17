9585204a7508ad5f5ed77f657cae5e632d42942a
laskarj <koks.adekvat@gmail.com>
2023-08-17 11:42:55 +0200

    Models fixed

--- db/models.py
+++ db/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
-# (1)
+
 class Genre(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
     def __str__(self) -> str:
         return self.name
 
-# (1)
+
 class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
@@ -15,7 +15,7 @@ class Actor(models.Model):
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
 
-#  4 -> (2) < -- > 1, 1
+
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
@@ -25,7 +25,7 @@ class Movie(models.Model):
     def __str__(self) -> str:
         return self.title
 
-# (3) <- 4
+
 class CinemaHall(models.Model):
     name = models.CharField(max_length=255)
     rows = models.IntegerField()
@@ -39,11 +39,18 @@ class CinemaHall(models.Model):
         return self.rows * self.seats_in_row
 
 
-# 3 <- (4) -> 2
 class MovieSession(models.Model):
     show_time = models.DateTimeField()
-    cinema_hall = models.ForeignKey(CinemaHall, related_name="halls", on_delete=models.CASCADE)
-    movie = models.ForeignKey(Movie, related_name="movies", on_delete=models.CASCADE)
+    cinema_hall = models.ForeignKey(
+        CinemaHall,
+        related_name="halls",
+        on_delete=models.CASCADE
+    )
+    movie = models.ForeignKey(
+        Movie,
+        related_name="movies",
+        on_delete=models.CASCADE
+    )
 
     def __str__(self) -> str:
         return (f"{self.movie.title}"
