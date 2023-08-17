e9debf78530ce1ca1226cd0cfe49e8806357a788
laskarj <koks.adekvat@gmail.com>
2023-08-17 13:45:02 +0200

    Change to ", fixed delete_movie_session_by_id

--- services/movie_session.py
+++ services/movie_session.py
@@ -52,5 +52,5 @@ def update_movie_session(
 
 
 def delete_movie_session_by_id(session_id: int) -> None:
-    delete_session = MovieSession.objects.get(id=session_id)
+    delete_session = get_movie_session_by_id(session_id)
     delete_session.delete()
