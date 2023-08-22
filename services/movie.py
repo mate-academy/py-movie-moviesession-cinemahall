15c2391e828587173e2f2c2a3be50b55334f702e
Veinmax <aturjust88@gmail.com>
2023-08-22 19:45:08 +0300

    Solution3

--- services/movie.py
+++ services/movie.py
@@ -1,10 +1,11 @@
 from db.models import Movie
 from django.db.models import QuerySet
+from typing import Optional
 
 
 def get_movies(
-        genres_ids: list[int] = None,
-        actors_ids: list[int] = None
+        genres_ids: Optional[list[int]] = None,
+        actors_ids: Optional[list[int]] = None
 ) -> QuerySet:
 
     queryset = Movie.objects.all()
@@ -13,10 +14,10 @@ def get_movies(
             genres__id__in=genres_ids
         ).filter(actors__id__in=actors_ids)
 
-    if actors_ids is None and genres_ids:
+    elif genres_ids:
         queryset = queryset.filter(genres__id__in=genres_ids)
 
-    if genres_ids is None and actors_ids:
+    elif actors_ids:
         queryset = queryset.filter(actors__id__in=actors_ids)
 
     return queryset
@@ -29,8 +30,8 @@ def get_movie_by_id(movie_id: int) -> Movie:
 def create_movie(
         movie_title: str,
         movie_description: str,
-        genres_ids: list[int] = None,
-        actors_ids: list[int] = None
+        genres_ids: Optional[list[int]] = None,
+        actors_ids: Optional[list[int]] = None
 ) -> None:
     movie = Movie.objects.create(
         title=movie_title,
--- services/movie_session.py
+++ services/movie_session.py
@@ -1,6 +1,7 @@
-import datetime
+from datetime import datetime
 from db.models import MovieSession
 from django.db.models import QuerySet
+from typing import Optional
 
 
 def create_movie_session(
@@ -17,7 +18,7 @@ def create_movie_session(
 
 
 def get_movies_sessions(
-        session_date: str = None
+        session_date: Optional[str] = None
 ) -> QuerySet:
     queryset = MovieSession.objects.all()
 
@@ -29,23 +30,21 @@ def get_movies_sessions(
 
 def update_movie_session(
         session_id: int,
-        show_time: str = None,
-        movie_id: int = None,
-        cinema_hall_id: int = None
+        show_time: Optional[str] = None,
+        movie_id: Optional[int] = None,
+        cinema_hall_id: Optional[int] = None
 ) -> None:
     movie_session = MovieSession.objects.filter(id=session_id)
-    if show_time:
-        movie_session.update(
-            show_time=show_time
-        )
-    if movie_id:
-        movie_session.update(
-            movie_id=movie_id
-        )
-    if cinema_hall_id:
-        movie_session.update(
-            cinema_hall_id=cinema_hall_id
-        )
+    update_fields = {}
+
+    if show_time is not None:
+        update_fields["show_time"] = show_time
+    if movie_id is not None:
+        update_fields["movie_id"] = movie_id
+    if cinema_hall_id is not None:
+        update_fields["cinema_hall_id"] = cinema_hall_id
+
+    movie_session.update(**update_fields)
 
 
 def get_movie_session_by_id(
@@ -57,5 +56,5 @@ def get_movie_session_by_id(
 def delete_movie_session_by_id(
         session_id: int
 ) -> None:
-    movie_session = MovieSession.objects.get(id=session_id)
+    movie_session = get_movie_session_by_id(session_id)
     movie_session.delete()
