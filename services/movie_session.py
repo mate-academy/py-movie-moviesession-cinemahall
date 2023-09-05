526a4f43ab87f6377f74e689f5c1b23560d49673
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-09-05 12:25:30 +0300

    Solution

---
+++ services/movie_session.py
@@ -0,0 +1,66 @@
+import init_django_orm  # noqa
+from django.db.models import QuerySet
+
+from db.models import MovieSession
+
+
+def create_movie_session(
+    movie_show_time: str, movie_id: int, cinema_hall_id: int
+) -> QuerySet[MovieSession]:
+    movie_session = MovieSession.objects.create(
+        show_time=movie_show_time,
+        cinema_hall_id=cinema_hall_id,
+        movie_id=movie_id,
+    )
+
+    return movie_session
+
+
+def get_movies_sessions(session_date: str = None) -> QuerySet[MovieSession]:
+    queryset = MovieSession.objects.all()
+
+    if session_date:
+        queryset = queryset.filter(show_time__date=session_date)
+
+    return queryset
+
+
+def get_movie_session_by_id(movie_session_id: int) -> QuerySet[MovieSession]:
+    movie_session = MovieSession.objects.get(id=movie_session_id)
+
+    return movie_session
+
+
+def update_movie_session(
+    session_id: int,
+    show_time: str = None,
+    movie_id: int = None,
+    cinema_hall_id: int = None,
+) -> QuerySet[MovieSession]:
+    updated = []
+
+    session = MovieSession.objects.get(id=session_id)
+
+    if show_time:
+        session.show_time = show_time
+        updated.append(f"Updated show time: {show_time}")
+
+    if movie_id:
+        session.movie_id = movie_id
+        updated.append(f"Updated movie id: {movie_id}")
+
+    if cinema_hall_id:
+        session.cinema_hall_id = cinema_hall_id
+        updated.append(f"Updated cinema hall: {cinema_hall_id}")
+
+    for update in updated:
+        print(update)
+
+    session.save()
+
+    return session
+
+
+def delete_movie_session_by_id(session_id: int) -> None:
+    MovieSession.objects.get(id=session_id).delete()
+    print("Movie session was deleted!")
