ce335b5496b161f535f0f3941c4a08e73af5f9c9
Kavbian <kavbian@gmail.com>
2023-09-06 22:24:29 +0300

    func_related_name

--- services/movie_session.py
+++ services/movie_session.py
@@ -31,7 +31,7 @@ def update_movie_session(
         movie_id: int = None,
         cinema_hall_id: int = None
 ) -> None:
-    movie_session = MovieSession.objects.get(id=session_id)
+    movie_session = get_movie_session_by_id(session_id)
 
     if show_time is not None:
         movie_session.show_time = show_time
@@ -46,4 +46,4 @@ def update_movie_session(
 
 
 def delete_movie_session_by_id(session_id: int) -> None:
-    MovieSession.objects.filter(id=session_id).delete()
+    get_movie_session_by_id(session_id).delete()
