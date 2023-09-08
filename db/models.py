f0a58d7be8e9cc657df8a1f70ebd7459090b3c2e
Olexander Bryl <edlrian814@gmail.com>
2023-09-08 12:47:17 +0300

    solution

--- db/models.py
+++ db/models.py
@@ -5,7 +5,7 @@ class Genre(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
     def __str__(self) -> str:
-        return self.name
+        return f"{self.name}"
 
 
 class Actor(models.Model):
@@ -14,3 +14,46 @@ class Actor(models.Model):
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    actors = models.ManyToManyField(Actor, related_name="movies")
+    genres = models.ManyToManyField(Genre, related_name="movies")
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+
+class MovieSession(models.Model):
+    show_time = models.DateTimeField()
+    cinema_hall = models.ForeignKey(
+        CinemaHall,
+        on_delete=models.SET_NULL,
+        related_name="sessions",
+        null=True
+    )
+    movie = models.ForeignKey(
+        Movie,
+        on_delete=models.SET_NULL,
+        related_name="sessions",
+        null=True
+    )
+
+    def __str__(self) -> str:
+        timestamp = self.show_time.strftime("%Y-%m-%d %H:%M:%S")
+        return f"{self.movie.title} {timestamp}"
