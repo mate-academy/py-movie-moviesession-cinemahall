0ceaf1dc1547a4876f09380315c78cfe85396734
Tanya Burdenko <burdenko.tetyana@gmail.com>
2023-09-10 16:51:16 +0300

    Solution 2

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,9 +1,15 @@
 from datetime import datetime, date
 
+from django.db.models import QuerySet
+
 from db.models import MovieSession
 
 
-def create_movie_session(movie_show_time: datetime, movie_id: int, cinema_hall_id: int):
+def create_movie_session(
+        movie_show_time: datetime,
+        movie_id: int,
+        cinema_hall_id: int
+) -> QuerySet:
     return MovieSession.objects.create(
         show_time=movie_show_time,
         movie_id=movie_id,
@@ -11,13 +17,13 @@ def create_movie_session(movie_show_time: datetime, movie_id: int, cinema_hall_i
     )
 
 
-def get_movies_sessions(session_date: date = None):
+def get_movies_sessions(session_date: date = None) -> QuerySet:
     if session_date is not None:
         return MovieSession.objects.filter(show_time__date=session_date)
     return MovieSession.objects.all()
 
 
-def get_movie_session_by_id(movie_session_id: int):
+def get_movie_session_by_id(movie_session_id: int) -> QuerySet:
     return MovieSession.objects.get(id=movie_session_id)
 
 
@@ -26,7 +32,7 @@ def update_movie_session(
         show_time: datetime = None,
         movie_id: int = None,
         cinema_hall_id: int = None
-):
+) -> None:
     session = MovieSession.objects.filter(id=session_id)
     if show_time is not None:
         session.update(show_time=show_time)
@@ -36,12 +42,5 @@ def update_movie_session(
         session.update(cinema_hall_id=cinema_hall_id)
 
 
-def delete_movie_session_by_id(session_id: int):
+def delete_movie_session_by_id(session_id: int) -> None:
     MovieSession.objects.filter(id=session_id).delete()
-
-
-print(create_movie_session(
-    datetime(2023, 9, 13, 19, 30, 0),
-    6,
-    4
-))
--- settings.py
+++ settings.py
@@ -11,7 +11,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "movie_sessions.sqlite3"),
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
@@ -21,6 +21,6 @@ TIME_ZONE = "Europe/Kiev"
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
-INSTALLED_APPS = ("db", )
+INSTALLED_APPS = ("db",)
