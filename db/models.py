a0aa7d94608d44cecb14f2433093c74715951866
CaCtUs1k <denischernish19012004@gmail.com>
2023-09-11 18:19:15 +0300

    Solution

--- db/models.py
+++ db/models.py
@@ -23,7 +23,7 @@ class Movie(models.Model):
     genres = models.ManyToManyField(Genre, related_name="movies")
 
     def __str__(self) -> str:
-        return f"{self.title}"
+        return self.title
 
 
 class CinemaHall(models.Model):
@@ -36,7 +36,7 @@ class CinemaHall(models.Model):
         return self.rows * self.seats_in_row
 
     def __str__(self) -> str:
-        return f"{self.name}"
+        return self.name
 
 
 class MovieSession(models.Model):
