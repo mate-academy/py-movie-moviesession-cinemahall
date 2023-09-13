5d210d18a9ad16e1b70a52b45019cbdea0fc5beb
Kateryna Zadorozhna <136272476+katryana@users.noreply.github.com>
2023-09-13 23:07:42 +0300

    Update movie.py

--- services/movie.py
+++ services/movie.py
@@ -7,16 +7,13 @@ def get_movies(
         genres_ids: list = None,
         actors_ids: list = None
 ) -> QuerySet:
-    if genres_ids is None:
-        if actors_ids is None:
-            return Movie.objects.all()
-        return Movie.objects.filter(actors__id__in=actors_ids)
-    if actors_ids is None:
-        return Movie.objects.filter(genres__id__in=genres_ids)
-    return Movie.objects.filter(
-        actors__id__in=actors_ids,
-        genres__id__in=genres_ids
-    )
+    queryset = Movie.objects.all()
+    if genres_ids is not None:
+        queryset = queryset.filter(genres__id__in=genres_ids)
+    if actors_ids is not None:
+        queryset = queryset.filter(actors__id__in=actors_ids)
+        
+    return queryset
 
 
 def get_movie_by_id(movie_id: int) -> Movie:
