187db215390ede95ed3e6d67faed3a1e48432c47
Viktor Deyneka <3lobites@gmail.com>
2023-09-25 21:22:18 +0300

    work230925_2

--- services/movie.py
+++ services/movie.py
@@ -5,23 +5,31 @@ from db.models import Genre, Actor, Movie
 
 def get_movies(genders_ids: List[Genre] = None, actors_ids: List[Actor] = None) -> List[Movie]:
     uuu = 0
-    list_movies = []
-    if not genders_ids and not actors_ids:
-        for movie in Movie.objects.all():
-#            print(movie)
-            list_movies.append(movie)
+    queryset = Movie.objects.all()
 
-    if genders_ids and actors_ids:
-        pass
-
-    return list_movies
+    if genders_ids is None and actors_ids is None:
+        return queryset
+    if genders_ids is not None and actors_ids is not None:
+        queryset = queryset.filter(genders__id__in=genders_ids, actors__id_in=actors_ids)
+        return queryset
+    if genders_ids is not None and actors_ids is None:
+        queryset = queryset.filter(genders__id__in=genders_ids)
+        return queryset
+    if actors_ids is not None and genders_ids is None:
+        queryset = queryset.filter( actors__id_in=actors_ids)
+        return queryset
 
 
 def get_movie_by_id(movie_id: int) -> Movie:
-    pass
+    return Movie.objects.get(__id=movie_id)
 
 
 def create_movie(movie_title: str, movie_description: str, genres_ids: List[Genre] = None,
                  actors_ids: List[Actor] = None) -> None:
-    pass
+    new_movie = Movie.objects.create(title=movie_title, description=movie_description,)
+    if genres_ids:
+        new_movie.genres.set(genres_ids)
+    if actors_ids:
+        new_movie.actors.set(actors_ids)
 
+    return new_movie
