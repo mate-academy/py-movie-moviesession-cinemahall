c5b9196648c48d0bbbc797f3419d8776ee98f856
3lobites <3lobites@gmail.com>
2023-09-26 16:58:15 +0300

    work20230926

--- services/movie_session.py
+++ services/movie_session.py
@@ -5,22 +5,34 @@ import datetime
 
 
 def create_movie_session(movie_show_time: datetime, movie_id: Movie, cinema_hall_id: CinemaHall) -> None:
-    pass
+    new_movie_session = MovieSession.objects.create(show_time=movie_show_time, movie=movie_id, cinema_hall=cinema_hall_id)
+    return new_movie_session
 
 
 # "year-month-day"
 def get_movies_sessions(session_date: str = None) -> List[MovieSession]:
-    pass
+    queryset = MovieSession.objects.all()
+    if session_date is None:
+        return queryset
+    queryset = queryset.filter(show_time=session_date)  # только тут фильтр по дате?
+
+    return queryset
 
 
 def get_movie_session_by_id(movie_session_id: MovieSession) -> Movie:
-    pass
+    return MovieSession.objects.get(__id=movie_session_id)
 
 
 def update_movie_session(session_id: MovieSession, show_time: datetime = None, movie_id: Movie = None,
                          cinema_hall_id: CinemaHall = None) -> None:
-    pass
+    # Person.objects.filter(id=1).update(name="Mike")
+    update_movies_session = MovieSession.objects.filter(__id=session_id)
+    update_movies_session.update(show_time=show_time,)
+    if movie_id is not None:
+        update_movies_session.movies.set(movie_id)
+    if cinema_hall_id is not None:
+        update_movies_session.cinema_hall.set(cinema_hall_id)
 
 
 def delete_movie_session_by_id(session_id: MovieSession) -> None:
-    pass
+    MovieSession.objects.delete(__id=session_id)
