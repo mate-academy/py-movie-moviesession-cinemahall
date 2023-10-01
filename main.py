86f7faa20a9bcfe25f1343783641ddb71ecb3cad
Anastasia <anastasiiya.p@gmail.com>
2023-10-01 12:47:33 +0300

    Fixed

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
 import init_django_orm  # noqa: F401
-# import datetime
-# from services import (
-#     movie as movie_service,
-#     cinema_hall as hall_service,
-#     movie_session as session_service
-# )
