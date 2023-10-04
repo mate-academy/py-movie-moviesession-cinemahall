0031b78dc2d0ee65bc855e64838aae854a027bc3
serhii_loboda <serhiy.loboda@gmail.com>
2023-10-04 20:29:31 +0300

    Solution: Movie, CinemaHall, and MovieSession.

---
+++ services/movie.py
@@ -0,0 +1,44 @@
+from typing import List
+
+from django.db.models import QuerySet
+
+from db.models import Movie
+
+
+def get_movies(
+        actors_ids: List[int] = None,
+        genres_ids: List[int] = None
+) -> QuerySet:
+    movie_set = Movie.objects.all()
+
+    if actors_ids is not None:
+        movie_set = movie_set.filter(actors__id__in=actors_ids)
+
+    if genres_ids is not None:
+        movie_set = movie_set.filter(genres__id__in=genres_ids)
+
+    return movie_set
+
+
+def get_movie_by_id(movie_id: int) -> Movie:
+    return Movie.objects.get(id=movie_id)
+
+
+def create_movie(
+        movie_title: str,
+        movie_description: str,
+        genres_ids: List[int] = None,
+        actors_ids: List[int] = None
+) -> Movie:
+    new_movie = Movie.objects.create(
+        title=movie_title,
+        description=movie_description
+    )
+
+    if genres_ids is not None:
+        new_movie.genres.set(genres_ids)
+
+    if actors_ids is not None:
+        new_movie.actors.set(actors_ids)
+
+    return new_movie
