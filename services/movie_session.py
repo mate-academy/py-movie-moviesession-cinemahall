14926d1133ca6158884cfce67fbd331037d25ca5
Ruslan <rusipbox@gmail.com>
2023-10-15 14:12:31 +0300

    Test3(py-movie-moviesession-cinemahall)

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,4 +1,4 @@
-from db.models import MovieSession
+from db.models import MovieSession, Movie, CinemaHall
 
 
 def create_movie_session(
@@ -6,10 +6,12 @@ def create_movie_session(
         movie_id: int,
         cinema_hall_id: int
 ) -> None:
+    movie = Movie.objects.get(id=movie_id)
+    cinema_hall = CinemaHall.objects.get(id=cinema_hall_id)
     MovieSession.objects.create(
         show_time=movie_show_time,
-        cinema_hall=cinema_hall_id,
-        movie=movie_id
+        cinema_hall=cinema_hall,
+        movie=movie
     )
 
 
