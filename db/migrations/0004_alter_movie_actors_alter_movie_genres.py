a29caffbb517f9d167d987c001241a992b852d83
Dmytro Kucherenko <dimakucherenko91@gmail.com>
2023-10-16 16:27:38 +0300

    'Solution'

--- db/migrations/0004_alter_movie_actors_alter_movie_genres.py
+++ db/migrations/0004_alter_movie_actors_alter_movie_genres.py
@@ -4,20 +4,19 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('db', '0003_alter_moviesession_cinema_hall_and_more'),
+        ("db", "0003_alter_moviesession_cinema_hall_and_more"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='movie',
-            name='actors',
-            field=models.ManyToManyField(related_name='actors', to='db.Actor'),
+            model_name="movie",
+            name="actors",
+            field=models.ManyToManyField(related_name="actors", to="db.Actor"),
         ),
         migrations.AlterField(
-            model_name='movie',
-            name='genres',
-            field=models.ManyToManyField(related_name='genres', to='db.Genre'),
+            model_name="movie",
+            name="genres",
+            field=models.ManyToManyField(related_name="genres", to="db.Genre"),
         ),
     ]
--- db/migrations/0005_alter_movie_actors.py
+++ db/migrations/0005_alter_movie_actors.py
@@ -4,15 +4,14 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('db', '0004_alter_movie_actors_alter_movie_genres'),
+        ("db", "0004_alter_movie_actors_alter_movie_genres"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='movie',
-            name='actors',
-            field=models.ManyToManyField(related_name='movies', to='db.Actor'),
+            model_name="movie",
+            name="actors",
+            field=models.ManyToManyField(related_name="movies", to="db.Actor"),
         ),
     ]
