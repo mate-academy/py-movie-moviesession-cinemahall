d64971de028c45e669f44376fa18f203cb268b2f
Ruslan <rusipbox@gmail.com>
2023-10-17 18:29:14 +0300

    Test5(py-movie-moviesession-cinemahall)

--- services/movie.py
+++ services/movie.py
@@ -1,10 +1,12 @@
+from django.db.models import QuerySet
+
 from db.models import Movie, Genre, Actor
 
 
 def get_movies(
         genres_ids: list[int] = None,
         actors_ids: list[int] = None
-) -> list[int]:
+) -> QuerySet:
     queryset = Movie.objects.all()
 
     if genres_ids:
@@ -16,7 +18,7 @@ def get_movies(
     return queryset
 
 
-def get_movie_by_id(movie_id: int) -> int:
+def get_movie_by_id(movie_id: int) -> Movie:
     return Movie.objects.get(id=movie_id)
 
 
@@ -25,15 +27,15 @@ def create_movie(
         movie_description: str,
         genres_ids: list[int] = None,
         actors_ids: list[int] = None
-) -> None:
+) -> Movie:
     movie = Movie.objects.create(
         title=movie_title,
         description=movie_description
     )
     if genres_ids:
         genres = Genre.objects.filter(id__in=genres_ids)
-        movie.genres.add(*genres)
+        movie.genres.set(genres)
     if actors_ids:
         actors = Actor.objects.filter(id__in=actors_ids)
-        movie.actors.add(*actors)
+        movie.actors.set(actors)
     return movie
