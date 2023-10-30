c44eb6db51c624a90419c88361dbc815f017333a
Dmytro Kolodii <dimakolodiy.ua@gmail.com>
2023-10-30 13:56:30 +0200

    Sol2

--- services/movie_session.py
+++ services/movie_session.py
@@ -27,26 +27,14 @@ def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
     return MovieSession.objects.get(id=movie_session_id)
 
 
-def update_movie_session(
-        session_id: int,
-        show_time: datetime = None,
-        movie_id: int = None,
-        cinema_hall_id: int = None
-) -> MovieSession:
+def update_movie_session(session_id: int, **kwargs) -> MovieSession:
     movie_session = MovieSession.objects.get(id=session_id)
-
-    if show_time is not None:
-        movie_session.show_time = show_time
-
-    if movie_id is not None:
-        movie_session.movie_id = movie_id
-
-    if cinema_hall_id is not None:
-        movie_session.cinema_hall_id = cinema_hall_id
-
+    for key, value in kwargs.items():
+        if hasattr(movie_session, key):
+            setattr(movie_session, key, value)
     movie_session.save()
     return movie_session
 
 
 def delete_movie_session_by_id(session_id: int) -> None:
-    MovieSession.objects.get(id=session_id).delete()
+    MovieSession.objects.get_movie_session_by_id(session_id).delete()
