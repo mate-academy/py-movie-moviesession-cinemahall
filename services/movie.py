95d9ae68d90955d61a3ad8e36e2e36496e85c6d5
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2023-11-02 15:29:26 +0200

    s2

--- services/movie.py
+++ services/movie.py
@@ -6,16 +6,24 @@ from db.models import Movie
 
 
 def get_movies(
-        genres_ids: Optional[List[int]] = None,
-        actors_ids: Optional[List[int]] = None
+        genres_ids: list[int] = None,
+        actors_ids: list[int] = None
 ) -> QuerySet:
-
-    movies = Movie.objects.all()
-    if genres_ids:
-        movies = movies.filter(genres__in=genres_ids)
-    if actors_ids:
-        movies = movies.filter(actors__in=actors_ids)
-    return movies
+    queryset = Movie.objects.all()
+    if genres_ids and actors_ids:
+        queryset = queryset.filter(
+            actors__id__in=actors_ids,
+            genres__id__in=genres_ids
+        )
+    elif actors_ids is not None:
+        queryset = queryset.filter(
+            actors__id__in=actors_ids
+        )
+    elif genres_ids is not None:
+        queryset = queryset.filter(
+            genres__id__in=genres_ids
+        )
+    return queryset
 
 
 def get_movie_by_id(movie_id: int) -> Movie:
