882c52183bec1978a7695ea548de9dbb47e684c2
Lytillis <chemurchem@gmail.com>
2023-11-06 19:26:24 +0200

    Add related_names, remove redundant code

--- services/movie.py
+++ services/movie.py
@@ -3,14 +3,11 @@ from db.models import Movie
 
 def get_movies(genres_ids: list[int] = None,
                actors_ids: list[int] = None) -> list[Movie]:
-    if genres_ids and actors_ids:
-        return Movie.objects.filter(genres__id__in=genres_ids,
-                                    actors__id__in=actors_ids)
     movies = Movie.objects.all()
     if genres_ids:
-        return movies.filter(genres__id__in=genres_ids)
+        movies = movies.filter(genres__id__in=genres_ids)
     if actors_ids:
-        return movies.filter(actors__id__in=actors_ids)
+        movies = movies.filter(actors__id__in=actors_ids)
     return movies
 
 
@@ -27,6 +24,6 @@ def create_movie(movie_title: str,
         description=movie_description
     )
     if genres_ids:
-        movie.genres.add(*genres_ids)
+        movie.genres.set(genres_ids)
     if actors_ids:
-        movie.actors.add(*actors_ids)
+        movie.actors.set(actors_ids)
