c8b9bfd501a9ecc4c14a14d98c20beb75dd86956
mykolamateichuk <mateichukmykola@gmail.com>
2023-11-07 17:11:15 +0200

    Solution_v2

--- services/movie.py
+++ services/movie.py
@@ -6,16 +6,16 @@ def get_movies(genres_ids: list[int] = None,
                actors_ids: list[int] = None) -> QuerySet:
     queryset = Movie.objects.all()
 
-    if genres_ids is not None:
+    if genres_ids:
         queryset = queryset.filter(genres__id__in=genres_ids)
 
-    if actors_ids is not None:
+    if actors_ids:
         queryset = queryset.filter(actors__id__in=actors_ids)
 
     return queryset
 
 
-def get_movie_by_id(movie_id: int) -> QuerySet:
+def get_movie_by_id(movie_id: int) -> Movie:
     return Movie.objects.get(id=movie_id)
 
 
--- services/movie_session.py
+++ services/movie_session.py
@@ -17,7 +17,7 @@ def create_movie_session(movie_show_time: datetime,
 def get_movies_sessions(session_date: str = None) -> QuerySet:
     queryset = MovieSession.objects.all()
 
-    if session_date is not None:
+    if session_date:
         queryset = queryset.filter(
             show_time__date=session_date
         )
@@ -25,7 +25,7 @@ def get_movies_sessions(session_date: str = None) -> QuerySet:
     return queryset
 
 
-def get_movie_session_by_id(movie_session_id: int) -> QuerySet:
+def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
     return MovieSession.objects.get(id=movie_session_id)
 
 
@@ -35,13 +35,13 @@ def update_movie_session(session_id: int,
                          cinema_hall_id: int = None) -> None:
     queryset = MovieSession.objects.filter(id=session_id)
 
-    if show_time is not None:
+    if show_time:
         queryset.update(show_time=show_time)
 
-    if movie_id is not None:
+    if movie_id:
         queryset.update(movie_id=movie_id)
 
-    if cinema_hall_id is not None:
+    if cinema_hall_id:
         queryset.update(cinema_hall_id=cinema_hall_id)
 
 
