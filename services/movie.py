ac23225f1702322dcc8a38d20f312a96ca270527
Katherine-Greg <katiasimpson17@gmail.com>
2023-11-14 17:19:57 +0200

    Solution

--- services/movie.py
+++ services/movie.py
@@ -7,14 +7,10 @@ def get_movies(genres_ids: list[int] = None,
                actors_ids: list[int] = None) -> QuerySet[Movie]:
     movies = Movie.objects.all()
 
-    if genres_ids and actors_ids:
-        return movies.filter(
-            genres__id__in=genres_ids
-        ).filter(actors__id__in=actors_ids)
-    if genres_ids:
-        return movies.filter(genres__in=genres_ids)
-    if actors_ids:
-        return movies.filter(actors__in=actors_ids)
+    if genres_ids is not None:
+        movies = movies.filter(genres__in=genres_ids)
+    if actors_ids is not None:
+        movies = movies.filter(actors__in=actors_ids)
     return movies
 
 
@@ -25,7 +21,7 @@ def get_movie_by_id(movie_id: int) -> QuerySet[Movie]:
 def create_movie(movie_title: str,
                  movie_description: str,
                  genres_ids: list[int] = None,
-                 actors_ids: list[int] = None) -> QuerySet[Movie]:
+                 actors_ids: list[int] = None) -> None:
     movie = Movie.objects.create(
         title=movie_title,
         description=movie_description,
@@ -35,4 +31,3 @@ def create_movie(movie_title: str,
         movie.genres.set(genres_ids)
     if actors_ids:
         movie.actors.set(actors_ids)
-    return movie
