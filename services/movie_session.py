a2b47ffba9fdea1a968cca33273c89d3e2d2b319
Vadym Tiron <tironvadim1@gmail.com>
2023-11-27 19:54:40 +0200

    migrations added

--- services/movie_session.py
+++ services/movie_session.py
@@ -27,7 +27,7 @@ def get_movies_sessions(
     return queryset
 
 
-def get_movie_session_by_id(movie_session_id: int) -> QuerySet[MovieSession]:
+def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
     return MovieSession.objects.get(id=movie_session_id)
 
 
