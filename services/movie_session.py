3626f55881d9b4b7746030c25c932ff6659db782
Denis Dymerlii <dum.denya15@gmail.com>
2023-11-30 11:49:54 +0200

    Fixed flake errors

--- services/movie_session.py
+++ services/movie_session.py
@@ -9,7 +9,7 @@ def create_movie_session(
         movie_id: int,
         cinema_hall_id: int
 ) -> None:
-    movie_session = MovieSession.objects.create(
+    MovieSession.objects.create(
         show_time=movie_show_time,
         movie_id=movie_id,
         cinema_hall_id=cinema_hall_id
