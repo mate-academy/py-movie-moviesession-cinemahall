cade4843f1737bab4ee09a2f90c8897aa50dd1ba
Olena Lisova <lisovausa@gmail.com>
2023-12-01 13:58:51 -0500

    Done movie moviesession

---
+++ services/movie.py
@@ -0,0 +1,33 @@
+from db.models import Genre, Actor, Movie
+
+
+def get_movies(genres_ids: list[Genre] = None,
+               actors_ids: list[Actor] = None) -> Movie:
+    queryset = Movie.objects.all()
+
+    if genres_ids is not None:
+        queryset = queryset.filter(genres__id__in=genres_ids)
+
+    if actors_ids is not None:
+        queryset = queryset.filter(actors__id__in=actors_ids)
+
+    return queryset
+
+
+def get_movie_by_id(movie_id: int) -> Movie:
+    return Movie.objects.get(id=movie_id)
+
+
+def create_movie(movie_title: str, movie_description: str,
+                 genres_ids: list[int] = None,
+                 actors_ids: list[int] = None
+                 ) -> Movie:
+    new_movie = Movie.objects.create(title=movie_title,
+                                     description=movie_description)
+    if genres_ids:
+        new_movie.genres.set(genres_ids)
+
+    if actors_ids:
+        new_movie.actors.set(actors_ids)
+
+    return new_movie
