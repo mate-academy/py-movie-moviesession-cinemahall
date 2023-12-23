b22a6ca5acbe6ea37e1356f2af0a18b77b381542
Volodymyr <elohimwz@gmail.com>
2023-12-23 21:00:14 +0200

    I have problems and I don't see solutions. Help, explain, please

--- db/models.py
+++ db/models.py
@@ -14,3 +14,41 @@ class Actor(models.Model):
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    actors = models.ManyToManyField(Actor)
+    genres = models.ManyToManyField(Genre)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+
+class MovieSession(models.Model):
+    show_time = models.DateTimeField(null=True)
+    cinema_hall = models.ForeignKey(
+        CinemaHall,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    movie = models.ForeignKey(
+        Movie,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+    def __str(self) -> str:
+        return f"{self.movie.title} " \
+               f"{self.show_time.strftime('%Y-%m-%d %H:%M:%S')}"
