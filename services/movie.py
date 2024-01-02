eb327d65fe0ccfc12c2edeb25b29764fe16abaef
Alla Kuksa <alla.kuksa92@gmail.com>
2024-01-02 13:52:03 +0200

    Changed mistakes 2

--- services/movie.py
+++ services/movie.py
@@ -11,18 +11,11 @@ def get_movies(
 
     query_set = Movie.objects.all()
 
-    if genres_ids is None and actors_ids is None:
-        return query_set
-    if genres_ids and actors_ids:
-        query_set = query_set.filter(
-            actors__id__in=actors_ids,
-            genres__id__in=genres_ids
-        )
-    if genres_ids is None and actors_ids:
+    if actors_ids:
         query_set = query_set.filter(
             actors__id__in=actors_ids
         )
-    if actors_ids is None and genres_ids:
+    if genres_ids:
         query_set = query_set.filter(
             genres__id__in=genres_ids
         )
@@ -46,8 +39,8 @@ def create_movie(
         description=movie_description
     )
     if actors_ids:
-        new_movie.actors.add(*actors_ids)
+        new_movie.actors.set(actors_ids)
     if genres_ids:
-        new_movie.genres.add(*genres_ids)
+        new_movie.genres.set(genres_ids)
 
     return new_movie
