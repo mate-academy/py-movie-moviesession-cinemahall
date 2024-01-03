4843b9142dc0c44e3539f5e57f249573176f1234
Anton Tkachuk <tkacuk2291@gmail.com>
2024-01-03 17:43:07 +0200

    Solutions_editsa

--- services/movie.py
+++ services/movie.py
@@ -2,35 +2,35 @@ from typing import List, Optional
 from db.models import Movie
 
 
+
 def get_movies(
-    genres_ids: List[int] = None, actors_ids: List[int] = None
+        genres_ids: List[int] = None, actors_ids: List[int] = None
 ) -> List[Movie]:
-    query_set = Movie.objects.all()
+    queryset = Movie.objects.all()
 
     if genres_ids is not None:
-        query_set = query_set.filter(genres__id__in=genres_ids)
+        queryset = queryset.filter(genres__id__in=genres_ids)
 
     if actors_ids is not None:
-        query_set = query_set.filter(actors__id__in=actors_ids)
+        queryset = queryset.filter(actors__id__in=actors_ids)
 
-    return query_set
+    return queryset
 
 
 def get_movie_by_id(movie_id: int = None) -> Optional[Movie]:
-    query_set = Movie.objects.all().get(pk=movie_id)
+    query_set = Movie.objects.get(pk=movie_id)
 
     return query_set
 
 
 def create_movie(
-    movie_title: str,
-    movie_description: str,
-    genres_ids: List[int] = None,
-    actors_ids: List[int] = None,
+        movie_title: str,
+        movie_description: str,
+        genres_ids: List[int] = None,
+        actors_ids: List[int] = None,
 ) -> Movie:
     movie = Movie.objects.create(title=movie_title,
-                                 description=movie_description
-                                 )
+                                 description=movie_description)
     if actors_ids:
         movie.actors.set(actors_ids)
     if genres_ids:
