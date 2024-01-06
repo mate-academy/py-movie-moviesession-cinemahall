b4efb59ca1ef1a9811cb1f01fe35239142b2fd1d
Volodymyr Kovalenko <380974261314kv@gmail.com>
2024-01-06 21:12:43 +0200

    added some changes: two empty lines, function reused

--- services/movie_session.py
+++ services/movie_session.py
@@ -37,15 +37,17 @@ def update_movie_session(
         cinema_hall_id: int = None
 ) -> MovieSession:
     movie_session = MovieSession.objects.get(id=session_id)
+
     if show_time:
         movie_session.show_time = show_time
     if movie_id:
         movie_session.movie = Movie.objects.get(id=movie_id)
     if cinema_hall_id:
         movie_session.cinema_hall = CinemaHall.objects.get(id=cinema_hall_id)
+
     movie_session.save()
     return movie_session
 
 
 def delete_movie_session_by_id(session_id: int) -> None:
-    MovieSession.objects.get(id=session_id).delete()
+    get_movie_session_by_id(session_id).delete()
