46f74dc9f4d1348567c4a9fca6f36d0bd2fd2c91
Maxim Voytkevichus <m4k51m15k4m@gmail.com>
2024-01-09 19:41:21 +0200

    Solution 2

--- settings.py
+++ settings.py
@@ -21,6 +21,7 @@ TIME_ZONE = "Europe/Kiev"
 
 USE_I18N = True
 
-USE_TZ = True  # SQLite backend does not support timezone-aware datetimes when USE_TZ is False.
+# SQLite backend does not support timezone-aware datetimes when USE_TZ is False
+USE_TZ = True
 
 INSTALLED_APPS = ("db",)
