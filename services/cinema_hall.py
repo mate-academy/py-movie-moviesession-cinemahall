b8f6a3c78ddbe066241a76462fa4e4fa1a9c9e8f
Ryshtun Denys <rishtund@gmail.com>
2024-01-25 14:18:44 +0200

    ADone

---
+++ services/cinema_hall.py
@@ -0,0 +1,22 @@
+from db.models import CinemaHall
+
+
+def get_cinema_halls():
+    return CinemaHall.objects.all()
+
+
+def create_cinema_hall(
+        hall_name: str,
+        hall_rows: int,
+        hall_seats_in_row: int
+    ) -> CinemaHall:
+    return CinemaHall.objects.create(
+        name=hall_name,
+        rows=hall_rows,
+        seats_in_row=hall_seats_in_row
+    )
+
+
+if __name__ == '__main__':
+    hall1 = create_cinema_hall("Japan", 7, 17)
+    print(hall1.name, hall1.capacity)
