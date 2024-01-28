4a3846971c3ee9eb93f7f0a6dc039e8c16dbe484
Vladyslav Bazhyn <147983482+VladyslavBazhyn@users.noreply.github.com>
2024-01-28 15:23:02 +0100

    Fix movie.py

--- services/movie.py
+++ services/movie.py
@@ -4,8 +4,8 @@ from db.models import Movie
 
 
 def get_movies(
-        genres_ids: list[int] = None,
-        actors_ids: list[int] = None
+        genres_ids: list[int] | None = None,
+        actors_ids: list[int] | None = None
 ) -> QuerySet:
 
     queryset = Movie.objects.all()
@@ -28,8 +28,8 @@ def get_movie_by_id(
 def create_movie(
         movie_title: str,
         movie_description: str,
-        genres_ids: list[int] = None,
-        actors_ids: list[int] = None
+        genres_ids: list[int] | None = None,
+        actors_ids: list[int] | None = None
 ) -> Movie:
 
     new_movie = Movie.objects.create(
