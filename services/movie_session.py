4e40c83cca4adb377dc42fe6d7a21158735f0904
Vlad <mr.darmstadtium@gmail.com>
2024-01-28 09:15:58 +0100

    flake8

--- services/movie_session.py
+++ services/movie_session.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from django.db.models import QuerySet
 
-from db.models import MovieSession, Movie, CinemaHall
+from db.models import MovieSession, Movie
 
 
 def create_movie_session(
