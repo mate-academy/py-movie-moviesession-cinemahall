1e633c1f39d64b6617c31c4ccef90abca5be280f
Vladyslav Bazhyn <147983482+VladyslavBazhyn@users.noreply.github.com>
2024-01-28 16:28:28 +0100

    dif

--- services/movie_session.py
+++ services/movie_session.py
@@ -19,7 +19,7 @@ def create_movie_session(
 
 
 def get_movies_sessions(
-    session_date: str | None = None,
+    session_date: str | none = None,
 ) -> QuerySet:
     current_session = MovieSession.objects.all()
 
@@ -42,9 +42,9 @@ def get_movie_session_by_id(
 
 def update_movie_session(
         session_id: int,
-        show_time: datetime.date | None = None,
-        movie_id: int | None = None,
-        cinema_hall_id: int | None = None,
+        show_time: datetime.date | none = None,
+        movie_id: int | none = None,
+        cinema_hall_id: int | none = None,
 
 ) -> None:
     movie_session = MovieSession.objects.get(
