29ba22168b62f58ee8d9f85b071960116b4de6e6
Artem Milchenko <pl301078map2@gmail.com>
2024-02-13 20:49:27 +0200

    fix comments

--- db/models.py
+++ db/models.py
@@ -42,9 +42,9 @@ class CinemaHall(models.Model):
 class MovieSession(models.Model):
     show_time = models.DateTimeField()
     cinema_hall = models.ForeignKey(CinemaHall, on_delete=models.CASCADE,
-                                    related_name="cinema_hall")
+                                    related_name="cinema_halls")
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE,
-                              related_name="movie")
+                              related_name="movies")
 
     def __str__(self) -> str:
         return f"{self.movie} {self.show_time}"
