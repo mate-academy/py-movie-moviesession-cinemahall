5103fd9ca743b2df4d19644a57cb8211138ccf7a
PythonZem <pythonzem@gmail.com>
2024-02-14 09:52:41 +0200

    Update movie_session.py

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,8 +1,8 @@
+import datetime
+
 from django.db.models import QuerySet
 
 from db.models import MovieSession
-
-import datetime
 
 
 def create_movie_session(
@@ -21,8 +21,10 @@ def get_movies_sessions(
         session_date: datetime.datetime | None = None
 ) -> QuerySet[MovieSession]:
     sessions_set = MovieSession.objects.all()
+
     if session_date:
         sessions_set = sessions_set.filter(show_time__date=session_date)
+
     return sessions_set
 
 
@@ -32,28 +34,22 @@ def get_movie_session_by_id(movie_id: int) -> MovieSession:
 
 def update_movie_session(
         session_id: int,
-        show_time: datetime = None,
-        movie_id: int = None,
-        cinema_hall_id: int = None
+        show_time: datetime.datetime | None = None,
+        movie_id: int | None = None,
+        cinema_hall_id: int | None = None
 ) -> None:
+    session = get_movie_session_by_id(session_id)
+
     if show_time:
-        MovieSession.objects.filter(
-            id=session_id
-        ).update(
-            show_time=show_time
-        )
+        session.show_time = show_time
+
     if movie_id:
-        MovieSession.objects.filter(
-            id=session_id
-        ).update(
-            movie_id=movie_id
-        )
+        session.movie_id = movie_id
+
     if cinema_hall_id:
-        MovieSession.objects.filter(
-            id=session_id
-        ).update(
-            cinema_hall_id=cinema_hall_id
-        )
+        session.cinema_hall_id = cinema_hall_id
+
+    session.save()
 
 
 def delete_movie_session_by_id(session_id: int) -> None:
