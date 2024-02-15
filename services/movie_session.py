3e8692585025c3e814a4fb8093ea84205861b3cd
Andrii Zubchenko <dissom1987@gmail.com>
2024-02-15 13:55:54 +0200

    Solution1

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,24 +1,23 @@
-# import operator
 from django.db.models import QuerySet
-from django.core.exceptions import ObjectDoesNotExist
-from db.models import MovieSession, CinemaHall, Movie
+from django.shortcuts import get_object_or_404
+from db.models import MovieSession
 from datetime import datetime
 
 
 def create_movie_session(
     movie_show_time: datetime, movie_id: int, cinema_hall_id: int
 ) -> MovieSession:
-    cinema_hall = CinemaHall.objects.get(id=cinema_hall_id)
-    movie = Movie.objects.get(id=movie_id)
 
     return MovieSession.objects.create(
-        show_time=movie_show_time, cinema_hall=cinema_hall, movie=movie
+        show_time=movie_show_time,
+        cinema_hall_id=cinema_hall_id,
+        movie_id=movie_id
     )
 
 
 def get_movies_sessions(
     session_date: str = None,
-) -> QuerySet[MovieSession] | MovieSession:
+) -> QuerySet[MovieSession]:
     movie_sessions = MovieSession.objects.all()
 
     if session_date:
@@ -32,13 +31,7 @@ def get_movies_sessions(
 
 
 def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
-    try:
-        return MovieSession.objects.get(id=movie_session_id)
-    except ObjectDoesNotExist as e:
-        raise e(
-            f"Movie session with specified ID: {movie_session_id} "
-            f"does not exist."
-        )
+    return get_object_or_404(MovieSession, id=movie_session_id)
 
 
 def update_movie_session(
@@ -48,8 +41,8 @@ def update_movie_session(
     cinema_hall_id: int = None,
 ) -> MovieSession:
 
-    if session_id:
-        movie_session = get_movie_session_by_id(session_id)
+    movie_session = get_movie_session_by_id(session_id)
+
     if show_time:
         movie_session.show_time = show_time
     if movie_id:
@@ -64,6 +57,6 @@ def update_movie_session(
 
 def delete_movie_session_by_id(session_id: int) -> None:
     try:
-        MovieSession.objects.filter(id=session_id).delete()
+        get_movie_session_by_id(session_id).delete()
     except MovieSession.DoesNotExist as e:
         raise e("Movie session does not exist.")
