1b29f1edd657a893c650ca9d1947fd5153cf4106
Artem Hryshko <artemggg2222@gmail.com>
2024-02-15 17:25:38 +0200

    Solution2

--- services/movie.py
+++ services/movie.py
@@ -1,33 +1,33 @@
+from django.db.models import QuerySet
+
 from db.models import Movie
 
 
-def get_movies(genres_ids: list = None, actors_ids: list = None) -> Movie:
-    if not genres_ids and not actors_ids:
-        return Movie.objects.all()
-
-    if genres_ids and actors_ids:
-        return Movie.objects.filter(
-            genres__id__in=genres_ids,
-            actors__id__in=actors_ids
-        )
+def get_movies(
+        genres_ids: list[int] | None = None,
+        actors_ids: list[int] | None = None
+) -> QuerySet[Movie]:
+    movie_all_objects = Movie.objects.all()
 
     if genres_ids:
-        return Movie.objects.filter(genres__id__in=genres_ids)
+        movie_all_objects = movie_all_objects.filter(genres__id__in=genres_ids)
 
     if actors_ids:
-        return Movie.objects.filter(actors__id__in=actors_ids)
+        movie_all_objects = movie_all_objects.filter(actors__id__in=actors_ids)
+
+    return movie_all_objects
 
 
-def get_movie_by_id(movie_id: int) -> Movie:
+def get_movie_by_id(movie_id: int) -> QuerySet[Movie]:
     return Movie.objects.get(id=movie_id)
 
 
 def create_movie(
         movie_title: str,
         movie_description: str,
-        genres_ids: list = None,
-        actors_ids: list = None
-) -> Movie:
+        genres_ids: list[int] | None = None,
+        actors_ids: list[int] | None = None
+) -> None:
     new_movie = Movie.objects.create(
         title=movie_title,
         description=movie_description
@@ -38,5 +38,3 @@ def create_movie(
 
     if actors_ids:
         new_movie.actors.set(actors_ids)
-
-    return new_movie
