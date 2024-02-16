d8f18a29194b92121c3321fdf8f1286db566e9e8
Nataliia Korzh <clarioruberia@gmail.com>
2024-02-16 11:50:50 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -41,8 +41,16 @@ class CinemaHall(models.Model):
 
 class MovieSession(models.Model):
     show_time = models.DateTimeField()
-    cinema_hall = models.ForeignKey(CinemaHall, related_name="movie_sessions", on_delete=models.CASCADE)
-    movie = models.ForeignKey(Movie, related_name="movie_sessions", on_delete=models.CASCADE)
+    cinema_hall = models.ForeignKey(
+        CinemaHall,
+        related_name="movie_sessions",
+        on_delete=models.CASCADE
+    )
+    movie = models.ForeignKey(
+        Movie,
+        related_name="movie_sessions",
+        on_delete=models.CASCADE
+    )
 
     def __str__(self) -> str:
         return f"{self.movie} {self.show_time}"
