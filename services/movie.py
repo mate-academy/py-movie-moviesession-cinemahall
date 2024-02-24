9b7786abe6af723d3cfed16a4acb973a1efe3b2a
maxmoshenskiy <42571265+MAFIOZNIK1999@users.noreply.github.com>
2024-02-24 12:52:08 +0200

    Update movie.py

--- services/movie.py
+++ services/movie.py
@@ -30,8 +30,8 @@ def create_movie(movie_title: str,
         description=movie_description,
     )
     if genres_ids:
-        new_movie.genres.add(genres_ids)
+        new_movie.genres.set(genres_ids)
     if actors_ids:
-        new_movie.actors.add(actors_ids)
+        new_movie.actors.set(actors_ids)
 
     return new_movie
