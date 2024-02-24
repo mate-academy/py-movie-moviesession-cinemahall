81015748e2f7f67e8204c8644210bb155fff095c
maxmoshenskiy <42571265+MAFIOZNIK1999@users.noreply.github.com>
2024-02-24 13:08:58 +0200

    Update movie_session.py

--- services/movie_session.py
+++ services/movie_session.py
@@ -26,16 +26,14 @@ def update_movie_session(session_id: int,
                          show_time: int = None,
                          movie_id: int = None,
                          cinema_hall_id: int = None) -> MovieSession:
-    movie_session = MovieSession.objects.get(id=session_id)
+    movie_session = MovieSession.objects.filter(id=session_id)
 
     if show_time:
-        movie_session.show_time = show_time
+        movie_session.update(show_time=show_time)
     if movie_id:
-        movie_session.movie_id = movie_id
+        movie_session.update(movie_id=movie_id)
     if cinema_hall_id:
-        movie_session.cinema_hall_id = cinema_hall_id
-
-    movie_session.save()
+        movie_session.update(cinema_hall_id=cinema_hall_id)
 
     return movie_session
 
