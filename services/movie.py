8675d8a6b5a1689d52e91e559f292dbd18adde4d
opminen <155957180+opminen@users.noreply.github.com>
2024-03-15 17:40:39 +0200

    Update movie.py

--- services/movie.py
+++ services/movie.py
@@ -2,8 +2,8 @@ from db.models import Movie
 
 
 def get_movies(
-        genres_ids: list[int] = None,
-        actors_ids: list[int] = None
+        genres_ids: list[int] | None = None,
+        actors_ids: list[int] | None = None
 ) -> list[Movie]:
     if genres_ids and actors_ids:
         return Movie.objects.filter(
