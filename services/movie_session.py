1d0eea20e3a9130d55e969fd854b8b240d797c8b
SysoevDmitro <113758312+SysoevDmitro@users.noreply.github.com>
2024-03-19 16:48:29 +0200

    Update movie_session.py

--- services/movie_session.py
+++ services/movie_session.py
@@ -11,7 +11,7 @@ def create_movie_session(movie_show_time: datetime,
                                        movie_id=movie_id)
 
 
-def get_movies_sessions(session_date: str = None) -> QuerySet[MovieSession]:
+def get_movies_sessions(session_date: str | None = None) -> QuerySet[MovieSession]:
 
     if session_date:
         return MovieSession.objects.filter(show_time__date=session_date)
@@ -23,9 +23,9 @@ def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
 
 
 def update_movie_session(session_id: int,
-                         show_time: datetime = None,
-                         movie_id: int = None,
-                         cinema_hall_id: int = None
+                         show_time: datetime | None = None,
+                         movie_id: int | None = None,
+                         cinema_hall_id: int | None = None
                          ) -> None:
     MovieSession.objects.filter(id=session_id).update(
         id=session_id)
