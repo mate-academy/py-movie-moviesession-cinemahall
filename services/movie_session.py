1ffd8ae2c9271c58ce22d2148f2c321d89034375
SysoevDmitro <113758312+SysoevDmitro@users.noreply.github.com>
2024-03-19 16:46:23 +0200

    Update services/movie_session.py
    Co-authored-by: Eduard Alekseiev <129166823+EdAlekseiev@users.noreply.github.com>

--- services/movie_session.py
+++ services/movie_session.py
@@ -11,7 +11,8 @@ def create_movie_session(movie_show_time: datetime,
                                        movie_id=movie_id)
 
 
-def get_movies_sessions(session_date: str = None) -> QuerySet:
+def get_movies_sessions(session_date: str = None) -> QuerySet[MovieSession]:
+
     if session_date:
         return MovieSession.objects.filter(show_time__date=session_date)
     return MovieSession.objects.all()
