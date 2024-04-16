5c209c5b8e9daf1c61da8c82f422587fa3f23043
meier <Meyertal2@gmail.com>
2024-04-16 17:47:09 +0300

    'Solution_V3'

--- services/movie_session.py
+++ services/movie_session.py
@@ -43,5 +43,4 @@ def update_movie_session(
 
 
 def delete_movie_session_by_id(session_id: int) -> None:
-    deleted_movie_session = MovieSession.objects.get(id=session_id)
-    deleted_movie_session.delete()
+    get_movie_session_by_id(session_id).delete()
