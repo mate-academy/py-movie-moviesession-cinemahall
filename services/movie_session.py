26a7fd9e3816b8e44b04bfd76431515c2b1f381c
Ivan Havrylyshyn <jyjuk1@gmail.com>
2024-04-16 12:19:52 +0300

    Solution

---
+++ services/movie_session.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+
+from django.db.models import QuerySet
+
+from db.models import MovieSession
+
+
+def create_movie_session(
+        movie_show_time: datetime,
+        movie_id: int,
+        cinema_hall_id: int
+) -> MovieSession:
+    return MovieSession.objects.create(
+        show_time=movie_show_time,
+        movie_id=movie_id,
+        cinema_hall_id=cinema_hall_id
+    )
+
+
+def get_movies_sessions(session_date: datetime.date = None) -> QuerySet:
+    session = MovieSession.objects.all()
+    if session_date:
+        return session.filter(show_time__date=session_date)
+
+
+def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
+    return MovieSession.objects.get(id=movie_session_id)
+
+
+def update_movie_session(
+        session_id: int,
+        show_time: datetime.date = None,
+        movie_id: int = None,
+        cinema_hall_id: int = None
+) -> None:
+    update_session = MovieSession.objects.get(id=session_id)
+    if show_time:
+        update_session.show_time = show_time
+    if movie_id:
+        update_session.movie_id = movie_id
+    if cinema_hall_id:
+        update_session.cinema_hall_id = cinema_hall_id
+    update_session.save()
+    return update_session
+
+
+def delete_movie_session_by_id(session_id: int) -> None:
+    delete_session = MovieSession.objects.get(id=session_id)
+    delete_session.delete()
