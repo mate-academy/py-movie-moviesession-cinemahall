49ce53c57612833abc8aca0a65ed1ed5ad9cbb54
Serhii Marynin <serhii.marynin@gmail.com>
2024-04-23 16:31:48 +0300

    update returns

--- services/movie_session.py
+++ services/movie_session.py
@@ -9,7 +9,7 @@ def create_movie_session(
         movie_show_time: datetime,
         movie_id: int,
         cinema_hall_id: int
-) -> MovieSession:
+) -> None:
     return MovieSession.objects.create(
         show_time=movie_show_time,
         movie_id=movie_id,
@@ -38,7 +38,7 @@ def update_movie_session(
         show_time: datetime = None,
         movie_id: int = None,
         cinema_hall_id: int = None
-) -> MovieSession:
+) -> None:
     movie_session = MovieSession.objects.get(id=session_id)
     if show_time:
         movie_session.show_time = show_time
@@ -47,8 +47,7 @@ def update_movie_session(
     if cinema_hall_id:
         movie_session.cinema_hall_id = cinema_hall_id
     movie_session.save()
-    return movie_session
 
 
-def delete_movie_session_by_id(session_id: int) -> MovieSession:
-    return MovieSession.objects.get(id=session_id).delete()
+def delete_movie_session_by_id(session_id: int) -> None:
+    MovieSession.objects.get(id=session_id).delete()
