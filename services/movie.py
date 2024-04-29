daf81431bafdb1c600d93936a238178bb8894498
Yuliia Dudych <juliadudych@gmail.com>
2024-04-29 20:23:11 +0300

    Add distinct() method to avoid duplication when returning movies QuerySet

--- services/movie.py
+++ services/movie.py
@@ -12,13 +12,13 @@ def get_movies(
         return Movie.objects.filter(
             genres__in=genres_ids,
             actors__in=actors_ids
-        )
+        ).distinct()
 
     if genres_ids:
-        return Movie.objects.filter(genres__id__in=genres_ids)
+        return Movie.objects.filter(genres__id__in=genres_ids).distinct()
 
     if actors_ids:
-        return Movie.objects.filter(actors__id__in=actors_ids)
+        return Movie.objects.filter(actors__id__in=actors_ids).distinct()
 
     return Movie.objects.all()
 
