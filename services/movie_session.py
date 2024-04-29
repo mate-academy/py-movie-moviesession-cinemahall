af7e18da1b942d93360236656fd215f9f39079ca
Yuliia Dudych <juliadudych@gmail.com>
2024-04-29 20:05:57 +0300

    Create service for MovieSession model to add a movie session to a db

---
+++ services/movie_session.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from django.db.models import QuerySet
+
+from db.models import MovieSession
+
+
+def create_movie_session(
+        movie_show_time: datetime,
+        movie_id: int,
+        cinema_hall_id: int,
+) -> None:
+    MovieSession.objects.create(
+        show_time=movie_show_time,
+        cinema_hall_id=cinema_hall_id,
+        movie_id=movie_id,
+    )
