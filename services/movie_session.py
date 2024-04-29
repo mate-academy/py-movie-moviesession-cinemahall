ed6643a3de19f0e4fae8638f6fb71126d7ca8862
Serhii Marynin <serhii.marynin@gmail.com>
2024-04-29 15:16:25 +0300

    add 'Optional' to arguments, update returns, update 'update_movie_session()'

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Optional
 
 from django.db.models import QuerySet
 
@@ -10,21 +11,18 @@ def create_movie_session(
         movie_id: int,
         cinema_hall_id: int
 ) -> None:
-    return MovieSession.objects.create(
+    MovieSession.objects.create(
         show_time=movie_show_time,
         movie_id=movie_id,
         cinema_hall_id=cinema_hall_id
     )
 
 
-def get_movies_sessions(session_date: str = None) -> QuerySet:
+def get_movies_sessions(session_date: Optional[str] = None) -> QuerySet:
     queryset = MovieSession.objects.all()
     if session_date:
-        session_date = datetime.strptime(session_date, "%Y-%m-%d")
         queryset = queryset.filter(
-            show_time__year=session_date.year,
-            show_time__month=session_date.month,
-            show_time__day=session_date.day
+            show_time__date=datetime.strptime(session_date, "%Y-%m-%d")
         )
     return queryset
 
@@ -35,18 +33,19 @@ def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
 
 def update_movie_session(
         session_id: int,
-        show_time: datetime = None,
-        movie_id: int = None,
-        cinema_hall_id: int = None
+        show_time: Optional[datetime] = None,
+        movie_id: Optional[int] = None,
+        cinema_hall_id: Optional[int] = None
 ) -> None:
-    movie_session = MovieSession.objects.get(id=session_id)
-    if show_time:
-        movie_session.show_time = show_time
-    if movie_id:
-        movie_session.movie_id = movie_id
-    if cinema_hall_id:
-        movie_session.cinema_hall_id = cinema_hall_id
-    movie_session.save()
+    if show_time or movie_id or cinema_hall_id:
+        movie_session = MovieSession.objects.get(id=session_id)
+        if show_time:
+            movie_session.show_time = show_time
+        if movie_id:
+            movie_session.movie_id = movie_id
+        if cinema_hall_id:
+            movie_session.cinema_hall_id = cinema_hall_id
+        movie_session.save()
 
 
 def delete_movie_session_by_id(session_id: int) -> None:
