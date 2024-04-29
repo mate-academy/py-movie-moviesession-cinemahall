e5d793adb457744ce59981e04434fa115237a61e
Yuliia Dudych <juliadudych@gmail.com>
2024-04-29 20:03:59 +0300

    Create service for Movie model to get movies based on genres/actors

---
+++ services/movie.py
@@ -0,0 +1,23 @@
+from django.db.models import QuerySet
+
+from db.models import Movie
+
+
+def get_movies(
+        genres_ids: list[int] = None,
+        actors_ids: list[int] = None
+) -> QuerySet:
+
+    if genres_ids and actors_ids:
+        return Movie.objects.filter(
+            genres__in=genres_ids,
+            actors__in=actors_ids
+        )
+
+    if genres_ids:
+        return Movie.objects.filter(genres__id__in=genres_ids)
+
+    if actors_ids:
+        return Movie.objects.filter(actors__id__in=actors_ids)
+
+    return Movie.objects.all()
