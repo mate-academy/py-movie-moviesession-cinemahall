804d71def86d4b3c1c7295a7fe6192d06e8b9f3d
Vasyl Rybalka <basilrybalka@gmail.com>
2024-05-06 17:56:46 +0200

    Solution

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,9 +1,14 @@
 from django.utils.dateparse import parse_date
+from django.db.models import QuerySet
 from db.models import MovieSession
 from datetime import datetime
 
 
-def create_movie_session(movie_show_time, movie_id, cinema_hall_id):
+def create_movie_session(
+    movie_show_time: datetime,
+    movie_id: int,
+    cinema_hall_id: int
+) -> MovieSession:
     new_session = MovieSession(
         show_time=movie_show_time,
         movie_id=movie_id,
@@ -12,16 +17,24 @@ def create_movie_session(movie_show_time, movie_id, cinema_hall_id):
     new_session.save()
     return new_session
 
-def get_movies_sessions(session_date=None):
+
+def get_movies_sessions(session_date: str = None) -> QuerySet:
     if session_date:
         date_parsed = parse_date(session_date)
         return MovieSession.objects.filter(show_time__date=date_parsed)
     return MovieSession.objects.all()
 
-def get_movie_session_by_id(movie_session_id):
+
+def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
     return MovieSession.objects.get(id=movie_session_id)
 
-def update_movie_session(session_id, show_time=None, movie_id=None, cinema_hall_id=None):
+
+def update_movie_session(
+    session_id: int,
+    show_time: datetime = None,
+    movie_id: int = None,
+    cinema_hall_id: int = None
+) -> MovieSession:
     session = MovieSession.objects.get(id=session_id)
     if show_time:
         session.show_time = show_time
@@ -32,7 +45,7 @@ def update_movie_session(session_id, show_time=None, movie_id=None, cinema_hall_
     session.save()
     return session
 
-def delete_movie_session_by_id(session_id):
+
+def delete_movie_session_by_id(session_id: int) -> None:
     session = MovieSession.objects.get(id=session_id)
     session.delete()
-    return session
