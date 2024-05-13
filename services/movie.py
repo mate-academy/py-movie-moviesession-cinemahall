cd304fda2dca6812dc477c9416c520ada99ea83c
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2024-05-13 15:30:55 +0300

    fix

--- services/movie.py
+++ services/movie.py
@@ -4,21 +4,14 @@ from db.models import Movie
 
 
 def get_movies(
-        genres_ids: list[int] | None,
-        actors_ids: list[int] | None,
+        genres_ids: list[int] | None = None,
+        actors_ids: list[int] | None = None,
 ) -> QuerySet[Movie]:
     queryset = Movie.objects.all()
-    if not genres_ids and not actors_ids:
-        queryset = queryset
-    if genres_ids and actors_ids:
-        queryset = queryset.filter(
-            genres__id__in=genres_ids,
-            actors__id__in=actors_ids
-        )
-    if genres_ids and not actors_ids:
-        queryset = queryset.filter(genres__id__in=genres_ids)
-    if actors_ids and not genres_ids:
-        queryset = queryset.filter(actors__id__in=actors_ids)
+    if genres_ids:
+        queryset = queryset.filter(genres__in=genres_ids)
+    if actors_ids:
+        queryset = queryset.filter(actors__in=actors_ids)
     return queryset
 
 
@@ -29,8 +22,8 @@ def get_movie_by_id(movie_id: int) -> Movie:
 def create_movie(
         movie_title: str,
         movie_description: str,
-        genres_ids: list[int] | None,
-        actors_ids: list[int] | None
+        genres_ids: list[int] | None = None,
+        actors_ids: list[int] | None = None
 ) -> Movie:
     movie = Movie.objects.create(
         title=movie_title,
