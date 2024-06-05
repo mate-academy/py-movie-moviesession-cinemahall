1a7a20495303b775b1eecde3141ba27db0a77263
Liliya <liliyakhu@ukr.net>
2024-06-05 12:43:27 +0300

    improve code 2

--- services/movie.py
+++ services/movie.py
@@ -25,8 +25,8 @@ def get_movie_by_id(movie_id: int) -> Movie:
 def create_movie(
         movie_title: str,
         movie_description: str,
-        genres_ids: list[int] = None,
-        actors_ids: list[int] = None
+        genres_ids: list[int] | None = None,
+        actors_ids: list[int] | None = None
 ) -> Movie:
     movie = Movie.objects.create(
         title=movie_title,
--- services/movie_session.py
+++ services/movie_session.py
@@ -19,7 +19,9 @@ def create_movie_session(
     return movie_session
 
 
-def get_movies_sessions(session_date: str = None) -> QuerySet[MovieSession]:
+def get_movies_sessions(
+        session_date: str | None = None
+) -> QuerySet[MovieSession]:
     if session_date:
         queryset = MovieSession.objects.filter(show_time__date=session_date)
         return queryset
@@ -32,9 +34,9 @@ def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
 
 def update_movie_session(
         session_id: int,
-        show_time: datetime = None,
-        movie_id: int = None,
-        cinema_hall_id: int = None
+        show_time: datetime | None = None,
+        movie_id: int | None = None,
+        cinema_hall_id: int | None = None
 ) -> None:
     movie_session = get_movie_session_by_id(movie_session_id=session_id)
     if show_time:
