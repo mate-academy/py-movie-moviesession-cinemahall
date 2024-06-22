2933b57e53996c434293dfce9fdb0d47ffeae44f
timur <stukantimur811@gmail.com>
2024-06-22 15:12:52 +0300

    fixes

--- services/movie.py
+++ services/movie.py
@@ -6,10 +6,7 @@ from db.models import Movie
 def get_movies(
         genres_ids: list[int] = None,
         actors_ids: list[int] = None
-) -> QuerySet | Movie:
-    if not genres_ids and not actors_ids:
-        return Movie.objects.all()
-
+) -> QuerySet[Movie] | Movie:
     queryset = Movie.objects.all()
     if genres_ids:
         queryset = queryset.filter(genres__id__in=genres_ids)
--- services/movie_session.py
+++ services/movie_session.py
@@ -15,7 +15,7 @@ def create_movie_session(
     )
 
 
-def get_movies_sessions(session_date: str = None) -> QuerySet:
+def get_movies_sessions(session_date: str = None) -> QuerySet[MovieSession]:
     if session_date:
         return MovieSession.objects.filter(show_time__date=session_date)
 
@@ -32,7 +32,7 @@ def update_movie_session(
         movie_id: int = None,
         cinema_hall_id: int = None
 ) -> None:
-    movie_to_change = MovieSession.objects.get(id=session_id)
+    movie_to_change = get_movie_session_by_id(session_id)
     if show_time:
         movie_to_change.show_time = show_time
     if movie_id:
@@ -44,4 +44,4 @@ def update_movie_session(
 
 
 def delete_movie_session_by_id(session_id: int) -> None:
-    MovieSession.objects.get(id=session_id).delete()
+    get_movie_session_by_id(session_id).delete()
