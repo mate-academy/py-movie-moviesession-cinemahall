3d1fabc44ea5541ab6dc6cb7eeaac11567d15879
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-06-24 17:03:56 +0200

    fix problem with flake8

--- services/movie_session.py
+++ services/movie_session.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from django.db.models import QuerySet
 
-from db.models import MovieSession, Movie, CinemaHall
+from db.models import MovieSession, CinemaHall
 from services.movie import get_movie_by_id
 
 
