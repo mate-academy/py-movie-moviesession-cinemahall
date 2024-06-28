795dada223dfcd60de45cf63a6fb900819f09188
unlie9 <unlie.devops9@gmail.com>
2024-06-28 13:15:01 +0300

    fix

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,17 +1,14 @@
 from datetime import datetime
 from django.db.models import QuerySet
 from db.models import MovieSession, Movie, CinemaHall
-
+from services.movie import get_movie_by_id
 
 def create_movie_session(movie_show_time: Movie, movie_id: Movie,
                          cinema_hall_id: CinemaHall) -> None:
-    movie = Movie.objects.get(id=movie_id)
-    cinema_hall = CinemaHall.objects.get(id=cinema_hall_id)
-
     MovieSession.objects.create(
         show_time=movie_show_time,
-        cinema_hall=cinema_hall,
-        movie=movie
+        cinema_hall_id=cinema_hall_id,
+        movie_id=movie_id
     )
 
 
@@ -31,12 +28,13 @@ def update_movie_session(session_id: MovieSession,
                          show_time: MovieSession = None,
                          movie_id: Movie = None,
                          cinema_hall_id: CinemaHall = None) -> MovieSession:
-    session = MovieSession.objects.get(id=session_id)
+
+    session = get_movie_session_by_id(session_id)
 
     if show_time:
         session.show_time = show_time
     if movie_id:
-        movie = Movie.objects.get(id=movie_id)
+        movie = get_movie_by_id(movie_id)
         session.movie = movie
     if cinema_hall_id:
         cinema_hall = CinemaHall.objects.get(id=cinema_hall_id)
@@ -47,4 +45,4 @@ def update_movie_session(session_id: MovieSession,
 
 
 def delete_movie_session_by_id(session_id: MovieSession) -> MovieSession:
-    MovieSession.objects.filter(id=session_id).delete()
+    get_movie_session_by_id(session_id).delete()
