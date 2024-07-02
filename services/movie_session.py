676249a6cf772e8d380ed202348347dfe89dc172
Sergiy Yurov <urovsergej10@gmail.com>
2024-07-02 18:19:24 +0300

    Solution

--- services/movie_session.py
+++ services/movie_session.py
@@ -42,4 +42,4 @@ def update_movie_session(session_id: int,
 
 
 def delete_movie_session_by_id(session_id: int) -> None:
-    return MovieSession.objects.filter(id=session_id).delete()
+    return MovieSession.objects.get(id=session_id).delete()
