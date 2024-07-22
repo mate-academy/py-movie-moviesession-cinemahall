778bed7020d07aaf25c8f5f5455a2f1cc559f6e1
Vadym Vyshnevskyi <wallian169@gmail.com>
2024-07-22 16:35:09 +0300

    changed def get_movie_by_id

--- services/movie.py
+++ services/movie.py
@@ -19,8 +19,7 @@ def get_movies(
 
 
 def get_movie_by_id(movie_id: int) -> Movie:
-    movie = Movie.objects.get(id=movie_id)
-    return movie
+    return Movie.objects.get(id=movie_id)
 
 
 def create_movie(
