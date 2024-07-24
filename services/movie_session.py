95377f93188a83378942d93b5d5be4f6f197c825
TsvetanKichuk <toys4babyodua@gmail.com>
2024-07-24 08:56:42 +0200

    'Solution'

--- services/movie_session.py
+++ services/movie_session.py
@@ -51,5 +51,4 @@ def update_movie_session(
 def delete_movie_session_by_id(session_id: int) -> MovieSession:
     movie_session = get_movie_session_by_id(session_id)
     if movie_session:
-        movie_session.delete()
-        return movie_session
+        return movie_session.delete()
