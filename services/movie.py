ccce997f47d0c1ae8f1f8c7b0d8c30bad15c9959
VR <vladrymarchuk@gmail.com>
2024-07-25 20:14:24 +0300

    Created services with module movie.py

---
+++ services/movie.py
@@ -0,0 +1,45 @@
+from django.db import transaction
+from django.db.models import QuerySet
+
+from db.models import Movie
+
+
+def get_movies(
+        genres_ids: list[int] = None,
+        actors_ids: list[int] = None,
+) -> QuerySet[Movie] | Movie:
+    query = Movie.objects.all()
+    if not genres_ids and not actors_ids:
+        return query
+    if genres_ids:
+        query = query.filter(genres__id__in=genres_ids)
+    if actors_ids:
+        query = query.filter(actors__id__in=actors_ids)
+    if genres_ids and actors_ids:
+        query = query.filter(
+            genres__id__in=genres_ids,
+            actors__id__in=actors_ids
+        )
+    return query.distinct()
+
+
+def get_movie_by_id(movie_id: int) -> Movie:
+    return Movie.objects.get(id=movie_id)
+
+
+@transaction.atomic
+def create_movie(
+        movie_title: str,
+        movie_description: str,
+        genres_ids: list[int] = None,
+        actors_ids: list[int] = None,
+) -> Movie:
+    new_movie = Movie.objects.create(
+        title=movie_title,
+        description=movie_description
+    )
+    if genres_ids:
+        new_movie.genres.set(genres_ids)
+    if actors_ids:
+        new_movie.actors.set(actors_ids)
+    return new_movie
