9aee4bfa128b5a309a6c7f083b4772df0384a41e
Viktor Zhdanovych <cryingtm27@gmail.com>
2024-07-29 16:00:38 +0300

    some changes

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,3 +1,5 @@
+from django.db.models import QuerySet
+
 from db.models import MovieSession
 
 
@@ -13,7 +15,7 @@ def create_movie_session(
     )
 
 
-def get_movies_sessions(session_date: str = None) -> MovieSession:
+def get_movies_sessions(session_date: str = None) -> QuerySet[MovieSession]:
     queryset = MovieSession.objects.all()
     if session_date:
         queryset = queryset.filter(show_time__date=session_date)
@@ -42,4 +44,4 @@ def update_movie_session(
 
 
 def delete_movie_session_by_id(session_id: int) -> None:
-    MovieSession.objects.get(id=session_id).delete()
+    get_movie_session_by_id(session_id).delete()
