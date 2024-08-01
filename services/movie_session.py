ff8f58e0c0d9ebb63c0d2260ff822cd29045d76e
Bohdan Bastiuk <bbastyuk@gmail.com>
2024-08-01 16:00:11 +0200

    Опис ваuse get

--- services/movie_session.py
+++ services/movie_session.py
@@ -37,13 +37,15 @@ def update_movie_session(
         movie_id: int = None,
         cinema_hall_id: int = None
 ) -> MovieSession:
-    movie_session = MovieSession.objects.get(id=session_id)
+    movie_session = get_movie_session_by_id(session_id)
+
     if show_time is not None:
         movie_session.show_time = show_time
     if movie_id is not None:
         movie_session.movie_id = movie_id
     if cinema_hall_id is not None:
         movie_session.cinema_hall_id = cinema_hall_id
+
     movie_session.save()
     return movie_session
 
@@ -51,4 +53,5 @@ def update_movie_session(
 def delete_movie_session_by_id(
         session_id: int
 ) -> None:
-    MovieSession.objects.get(id=session_id).delete()
+    movie_session = get_movie_session_by_id(session_id)
+    movie_session.delete()
