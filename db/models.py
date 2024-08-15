db8dac2b0301ed91b5679eca97c43fb168ad0c73
Vova <v.kampanchenko@gmail.com>
2024-08-15 15:45:08 +0300

    Solution

--- db/models.py
+++ db/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(
+        max_length=255,
+        unique=True
+    )
 
     def __str__(self) -> str:
         return self.name
@@ -14,3 +17,50 @@ class Actor(models.Model):
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    actors = models.ManyToManyField(Actor)
+    genres = models.ManyToManyField(Genre)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField(
+        "number of rows of seats in the hall",
+        default=0
+    )
+    seats_in_row = models.IntegerField(
+        "number of seats in each row",
+        default=0
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+
+class MovieSession(models.Model):
+    show_time = models.DateTimeField()
+    cinema_hall = models.ForeignKey(
+        CinemaHall,
+        on_delete=models.CASCADE
+    )
+    movie = models.ForeignKey(
+        Movie,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self) -> str:
+        return (
+            f"{self.movie.title} "
+            f"{self.show_time.strftime('%Y-%m-%d %H:%M:%S')}"
+        )
---
+++ services/movie_session.py
@@ -0,0 +1,62 @@
+from datetime import datetime
+from django.utils.dateparse import parse_date
+
+import init_django_orm  # noqa: F401
+
+from django.db.models import QuerySet
+from db.models import MovieSession
+
+
+def create_movie_session(
+        movie_show_time: datetime,
+        movie_id: int,
+        cinema_hall_id: int
+) -> MovieSession:
+    return MovieSession.objects.create(
+        show_time=movie_show_time,
+        movie_id=movie_id,
+        cinema_hall_id=cinema_hall_id,
+    )
+
+
+def get_movies_sessions(
+        session_date: datetime = None
+) -> MovieSession | QuerySet:
+    if not session_date:
+        return MovieSession.objects.all()
+    date = parse_date(session_date)
+    if date:
+        return MovieSession.objects.filter(show_time__date=date)
+    else:
+        raise ValueError("Invalid date format. "
+                         "Use 'year-month-day'.")
+
+
+def get_movie_session_by_id(
+        movie_session_id: int,
+) -> QuerySet:
+    return MovieSession.objects.get(id=movie_session_id)
+
+
+def update_movie_session(
+        session_id: int,
+        show_time: datetime = None,
+        movie_id: int = None,
+        cinema_hall_id: int = None
+) -> MovieSession:
+    if show_time:
+        (MovieSession.objects.filter(id=session_id)
+         .update(show_time=show_time))
+    if movie_id:
+        (MovieSession.objects.filter(id=session_id)
+         .update(movie_id=movie_id))
+    if cinema_hall_id:
+        (MovieSession.objects.filter(id=session_id)
+         .update(cinema_hall_id=cinema_hall_id))
+    return MovieSession.objects.filter(id=session_id)
+
+
+def delete_movie_session_by_id(
+        session_id: int
+) -> MovieSession:
+    return MovieSession.objects.filter(id=session_id).delete()
