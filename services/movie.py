5022de6df0dba9f2f44625d1d20029e70ebfd349
artem.puhach <artem.puhach@cyberpolice.gov.ua>
2024-08-19 17:00:16 +0300

    fix comments provided by mentor

--- services/movie.py
+++ services/movie.py
@@ -1,26 +1,18 @@
-from db.models import Movie
 from django.db.models import QuerySet
+
+from db.models import Movie
 
 
 def get_movies(
         genres_ids: list = None,
         actors_ids: list = None
-) -> QuerySet:
-    if genres_ids is None and actors_ids is None:
-        return Movie.objects.all()
-    if genres_ids is not None and actors_ids is not None:
-        return Movie.objects.filter(
-            genres__pk__in=genres_ids,
-            actors__pk__in=actors_ids
-        )
-    if genres_ids is not None and actors_ids is None:
-        return Movie.objects.filter(
-            genres__pk__in=genres_ids
-        )
-    if actors_ids is not None and genres_ids is None:
-        return Movie.objects.filter(
-            actors__pk__in=actors_ids
-        )
+) -> QuerySet[Movie]:
+    queryset = Movie.objects.all()
+    if genres_ids:
+        queryset = queryset.filter(genres__pk__in=genres_ids)
+    if actors_ids:
+        queryset = queryset.filter(actors__pk__in=actors_ids)
+    return queryset
 
 
 def get_movie_by_id(movie_id: int) -> Movie:
@@ -32,15 +24,13 @@ def create_movie(movie_title: str,
                  genres_ids: list = None,
                  actors_ids: list = None,
                  ) -> None:
-    new_movie = Movie.objects.create(
+    movie = Movie.objects.create(
         title=movie_title,
         description=movie_description
     )
+    if genres_ids:
+        movie.genres.set(genres_ids)
+    if actors_ids:
+        movie.actors.set(actors_ids)
 
-    if genres_ids is not None:
-        new_movie.genres.set(genres_ids)
 
-    if actors_ids is not None:
-        new_movie.actors.set(actors_ids)
-
-    new_movie.save()
--- services/movie_session.py
+++ services/movie_session.py
@@ -10,17 +10,8 @@ def create_movie_session(
         movie_id: int,
         cinema_hall_id: int
 ) -> None:
-    try:
-        movie = Movie.objects.get(id=movie_id)
-    except Movie.DoesNotExist:
-        raise ValueError(f"Movie with id {movie_id} does not exist")
-
-    try:
-        cinema_hall = CinemaHall.objects.get(id=cinema_hall_id)
-    except CinemaHall.DoesNotExist:
-        raise ValueError(f"CinemaHall with id {cinema_hall_id} "
-                         f"does not exists")
-
+    movie = Movie.objects.get(id=movie_id)
+    cinema_hall = CinemaHall.objects.get(id=cinema_hall_id)
     MovieSession.objects.create(
         show_time=movie_show_time,
         movie=movie,
@@ -52,15 +43,11 @@ def update_movie_session(
         cinema_hall_id: int = None
 ) -> None:
     session = MovieSession.objects.get(id=session_id)
-    if show_time is not None:
+    if show_time:
         session.show_time = show_time
-    if movie_id is not None:
-        try:
-            movie = Movie.objects.get(id=movie_id)
-        except Movie.DoesNotExist:
-            raise ValueError(f"Movie with id {movie_id} does not exist")
-        session.movie = movie
-    if cinema_hall_id is not None:
+    if movie_id:
+        session.movie = Movie.objects.get(id=movie_id)
+    if cinema_hall_id:
         session.cinema_hall_id = cinema_hall_id
     session.save()
 
@@ -68,4 +55,4 @@ def update_movie_session(
 def delete_movie_session_by_id(
         session_id: int
 ) -> None:
-    MovieSession.objects.get(pk=session_id).delete()
+    get_movie_session_by_id(session_id).delete()
