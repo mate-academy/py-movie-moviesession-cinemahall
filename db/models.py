b343c77001f708d26a41a6259cdd5907fe1fc4a6
Vsevoloddeez <143706389+Vsevoloddeez@users.noreply.github.com>
2024-08-22 20:12:15 +0300

    Update models.py

--- db/models.py
+++ db/models.py
@@ -1,26 +1,49 @@
 from django.db import models
 
 
-class Race(models.Model):
+class Genre(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    description = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return self.name
 
 
-class Skill(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    bonus = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
 
 
-class Guild(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    description = models.TextField(null=True)
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    actors = models.ManyToManyField(Actor)
+    genres = models.ManyToManyField(Genre)
+
+    def __str__(self) -> str:
+        return self.title
 
 
-class Player(models.Model):
-    nickname = models.CharField(max_length=255, unique=True)
-    email = models.EmailField(max_length=255, unique=True)
-    bio = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE)
-    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.name
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+
+class MovieSession(models.Model):
+    show_time = models.DateTimeField()
+    cinema_hall = models.ForeignKey(CinemaHall, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.movie.title} {self.show_time}"
+        
