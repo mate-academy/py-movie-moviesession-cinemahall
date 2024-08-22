ddc8031859fed1f3bf8d02f0edd55ce605dcc059
Seva Sorokopud <seva.soroka.94@gmail.com>
2024-08-22 19:09:19 +0300

    Solution

--- db/models.py
+++ db/models.py
@@ -1,16 +1,26 @@
 from django.db import models
 
 
-class Genre(models.Model):
+class Race(models.Model):
     name = models.CharField(max_length=255, unique=True)
-
-    def __str__(self) -> str:
-        return self.name
+    description = models.TextField(blank=True)
 
 
-class Actor(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
+class Skill(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    bonus = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
 
-    def __str__(self) -> str:
-        return f"{self.first_name} {self.last_name}"
+
+class Guild(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True)
+
+
+class Player(models.Model):
+    nickname = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
+    bio = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
