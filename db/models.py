b75e3c6c8f3cda26abe9561ab42178be409c4342
Valeriia Kyrylenko <valerya.kyrylenko@gmail.com>
2024-08-27 14:42:01 +0200

    implement py-movie-moviesession-cinemahall

--- db/models.py
+++ db/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 
 
 class Genre(models.Model):
@@ -14,3 +15,44 @@ class Actor(models.Model):
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    actors = models.ManyToManyField(Actor)
+    genres = models.ManyToManyField(Genre)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.name
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+
+class MovieSession(models.Model):
+    show_time = models.DateTimeField()
+    cinema_hall = models.ForeignKey(
+        CinemaHall,
+        on_delete=models.CASCADE,
+        related_name="movie_session_halls"
+    )
+    movie = models.ForeignKey(
+        Movie,
+        on_delete=models.CASCADE,
+        related_name="movie_sessions_movies"
+    )
+
+    def __str__(self) -> str:
+        date = datetime.datetime.strftime(self.show_time, "%Y-%m-%d %H:%M:%S")
+        return f"{self.movie.title} {date}"
