b113759287f87bf80dea5ea1d2ad6679c26fa584
Alexandr Politov <apolitov83@gmail.com>
2024-08-28 12:27:03 +0300

    Solution

--- services/movie.py
+++ services/movie.py
@@ -1,10 +1,12 @@
 from django.db.models import QuerySet
+from typing import Optional
+
 
 from db.models import Movie
 
 
-def get_movies(genres_ids: list[int] = None,
-               actors_ids: list[int] = None) -> QuerySet:
+def get_movies(genres_ids: Optional[list[int]] = None,
+               actors_ids: Optional[list[int]] = None) -> QuerySet:
     result = Movie.objects.all()
     if genres_ids:
         result = result.filter(genres__id__in=genres_ids)
@@ -14,13 +16,13 @@ def get_movies(genres_ids: list[int] = None,
 
 
 def get_movie_by_id(movie_id: int) -> Movie:
-    return Movie.objects.get(id=movie_id)
+    return Movie.objects.get(pk=movie_id)
 
 
 def create_movie(movie_title: str,
                  movie_description: str,
-                 genres_ids: list[int] = None,
-                 actors_ids: list[int] = None) -> None:
+                 genres_ids: Optional[list[int]] = None,
+                 actors_ids: Optional[list[int]] = None) -> None:
     new_movie = Movie.objects.create(title=movie_title,
                                      description=movie_description)
     if genres_ids:
