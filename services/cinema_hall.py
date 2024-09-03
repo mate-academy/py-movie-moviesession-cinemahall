ed90b99bb0ceb1253548837a77d0359f4e1e8573
NazarSerdiuk1 <nazarserdiuk260@gmail.com>
2024-09-03 13:53:18 +0300

    Solution1

--- services/cinema_hall.py
+++ services/cinema_hall.py
@@ -1,9 +1,8 @@
 from db.models import CinemaHall
-
 from django.db.models import QuerySet
 
 
-def get_cinema_halls() -> QuerySet:
+def get_cinema_halls() -> QuerySet[CinemaHall]:
     return CinemaHall.objects.all()
 
 
