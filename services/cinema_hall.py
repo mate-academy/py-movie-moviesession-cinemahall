90479905b63c74292bc208186e04732bb1351e3a
Anastasiia Karpenko <karpenko.anastasiya@gmail.com>
2024-09-23 17:26:17 +0300

    code refactor according to flake8

--- services/cinema_hall.py
+++ services/cinema_hall.py
@@ -5,10 +5,12 @@ def get_cinema_halls() -> list[CinemaHall]:
     return CinemaHall.objects.all()
 
 
-def create_cinema_hall(hall_name: str, hall_rows: int, seats_in_row: int) -> CinemaHall:
+def create_cinema_hall(hall_name: str,
+                       hall_rows: int,
+                       hall_seats_in_row: int) -> CinemaHall:
     cinema = CinemaHall.objects.create(
         name=hall_name,
         rows=hall_rows,
-        seats_in_row=seats_in_row,
+        seats_in_row=hall_seats_in_row,
     )
     return cinema
--- services/movie.py
+++ services/movie.py
@@ -1,9 +1,11 @@
 from db.models import Movie
 
 
-def get_movies(genres_ids: list[int] = None, actors_ids: list[int] = None) -> list[Movie]:
+def get_movies(genres_ids: list[int] = None,
+               actors_ids: list[int] = None) -> list[Movie]:
     if genres_ids and actors_ids:
-        return Movie.objects.filter(genres__in=genres_ids).filter(actors__in=actors_ids)
+        return (Movie.objects.filter(genres__in=genres_ids)
+                .filter(actors__in=actors_ids))
     if genres_ids:
         return Movie.objects.filter(genres__in=genres_ids)
     if actors_ids:
@@ -15,7 +17,10 @@ def get_movie_by_id(movie_id: int) -> Movie:
     return Movie.objects.get(id=movie_id)
 
 
-def create_movie(movie_title: str, movie_description: str, genres_ids: list[int] = None, actors_ids: list[int] = None) -> None:
+def create_movie(movie_title: str,
+                 movie_description: str,
+                 genres_ids: list[int] = None,
+                 actors_ids: list[int] = None) -> None:
     movie = Movie.objects.create(
         title=movie_title,
         description=movie_description,
@@ -27,5 +32,3 @@ def create_movie(movie_title: str, movie_description: str, genres_ids: list[int]
         movie.actors.add(*actors_ids)
 
     return movie
-
-
