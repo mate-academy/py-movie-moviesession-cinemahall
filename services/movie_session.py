0ec24163aeff0431415292aa95999cec323d32b9
Vladyslav Timofieiev <vladislav.tmf@gmail.com>
2024-10-28 09:38:38 +0200

    Fixes

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,4 +1,5 @@
 from django.db.models import QuerySet
+from datetime import date
 
 from db.models import MovieSession
 
@@ -16,7 +17,7 @@ def create_movie_session(
 
 
 def get_movies_sessions(
-    session_date: str = None
+    session_date: date = None
 ) -> QuerySet[MovieSession]:
     if session_date:
         return MovieSession.objects.filter(show_time__date=session_date)
@@ -47,4 +48,4 @@ def update_movie_session(
 
 
 def delete_movie_session_by_id(session_id: int) -> None:
-    MovieSession.objects.get(id=session_id).delete()
+    get_movie_session_by_id(session_id).delete()
