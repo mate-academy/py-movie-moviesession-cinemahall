f95ce9ede2d2e4b4aae3540320fd3c2193531a74
Matvii Safonov <matveysafonov2@gmail.com>
2024-11-05 09:22:00 +0200

    Solution

--- services/cinema_hall.py
+++ services/cinema_hall.py
@@ -1,15 +1,16 @@
+from django.db.models.query import QuerySet
 from db.models import CinemaHall
 
 
-def get_cinema_halls() -> None:
+def get_cinema_halls() -> QuerySet:
     return CinemaHall.objects.all()
 
 
 def create_cinema_hall(
-        hall_name: None,
-        hall_rows: None,
-        hall_seats_in_row: None
-) -> None:
+        hall_name: str,
+        hall_rows: int,
+        hall_seats_in_row: int
+) -> CinemaHall:
     return CinemaHall.objects.create(
         name=hall_name,
         rows=hall_rows,
