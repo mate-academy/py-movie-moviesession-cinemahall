196e0af25b9ab008064f47e48fc9feec093e79c8
fanni <fmalevych@zoolatech.com>
2024-11-28 14:35:09 +0200

    fix

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,5 +1,4 @@
-import datetime
-
+from datetime import datetime
 from django.db.models import QuerySet
 
 from db.models import MovieSession
@@ -9,7 +8,7 @@ def get_movies_sessions(session_date: str = None) -> QuerySet:
     query_set = MovieSession.objects.all()
 
     if session_date:
-        session_date = datetime.datetime.strptime(
+        session_date = datetime.strptime(
             session_date, "%Y-%m-%d"
         ).date()
         return MovieSession.objects.filter(show_time__date=session_date)
@@ -21,7 +20,7 @@ def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
     return MovieSession.objects.get(id=movie_session_id)
 
 
-def create_movie_session(movie_show_time: str,
+def create_movie_session(movie_show_time: datetime,
                          movie_id: int,
                          cinema_hall_id: int) -> MovieSession:
     return MovieSession.objects.create(
@@ -32,16 +31,17 @@ def create_movie_session(movie_show_time: str,
 
 
 def update_movie_session(session_id: int,
-                         show_time: str = None,
+                         show_time: datetime = None,
                          movie_id: int = None,
-                         cinema_hall_id: int = None) -> QuerySet:
-    movie_session_to_upd = MovieSession.objects.filter(id=session_id)
+                         cinema_hall_id: int = None) -> MovieSession:
+    movie_session_to_upd = MovieSession.objects.get(id=session_id)
     if show_time:
-        movie_session_to_upd.update(show_time=show_time)
+        movie_session_to_upd.show_time = show_time
     if movie_id:
-        movie_session_to_upd.update(movie_id=movie_id)
+        movie_session_to_upd.movie_id = movie_id
     if cinema_hall_id:
-        movie_session_to_upd.update(cinema_hall_id=cinema_hall_id)
+        movie_session_to_upd.cinema_hall_id = cinema_hall_id
+    movie_session_to_upd.save()
 
     return movie_session_to_upd
 
