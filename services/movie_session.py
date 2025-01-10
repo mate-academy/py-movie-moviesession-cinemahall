69f0e4a3590fc954edb66e0e7a3a7cab859cd6bb
Vova <vova.gld05@gmail.com>
2025-01-10 09:54:34 +0200

    'fix'

--- services/movie_session.py
+++ services/movie_session.py
@@ -26,8 +26,8 @@ def get_movies_sessions(session_date: datetime = None) -> QuerySet:
     return sessions
 
 
-def get_movie_session_by_id(movie_id: int) -> MovieSession:
-    return MovieSession.objects.get(id=movie_id)
+def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
+    return MovieSession.objects.get(id=movie_session_id)
 
 
 def update_movie_session(
