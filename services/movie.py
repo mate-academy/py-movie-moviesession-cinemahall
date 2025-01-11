48ab5dc404def11f585b95281a7283e3f29072ea
Roman Zaliznyi <romanzaliznyi.python@gmail.com>
2025-01-11 16:32:04 +0200

    Solution

---
+++ services/movie.py
@@ -0,0 +1,34 @@
+from db.models import Movie
+
+
+def get_movies(genres_ids: list = None, actors_ids: list = None) -> str:
+    query = Movie.objects.all()
+    if genres_ids:
+        query = query.filter(genres__id__in=genres_ids)
+    if actors_ids:
+        query = query.filter(actors__id__in=actors_ids)
+    return query.distinct()
+
+
+def get_movie_by_id(movie_id: int) -> str:
+    return Movie.objects.get(id=movie_id)
+
+
+def create_movie(
+    movie_title: str,
+    movie_description: str,
+    genres_ids: list = None,
+    actors_ids: list = None
+) -> str:
+
+    movie = Movie.objects.create(
+        title=movie_title,
+        description=movie_description,
+        actors=actors_ids,
+        genres=genres_ids
+    )
+    if genres_ids:
+        movie.genres.set(genres_ids)
+    if actors_ids:
+        movie.actors.set(actors_ids)
+    return movie
---
+++ services/movie_session.py
@@ -0,0 +1,47 @@
+from db.models import MovieSession
+
+
+def create_movie_session(
+        movie_show_time: str,
+        movie_id: int,
+        cinema_hall_id: int
+) -> str:
+    return MovieSession.objects.create(
+        show_time=movie_show_time,
+        cinema_hall=cinema_hall_id,
+        movie=movie_id
+    )
+
+
+def get_movies_sessions(session_date: str = None) -> str:
+    query = MovieSession.objects.all()
+    if session_date:
+        query = query.filter(show_time__date=session_date)
+    return query
+
+
+def get_movie_session_by_id(movie_session_id: int) -> str:
+    return MovieSession.objects.get(id=movie_session_id)
+
+
+def update_movie_session(
+        session_id: int,
+        show_time: str = None,
+        movie_id: int = None,
+        cinema_hall_id: int = None
+) -> str:
+    session = MovieSession.objects.get(id=session_id)
+
+    if show_time:
+        session.show_time = show_time
+    if movie_id:
+        session.movie = movie_id
+    if cinema_hall_id:
+        session.cinema_hall = cinema_hall_id
+
+    session.save()
+    return session
+
+
+def delete_movie_session_by_id(session_id: int) -> None:
+    MovieSession.objects.filter(id=session_id).delete()
