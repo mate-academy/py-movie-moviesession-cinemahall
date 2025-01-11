a42ff82f78625d9f9616ad2158fb5e8a451ad69d
Arsen Myroniuk <arisenmyr@gmail.com>
2025-01-11 21:49:46 +0200

    fixed solution

--- db/models.py
+++ db/models.py
@@ -28,8 +28,8 @@ class Movie(models.Model):
 
 class CinemaHall(models.Model):
     name = models.CharField(max_length=255)
-    rows = models.IntegerField()
-    seats_in_row = models.IntegerField()
+    rows = models.PositiveIntegerField()
+    seats_in_row = models.PositiveIntegerField()
 
     def __str__(self) -> str:
         return self.name
@@ -44,12 +44,11 @@ class MovieSession(models.Model):
     cinema_hall = models.ForeignKey(
         CinemaHall,
         on_delete=models.CASCADE,
-        related_name="cinema_halls"
+        related_name="movie_sessions",
     )
     movie = models.ForeignKey(
         Movie,
         on_delete=models.CASCADE,
-        related_name="movie_sessions"
     )
 
     def __str__(self) -> str:
