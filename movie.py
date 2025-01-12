f5275e9528f6240b03000e8ac78bfa0c86f2e3d7
Oleksandr Barabanov <imtence@icloud.com>
2025-01-12 13:59:09 +0200

    Solution

--- movie.py
+++ movie.py
@@ -11,18 +11,24 @@ def get_movies(
 
     if genres_ids and actors_ids:
         movies = movies.filter(
-            genre__id__in=genres_ids,
-            actor__id__in=actors_ids
+            genres__id__in=genres_ids,
+            actors__id__in=actors_ids
         ).distinct()
     elif genres_ids:
-            movies = movies.filter(genre__id__in=genres_ids).distinct()
+        movies = movies.filter(
+            genres__id__in=genres_ids
+        ).distinct()
     elif actors_ids:
-            movies = movies.filter(actor__id__in=actors_ids).distinct()
+        movies = movies.filter(
+            actors__id__in=actors_ids
+        ).distinct()
 
     return list(movies)
 
+
 def get_movie_by_id(movie_id: int) -> Optional[Movie]:
     return Movie.objects.get(id=movie_id)
+
 
 def create_movie(movie_title: str,
                  movie_description: str,
