1101b40fd63b0de7a36b5fc9d9a00060ca7dccc0
10kkyvl <ztkkre@gmail.com>
2025-01-13 10:29:45 +0200

    Solution fix

--- services/movie.py
+++ services/movie.py
@@ -10,8 +10,10 @@ def get_movies(
     movies = Movie.objects.all()
 
     if genres_ids and actors_ids:
-        movies = movies.filter(genres__id__in=genres_ids,
-                               actors__id__in=actors_ids)
+        movies = movies.filter(
+            genres__id__in=genres_ids,
+            actors__id__in=actors_ids
+    )
     if genres_ids:
         movies = movies.filter(genres__id__in=genres_ids)
     if actors_ids:
