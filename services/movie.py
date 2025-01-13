2f9f8bed540a7f7fb9d0ee5893192648b1cef5a8
Blythe Ballesteros <blytheballesteros@gmail.com>
2025-01-13 01:42:24 -1000

    solution

---
+++ services/movie.py
@@ -0,0 +1,36 @@
+from db.models import Movie
+from django.db.models import QuerySet
+
+
+def get_movies(genres_ids: list[int] = None,
+               actors_ids: list[int] = None) -> QuerySet:
+
+    query_set = Movie.objects.all()
+
+    if genres_ids:
+        query_set = query_set.filter(genres__in=genres_ids)
+    if actors_ids:
+        query_set = query_set.filter(actors__in=actors_ids)
+
+    return query_set
+
+
+def get_movie_by_id(movie_id: int) -> Movie:
+    return Movie.objects.get(id=movie_id)
+
+
+def create_movie(movie_title: str,
+                 movie_description: str,
+                 genres_ids: list[int] = None,
+                 actors_ids: list[int] = None) -> Movie:
+    movie = Movie.objects.create(
+        title=movie_title,
+        description=movie_description)
+
+    if genres_ids:
+        movie.genres.set(genres_ids)
+
+    if actors_ids:
+        movie.actors.set(actors_ids)
+
+    return movie
