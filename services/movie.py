049a64410aa28f24004127ca72920c38b0de13ae
Mykola <kutiamykola@gmail.com>
2025-01-17 23:17:21 +0200

    Solution_MK_v1

---
+++ services/movie.py
@@ -0,0 +1,34 @@
+from typing import List, Optional
+from db.models import Movie
+
+
+def get_movies(genres_ids: Optional[List[int]] = None,
+               actors_ids: Optional[List[int]] = None) -> any:
+    query = Movie.objects.all()
+
+    if genres_ids:
+        query = query.filter(genres__id__in=genres_ids).distinct()
+
+    if actors_ids:
+        query = query.filter(actors__id__in=actors_ids).distinct()
+
+    return query
+
+
+def get_movie_by_id(movie_id: int) -> any:
+    return Movie.objects.get(id=movie_id)
+
+
+def create_movie(movie_title: str, movie_description: str,
+                 genres_ids: Optional[List[int]] = None,
+                 actors_ids: Optional[List[int]] = None) -> any:
+    movie = Movie.objects.create(title=movie_title,
+                                 description=movie_description)
+
+    if genres_ids:
+        movie.genres.set(genres_ids)
+
+    if actors_ids:
+        movie.actors.set(actors_ids)
+
+    return movie
