96d4a1b347a4b5dd704108746bd36ceaa05ea2ae
Sergii Hrytchyn <ichbinjagerrr@gmail.com>
2025-01-23 05:39:47 +0200

    implement py-movie-moviesession-cinemahall

--- db/models.py
+++ db/models.py
@@ -22,7 +22,7 @@ class Movie(models.Model):
     actors = models.ManyToManyField(Actor, related_name="actors")
     genres = models.ManyToManyField(Genre, related_name="genres")
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.title
 
 
@@ -32,10 +32,10 @@ class CinemaHall(models.Model):
     seats_in_row = models.IntegerField()
 
     @property
-    def capacity(self):
+    def capacity(self) -> int:
         return self.rows * self.seats_in_row
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
 
@@ -52,5 +52,5 @@ class MovieSession(models.Model):
         on_delete=models.CASCADE
     )
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.movie.title} {self.show_time}"
