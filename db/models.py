3e5582961fe1a345270b073d8590db1a02df88bf
Your Name <nadeemrazani@gmail.com>
2025-01-27 17:50:02 +0800

    'Solution'

--- db/models.py
+++ db/models.py
@@ -15,14 +15,16 @@ class Actor(models.Model):
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
 
+
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    actors = models.ManyToManyField(Actor, related_name='movies')
-    genres = models.ManyToManyField(Genre, related_name='movies')
+    actors = models.ManyToManyField(Actor, related_name="movies")
+    genres = models.ManyToManyField(Genre, related_name="movies")
 
     def __str__(self) -> str:
         return self.title
+
 
 class CinemaHall(models.Model):
     name = models.CharField(max_length=255)
@@ -36,14 +38,22 @@ class CinemaHall(models.Model):
     def capacity(self) -> int:
         return self.rows * self.seats_in_row
 
+
 class MovieSession(models.Model):
     show_time = models.DateTimeField()
     cinema_hall = models.ForeignKey(
-        CinemaHall, on_delete=models.CASCADE, related_name="movie_sessions"
+        CinemaHall,
+        on_delete=models.CASCADE,
+        related_name="movie_sessions",
     )
     movie = models.ForeignKey(
-        Movie, on_delete=models.CASCADE, related_name="movie_sessions"
+        Movie,
+        on_delete=models.CASCADE,
+        related_name="movie_sessions",
     )
 
     def __str__(self) -> str:
-        return f"{self.movie.title} {self.show_time.strftime('%Y-%m-%d %H:%M:%S')}"
+        return (
+            f"{self.movie.title} "
+            f"{self.show_time.strftime("%Y-%m-%d %H:%M:%S")}"
+        )
