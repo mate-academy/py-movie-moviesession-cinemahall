af95c608a730f33802b9d7d35da1f0fc9eee0b6e
Leo <leonsiypopov@gmail.com>
2025-02-13 20:22:23 +0200

    done

--- services/movie_session.py
+++ services/movie_session.py
@@ -16,7 +16,7 @@ def create_movie_session(
     )
 
 
-def get_movies_sessions(session_date: datetime = None) -> QuerySet:
+def get_movies_sessions(session_date: str = None) -> QuerySet:
     if session_date:
         return MovieSession.objects.filter(show_time__date=session_date)
     return MovieSession.objects.all()
