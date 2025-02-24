16a748a58c05f70a97e8dd4eb230c9f0399e144b
Artem <stilon670@gmail.com>
2025-02-24 15:31:53 +0200

    Solution

---
+++ services/movie_session.py
@@ -0,0 +1,59 @@
+from xmlrpc.client import DateTime
+
+from django.db.models import QuerySet
+
+from db.models import MovieSession
+
+
+def create_movie_session(
+        movie_show_time: DateTime,
+        movie_id: int,
+        cinema_hall_id: int,
+) -> MovieSession:
+    movie_session = MovieSession.objects.create(
+        show_time=movie_show_time,
+        movie_id=movie_id,
+        cinema_hall_id=cinema_hall_id,
+    )
+
+    return movie_session
+
+
+def get_movies_sessions(session_date: DateTime = None) -> QuerySet:
+    queryset = MovieSession.objects.all()
+    if session_date:
+        queryset = MovieSession.objects.filter(show_time__date=session_date)
+
+    return queryset
+
+
+def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
+    return MovieSession.objects.get(id=movie_session_id)
+
+
+def update_movie_session(
+        session_id: int,
+        show_time: DateTime = None,
+        movie_id: int = None,
+        cinema_hall_id: int = None
+) -> MovieSession:
+    movie_session = MovieSession.objects.get(id=session_id)
+    if show_time:
+        movie_session.show_time = show_time
+
+    if movie_id:
+        movie_session.movie_id = movie_id
+
+    if cinema_hall_id:
+        movie_session.cinema_hall_id = cinema_hall_id
+
+    movie_session.save()
+
+    return movie_session
+
+
+def delete_movie_session_by_id(session_id: int) -> MovieSession:
+    movie_session = MovieSession.objects.get(id=session_id)
+    movie_session.delete()
+
+    return movie_session
