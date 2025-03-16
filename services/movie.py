22c89c8dbfb54842dbe0d8d4fedbabf26ba888fe
Ksenia Malashchuk <malashchuk.ksenia@gmail.com>
2025-03-16 20:03:47 +0200

    Solution

---
+++ services/movie.py
@@ -0,0 +1,33 @@
+from db.models import Movie
+from django.db.models import Q
+from typing import Optional, List
+
+
+def get_movies(genres_ids: Optional[List[int]] = None, actors_ids: Optional[List[int]] = None):
+    movies = Movie.objects.all()
+
+    if genres_ids:
+        movies = movies.filter(genres__id__in=genres_ids).distinct()
+
+    if actors_ids:
+        movies = movies.filter(actors__id__in=actors_ids).distinct()
+
+    return movies
+
+
+def get_movie_by_id(movie_id: int) -> Optional[Movie]:
+
+    return Movie.objects.filter(id=movie_id).first()
+
+def create_movie(
+    movie_title: str,
+    movie_description: str,
+    genres_ids: Optional[List[int]] = None,
+    actors_ids: Optional[List[int]] = None
+) -> Movie:
+    movie = Movie.objects.create(title=movie_title, description=movie_description)
+    if genres_ids:
+        movie.genres.set(genres_ids)
+    if actors_ids:
+        movie.actors.set(actors_ids)
+    return movie
