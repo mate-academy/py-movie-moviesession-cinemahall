3f76e722f99080977ad1d7b82f0af3f83c0bd17c
BronkstoneBro <atlantwindow@gmail.com>
2025-03-22 13:19:50 +0200

    Mission Completed/change in setting Kyiv-datetime at last version django

--- services/movie_session.py
+++ services/movie_session.py
@@ -5,23 +5,26 @@ from django.db.models import QuerySet
 from db.models import MovieSession
 
 
-def create_movie_session(movie_show_time: datetime,
-                         movie_id: int,
-                         cinema_hall_id: int) -> MovieSession:
+def create_movie_session(
+        movie_show_time: datetime,
+        movie_id: int,
+        cinema_hall_id: int
+) -> MovieSession:
     return MovieSession.objects.create(
         show_time=movie_show_time,
         movie_id=movie_id,
-        cinema_hall_id=cinema_hall_id,
+        cinema_hall_id=cinema_hall_id
     )
 
 
-def get_movie_session(session_date: str = None) -> QuerySet:
+def get_movies_sessions(session_date: str = None) -> QuerySet:
     queryset = MovieSession.objects.all()
 
     if session_date:
         queryset = queryset.filter(
             show_time__date=session_date,
         )
+
     return queryset
 
 
@@ -49,7 +52,5 @@ def update_movie_session(
     movie_session.save()
 
 
-def delete_movie_session(
-        session_id: int,
-) -> None:
+def delete_movie_session_by_id(session_id: int) -> None:
     MovieSession.objects.filter(id=session_id).delete()
