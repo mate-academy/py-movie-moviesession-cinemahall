7f658ed0e128b379e5e36fccf0d48a60d959b0b2
Roksolana Kalinska <woops359@gmail.com>
2025-03-30 18:14:52 +0300

    Related names edit + filter to get

--- db/models.py
+++ db/models.py
@@ -42,13 +42,13 @@ class CinemaHall(models.Model):
 
 
 class MovieSession(models.Model):
-    show_time = models.DateTimeField(datetime.now())
+    show_time = models.DateTimeField()
     cinema_hall = models.ForeignKey(CinemaHall,
                                     on_delete=models.CASCADE,
-                                    related_name="cinema_hall")
+                                    related_name="cinema_halls")
     movie = models.ForeignKey(Movie,
                               on_delete=models.CASCADE,
-                              related_name="movie")
+                              related_name="movies")
 
     def __str__(self) -> str:
         return f"{self.movie.title} {self.show_time}"
