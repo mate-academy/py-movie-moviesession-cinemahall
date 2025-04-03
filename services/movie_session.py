955325726abfc3a4f94f704097f494b2e846571b
Miwacer <il>
2025-04-03 19:48:58 +0300

    Adaptation for flake

--- services/movie_session.py
+++ services/movie_session.py
@@ -30,11 +30,11 @@ def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
 
 
 def update_movie_session(
-        session_id: int,
-        show_time: datetime = None,
-        movie_id: int = None,
-        cinema_hall_id: int = None,
-    ) -> None:
+    session_id: int,
+    show_time: datetime = None,
+    movie_id: int = None,
+    cinema_hall_id: int = None,
+) -> None:
 
     session = MovieSession.objects.get(id=session_id)
 
