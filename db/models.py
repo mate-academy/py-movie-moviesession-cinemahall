9e2fcb143cd991f73e6251f8efafad88765fb803
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-04-07 12:21:08 +0300

    models.py

--- db/models.py
+++ db/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import SET_NULL
 
 
 class Genre(models.Model):
@@ -14,3 +15,35 @@ class Actor(models.Model):
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    actors = models.ManyToManyField(Actor)
+    genres = models.ManyToManyField(Genre)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class MovieSession(models.Model):
+    show_time = models.DateTimeField()
+    cinema_hall = models.ForeignKey(CinemaHall, on_delete=SET_NULL, null=True)
+    movie = models.ForeignKey(Movie, on_delete=SET_NULL, null=True)
+
+    def __str__(self) -> str:
+        return f"{self.movie.title} {self.show_time}"
