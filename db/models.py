4aa2a1569d4be00325080b862a07e020d76470c6
Grigoriy Gesiod <g.gesiod@gmail.com>
2025-04-11 01:57:46 +0300

    First commit

--- db/models.py
+++ db/models.py
@@ -1,3 +1,7 @@
+import datetime
+
+import django.utils.timezone
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
@@ -5,7 +9,7 @@ class Genre(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
     def __str__(self) -> str:
-        return self.name
+        return f"{self.name}"
 
 
 class Actor(models.Model):
@@ -14,3 +18,33 @@ class Actor(models.Model):
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    actors = models.ManyToManyField(Actor)
+    genres = models.ManyToManyField(Genre)
+
+    def __str__(self) -> str:
+        return f"{self.title}"
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+    @property
+    def capacity(self) -> int:
+        return self.row * self.seats_in_row
+
+
+class MovieSession(models.Model):
+    show_time = models.DateTimeField(default=django.utils.timezone.now)
+    cinema_hall = models.ForeignKey(CinemaHall, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+    def __repr__(self) -> str:
+        return f"{self.movie.name} {self.show_time}"
