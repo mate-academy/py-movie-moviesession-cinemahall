bb5f40cdc297f21b35ae21bc794e0591649892ae
Dmytro Deviatlytskyi <videv1410@gmail.com>
2025-04-17 16:13:02 +0200

    default price 15

--- services/movie_session.py
+++ services/movie_session.py
@@ -9,7 +9,7 @@ def create_movie_session(
         movie_show_time: datetime,
         movie_id: int,
         cinema_hall_id: int,
-        price: float = 10
+        price: float = 15
 ) -> None:
     MovieSession.objects.create(
         show_time=movie_show_time,
