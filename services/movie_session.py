65b93191367202f6c7a6d446066718b5425136ba
Anton Bliznuk <antonbliznuk71@gmail.com>
2025-04-18 09:02:03 +0300

    Solution

--- services/movie_session.py
+++ services/movie_session.py
@@ -51,6 +51,6 @@ def update_movie_session(
 
 def delete_movie_session_by_id(
         session_id: int
-) -> MovieSession:
+) -> tuple:
     movie = MovieSession.objects.get(id=session_id)
     return movie.delete()
