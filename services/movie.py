202305f7f1f79393595016b454675fce8132508d
Roman Kostiv <rkostiv@gmail.com>
2025-04-22 16:10:41 +0300

    Solution

--- services/movie.py
+++ services/movie.py
@@ -5,29 +5,25 @@ from django.db.models import QuerySet
 def get_movies(genres_ids: list[int] = None,
                actors_ids: list[int] = None) -> QuerySet[Movie]:
 
+    queryset = Movie.objects.all()
     if genres_ids and actors_ids:
-        return Movie.objects.filter(
-            genres__id__in=genres_ids
-        ).filter(
-            actors__id__in=actors_ids
-        )
-
-    if not genres_ids and not actors_ids:
-        return Movie.objects.all()
+        return queryset.filter(genre__in=genres_ids, actor__in=actors_ids)
 
     queryset = Movie.objects.all()
 
     if genres_ids:
         queryset = queryset.filter(genres_id__in=genres_ids)
+        return queryset
 
     if actors_ids:
         queryset = queryset.filter(actors_id__in=actors_ids)
+        return queryset
 
     return queryset
 
 
 def get_movie_by_id(movie_id: int) -> Movie:
-    return Movie.objects.get(id=movie_id)
+    return Movie.objects.get(movie_id=movie_id)
 
 
 def create_movie(movie_title: str,
@@ -44,6 +40,6 @@ def create_movie(movie_title: str,
         new_movie.genres.set(genres_ids)
 
     if actors_ids:
-        new_movie.genres.set(actors_ids)
+        new_movie.actors.set(actors_ids)
 
     return new_movie
--- services/movie_session.py
+++ services/movie_session.py
@@ -18,14 +18,15 @@ def create_movie_session(movie_show_time: datetime.datetime,
 
 
 def get_movies_sessions(session_date: str = None) -> QuerySet[MovieSession]:
+    queryset = MovieSession.objects.all()
     if session_date:
         try:
             datetime.strptime(session_date, "%Y-%m-%d")
         except ValueError:
             raise ValueError("Invalid date format. Use 'YYYY-MM-DD'.")
-        return MovieSession.objects.get(show_time=session_date)
+        return queryset.filter(show_time__date=session_date)
 
-    return MovieSession.objects.all()
+    return queryset
 
 
 def update_movie_session(session_id: int,
@@ -33,8 +34,9 @@ def update_movie_session(session_id: int,
                          movie_id: int = None,
                          cinema_hall_id: int = None) -> None:
 
+    sessions = MovieSession.objects.all()
     if session_id:
-        session_to_update = MovieSession.objects.get(id=session_id)
+        session_to_update = sessions.filter(id=session_id)
         if show_time is not None:
             session_to_update.show_time = show_time
         if movie_id is not None:
@@ -46,4 +48,5 @@ def update_movie_session(session_id: int,
 
 
 def delete_movie_session(session_id: int) -> None:
-    MovieSession.objects.get(id=session_id).delete()
+    sessions = MovieSession.objects.all()
+    sessions.filter(id=session_id).delete()
