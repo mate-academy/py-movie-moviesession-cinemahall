d8a988a8429a4c080e57181e3a38ac58cc1e527c
Yaroslav <yaroslav.liubarets@icloud.com>
2025-04-25 05:44:19 +0000

    Solutiom 1.2

--- db/migrations/0002_cinemahall_movie_moviesession.py
+++ db/migrations/0002_cinemahall_movie_moviesession.py
@@ -32,9 +32,9 @@ class Migration(migrations.Migration):
             name='MovieSession',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('show_time', models.DateTimeField(auto_now_add=True)),
-                ('cinema_hall', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='db.cinemahall')),
-                ('movie', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='db.movie')),
+                ('show_time', models.DateTimeField()),
+                ('cinema_hall', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='db.CinemaHall')),
+                ('movie', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='db.Movie')),
             ],
         ),
     ]
