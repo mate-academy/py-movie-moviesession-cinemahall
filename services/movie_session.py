75f10a7c6e8a673c0bb3ad4dd1711f06af6b57e4
Viktor Stadnik <stadhik.victor@gmail.com>
2025-04-27 20:45:27 +0300

    changed "session_date" to "movie_session_id"

--- services/movie_session.py
+++ services/movie_session.py
@@ -26,8 +26,8 @@ def get_movies_sessions(movie_show_time: str = None) -> QuerySet:
     return queryset
 
 
-def get_movie_session_by_id(session_date: int) -> MovieSession:
-    return MovieSession.objects.get(id=session_date)
+def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
+    return MovieSession.objects.get(id=movie_session_id)
 
 
 def update_movie_session(
