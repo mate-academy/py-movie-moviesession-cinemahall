322c059cef036089d831a53e57604c75123ced3f
Qellexi <99039851+Qellexi@users.noreply.github.com>
2025-05-19 21:41:55 +0300

    Update movie.py

--- services/movie.py
+++ services/movie.py
@@ -5,15 +5,15 @@ from db.models import Movie
 def get_movies(
   genres_ids: list[int] = None,
   actors_ids: list[int] = None
-  ) -> QuerySet:
-    queryset = Movie.objects.all()
-    if genres_ids and actors_ids:
-      queryset = queryset.filter(genres__id__in=genres_ids, actors__id__in=actors_ids)
-    if actors_ids:
-      queryset = queryset.filter(actors__id__in=actors_ids)
-    if genres_ids:
-      queryset = queryset.filter(genres__id__in=genres_ids)
-    return queryset
+) -> QuerySet:
+  queryset = Movie.objects.all()
+  if genres_ids and actors_ids:
+    queryset = queryset.filter(genres__id__in=genres_ids, actors__id__in=actors_ids)
+  if actors_ids:
+    queryset = queryset.filter(actors__id__in=actors_ids)
+  if genres_ids:
+    queryset = queryset.filter(genres__id__in=genres_ids)
+  return queryset
 
 def get_movie_by_id(movie_id: int) -> Movie:
   return Movie.objects.get(id=movie_id)
@@ -23,10 +23,10 @@ def create_movie(
   movie_description: str,
   genres_ids: list[int] = None,
   actors_ids: list[int] = None
-  ) -> Movie:
-    new_movie = Movie.objects.create(movie_title, movie_description)
-    if genres_ids:
-      new_movie.add(genres_ids)
-    if actors_ids:
-      new_movie.add(actors_ids)
+) -> Movie:
+  new_movie = Movie.objects.create(movie_title, movie_description)
+  if genres_ids:
+    new_movie.add(genres_ids)
+  if actors_ids:
+    new_movie.add(actors_ids)
   
