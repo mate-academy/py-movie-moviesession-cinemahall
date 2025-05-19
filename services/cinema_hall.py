41abfa7d788d1624f8584cac9a5f622eaa2e3b15
Qellexi <99039851+Qellexi@users.noreply.github.com>
2025-05-19 21:42:18 +0300

    Update cinema_hall.py

--- services/cinema_hall.py
+++ services/cinema_hall.py
@@ -9,5 +9,5 @@ def create_cinema_hall(
   hall_name: str,
   hall_rows: int,
   hall_seats_in_row: int
-  ) -> CinemaHall:
-    return CinemaHall.objects.create(hall_name, hall_rows, hall_seats_in_row)
+) -> CinemaHall:
+  return CinemaHall.objects.create(hall_name, hall_rows, hall_seats_in_row)
