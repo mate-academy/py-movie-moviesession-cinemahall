e4adfa9a3a4177a9c3d5d5f94cd7ad411b5a8f50
Vadym <strauuch@gmail.com>
2025-05-20 12:48:19 +0200

    fix anotations and 0002 migration

--- services/movie.py
+++ services/movie.py
@@ -6,7 +6,7 @@ from db.models import Movie
 def get_movies(
         genres_ids: list[int] = None,
         actors_ids: list[int] = None
-) -> QuerySet[Movie] | Movie:
+) -> QuerySet[Movie]:
     if not genres_ids and not actors_ids:
         return Movie.objects.all()
     if genres_ids and not actors_ids:
