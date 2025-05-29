512abdb79b9b0f7c165943ffafbf25c86f84fac1
Vitalik <lg.mobile0808@gmail.com>
2025-05-29 10:26:20 +0300

    olution

--- services/movie.py
+++ services/movie.py
@@ -9,26 +9,26 @@ def get_movie(genres_ids: list = None,
 
     if genres_ids and actors_ids:
         movies = movies.filter(
-            genres__id__in = genres_ids,
-            actor__id__in = actors_ids
+            genres__id__in=genres_ids,
+            actor__id__in=actors_ids
         )
     elif genres_ids:
-        movies = movies.filter(genres__id__in = genres_ids)
+        movies = movies.filter(genres__id__in=genres_ids)
     elif actors_ids:
-        movies = movies.filter(actor__id__in = actors_ids)
+        movies = movies.filter(actor__id__in=actors_ids)
     return movies
 
 
 def get_movie_by_id(movie_id: int) -> QuerySet:
-    return Movie.objects.filter(id = movie_id)
+    return Movie.objects.filter(id=movie_id)
 
 
 def create_movie(movie_title: str, movie_description: str,
                  genres_id: list = None, actors_id: list = None
                  ) -> QuerySet:
-    movie = Movie.objects.create(title = movie_title,
+    movie = Movie.objects.create(title=movie_title,
                                  description=movie_description)
     if genres_id:
-        movie.genres.add(genres_id = genres_id)
+        movie.genres.add(genres_id=genres_id)
     if actors_id:
-        movie.actors.add(actors_id = actors_id)
+        movie.actors.add(actors_id=actors_id)
