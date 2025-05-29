e2d374d0024d9f5709c38f91506e17c1971a45e3
Vitalik <lg.mobile0808@gmail.com>
2025-05-29 10:18:41 +0300

    olution

--- db/models.py
+++ db/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import ManyToOneRel
 
 
 class Genre(models.Model):
@@ -14,3 +15,35 @@ class Actor(models.Model):
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    actors = models.ManyToManyField(Actor)
+    genres = models.ManyToManyField(Genre)
+
+    def __str__(self) -> str:
+        return f"self.title: {self.title}"
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.rows} rows and {self.seats_in_row} in row of {self.name}"
+
+    @property
+    def capacity(self):
+        return self.rows * self.seats_in_row
+
+
+class MovieSession(models.Model):
+    show_time = models.DateTimeField()
+    cinema_hall = models.ForeignKey(CinemaHall, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.movie} at {self.show_time}"
---
+++ services/movie.py
@@ -0,0 +1,31 @@
+from django.db.models import QuerySet
+
+from db.models import Movie
+
+
+def get_movie(genres_ids: list = None,
+              actors_ids: list = None) -> QuerySet:
+    movies = Movie.objects.all()
+
+    if genres_ids and actors_ids:
+        movies = movies.filter(
+            genres__id__in=genres_ids,
+            actor__id__in=actors_ids
+        )
+    elif genres_ids:
+        movies = movies.filter(genres__id__in=genres_ids)
+    elif actors_ids:
+        movies = movies.filter(actor__id__in=actors_ids)
+    return movies
+
+def get_movie_by_id(movie_id: int) -> QuerySet:
+    return Movie.objects.filter(id=movie_id)
+
+def create_movie(movie_title: str, movie_description: str,
+                 genres_id: list = None, actors_id: list = None
+                 ) -> QuerySet:
+    movie = Movie.objects.create(title=movie_title, description=movie_description)
+    if genres_id:
+        movie.genres.add(genres_id = genres_id)
+    if actors_id:
+        movie.actors.add(actors_id = actors_id)
