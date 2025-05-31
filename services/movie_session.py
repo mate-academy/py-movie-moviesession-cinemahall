d7e5f082b96f181f7ca499bb2378fbb7adbb059b
andrey <serdyukov963@gmail.com>
2025-06-01 01:24:54 +0300

    solution

---
+++ services/movie_session.py
@@ -0,0 +1,51 @@
+from xmlrpc.client import DateTime
+
+from db.models import MovieSession
+from django.db.models import QuerySet
+
+
+def create_movie_session(
+        movie_show_time: DateTime,
+        movie_id: int,
+        cinema_hall_id: int) -> MovieSession:
+    new_movie_session = MovieSession.objects.create(
+        movie_id=movie_id,
+        cinema_hall_id=cinema_hall_id,
+        show_time=movie_show_time,
+    )
+    return new_movie_session
+
+
+def get_movies_sessions(session_date: str = None) -> QuerySet:
+    sessions = MovieSession.objects.all()
+
+    if session_date:
+        sessions = sessions.filter(show_time__date=session_date)
+
+    return sessions
+
+
+def get_movie_session_by_id(movie_session_id: int) -> QuerySet:
+    movie_session = MovieSession.objects.get(id=movie_session_id)
+    return movie_session
+
+
+def update_movie_session(
+        session_id: int,
+        show_time: DateTime = None,
+        movie_id: int = None,
+        cinema_hall_id: int = None) -> QuerySet:
+    session = MovieSession.objects.get(id=session_id)
+    if show_time:
+        session.show_time = show_time
+    if movie_id:
+        session.movie_id = movie_id
+    if cinema_hall_id:
+        session.cinema_hall_id = cinema_hall_id
+    session.save()
+    return session
+
+
+def delete_movie_session_by_id(session_id: int) -> None:
+    movie_sessions = MovieSession.objects.filter(id=session_id)
+    movie_sessions.delete()
