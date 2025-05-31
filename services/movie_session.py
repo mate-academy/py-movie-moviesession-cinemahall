4b8621937b8729f96d0e90bdc269d4a287a7d607
andrey <serdyukov963@gmail.com>
2025-06-01 01:38:00 +0300

    solution fixed

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,11 +1,11 @@
-from xmlrpc.client import DateTime
+from datetime import datetime
 
 from db.models import MovieSession
 from django.db.models import QuerySet
 
 
 def create_movie_session(
-        movie_show_time: DateTime,
+        movie_show_time: datetime,
         movie_id: int,
         cinema_hall_id: int) -> MovieSession:
     new_movie_session = MovieSession.objects.create(
@@ -25,16 +25,16 @@ def get_movies_sessions(session_date: str = None) -> QuerySet:
     return sessions
 
 
-def get_movie_session_by_id(movie_session_id: int) -> QuerySet:
+def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
     movie_session = MovieSession.objects.get(id=movie_session_id)
     return movie_session
 
 
 def update_movie_session(
         session_id: int,
-        show_time: DateTime = None,
+        show_time: datetime = None,
         movie_id: int = None,
-        cinema_hall_id: int = None) -> QuerySet:
+        cinema_hall_id: int = None) -> MovieSession:
     session = MovieSession.objects.get(id=session_id)
     if show_time:
         session.show_time = show_time
