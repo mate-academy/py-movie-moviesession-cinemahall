0aaca3f49404daeda7eb209711ddd875e1333f94
Andrii Omelchenko <andreyomem@gmail.com>
2025-06-03 15:01:40 +0300

    implement services and models

--- services/movie.py
+++ services/movie.py
@@ -7,7 +7,8 @@ def get_movies(genres_ids: list[int] = None,
                actors_ids: list[int] = None, ) -> QuerySet:
     queryset = Movie.objects.all()
     if genres_ids and actors_ids:
-        queryset = queryset.filter(genres__id__in=genres_ids, actors__id__in=actors_ids)
+        queryset = queryset.filter(genres__id__in=genres_ids,
+                                   actors__id__in=actors_ids)
     if genres_ids:
         queryset = queryset.filter(genres__in=genres_ids)
     if actors_ids:
