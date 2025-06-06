b9ab5e955ec6c1929175433c2143599df4cfa53b
Ihor Borunov <borunov65@gmail.com>
2025-06-06 15:58:07 +0200

    olution

---
+++ services/movie.py
@@ -0,0 +1,44 @@
+from django.db.models import QuerySet
+
+from db.models import Movie, Genre, Actor
+
+
+def get_movies(
+        genres_ids: list = None,
+        actors_ids: list = None
+) -> QuerySet:
+    if genres_ids and actors_ids:
+        return Movie.objects.filter(
+            genres__id__in=genres_ids,
+            actors__id__in=actors_ids
+        ).distinct()
+    if genres_ids:
+        return Movie.objects.filter(
+            genres__id__in=genres_ids
+        ).distinct()
+    if actors_ids:
+        return Movie.objects.filter(
+            actors__id__in=actors_ids
+        ).distinct()
+    return Movie.objects.all()
+
+def get_movie_by_id(movie_id: int) -> Movie:
+    return Movie.objects.get(id=movie_id)
+
+def create_movie(
+        movie_title: str,
+        movie_description: str,
+        genres_ids:list = None,
+        actors_ids: list = None
+) -> Movie:
+    new_movie = Movie.objects.create(
+        title=movie_title,
+        description=movie_description,
+    )
+    if genres_ids:
+        genres = Genre.objects.filter(id__in=genres_ids)
+        new_movie.genres.add(*genres)
+    if actors_ids:
+        actors = Actor.objects.filter(id__in=actors_ids)
+        new_movie.actors.add(*actors)
+    return new_movie
