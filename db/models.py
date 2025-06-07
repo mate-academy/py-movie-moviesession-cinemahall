96edc709b7aab2dc842a6d816ca36f947aea45e1
MiKPo4eLiK <stassojcuk@gmail.com>
2025-06-07 14:30:44 +0300

    done

--- db/models.py
+++ db/models.py
@@ -45,4 +45,5 @@ class MovieSession(models.Model):
     cinema_hall = models.ForeignKey(CinemaHall, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
-        return f"{self.movie.title} {self.show_time}"
+        formatted_time = self.show_time.strftime("%Y-%m-%d %H:%M")
+        return f"{self.movie.title} {formatted_time}"
