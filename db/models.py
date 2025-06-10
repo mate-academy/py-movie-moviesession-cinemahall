fbc0f9a602452bb2ca92bdde54a7cee54be6538d
= <=>
2025-06-11 01:54:05 +0300

    implement py-movie

--- db/models.py
+++ db/models.py
@@ -1,16 +1,49 @@
 from django.db import models
 
 
+class Actor(models.Model):
+    full_name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.full_name
+
+
 class Genre(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
 
     def __str__(self) -> str:
         return self.name
 
 
-class Actor(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    actors = models.ManyToManyField(Actor)
+    genres = models.ManyToManyField(Genre)
 
     def __str__(self) -> str:
-        return f"{self.first_name} {self.last_name}"
+        return self.title
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.PositiveIntegerField()
+    seats_in_row = models.PositiveIntegerField()
+
+    def __str__(self) -> str:
+        return self.name
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+
+class MovieSession(models.Model):
+    show_time = models.DateTimeField()
+    cinema_hall = models.ForeignKey(
+        CinemaHall, on_delete=models.CASCADE
+    )
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.movie.title} {self.show_time}"
