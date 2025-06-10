ffd13bfc91376f67001f7ed3b8bc4ff532edfa64
= <=>
2025-06-11 02:10:02 +0300

    implement py-movie

--- services/cinema_hall.py
+++ services/cinema_hall.py
@@ -1,7 +1,8 @@
+from django.db.models import QuerySet
 from db.models import CinemaHall
 
 
-def get_cinema_halls():
+def get_cinema_halls() -> QuerySet[CinemaHall]:
     halls = CinemaHall.objects.all()
     return halls
 
