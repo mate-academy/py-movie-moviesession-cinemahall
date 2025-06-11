41ffaa41c9bcb224c76b663977dabeb4903de039
Oksana Dzeryn <oks.dzeryn@gmail.com>
2025-06-11 23:07:35 +0300

    'Solution'

--- services/cinema_hall.py
+++ services/cinema_hall.py
@@ -9,7 +9,7 @@ def get_cinema_halls() -> QuerySet:
 
 def create_cinema_hall(hall_name: str, hall_rows: int,
                        hall_seats_in_row: int) -> None:
-    new_cinema_hall = CinemaHall.objects.create(name=hall_name,
-                                                rows=hall_rows,
-                                                seats_in_row=hall_seats_in_row)
+    CinemaHall.objects.create(name=hall_name,
+                              rows=hall_rows,
+                              seats_in_row=hall_seats_in_row)
     return None
