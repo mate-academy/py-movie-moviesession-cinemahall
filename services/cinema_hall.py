dc0cd6027911fe417fc2449fd3726c1981316982
Oksana Dzeryn <oks.dzeryn@gmail.com>
2025-06-11 23:02:12 +0300

    'Solution

--- services/cinema_hall.py
+++ services/cinema_hall.py
@@ -12,5 +12,4 @@ def create_cinema_hall(hall_name: str, hall_rows: int,
     new_cinema_hall = CinemaHall.objects.create(name=hall_name,
                                                 rows=hall_rows,
                                                 seats_in_row=hall_seats_in_row)
-    new_cinema_hall.save()
     return None
--- services/movie_session.py
+++ services/movie_session.py
@@ -1,21 +1,21 @@
 from db.models import MovieSession, CinemaHall, Movie
 from django.db.models.query import QuerySet
-from datetime import datetime
+import datetime
 
 
 def create_movie_session(movie_show_time: str,
                          movie_id: int, cinema_hall_id: int) -> None:
-    cinema_hall = CinemaHall.objects.filter(cinema_hall=cinema_hall_id)
+    cinema_hall = CinemaHall.objects.get(id=cinema_hall_id)
     movie = Movie.objects.filter(id=movie_id)
     new_obj = MovieSession.objects.create(show_time=movie_show_time,
                                           cinema_hall=cinema_hall, movie=movie)
-    new_obj.save()
     return None
 
 
 def get_movies_sessions(session_date: str) -> QuerySet:
+    parsed_date = datetime.datetime.strptime(session_date, "%Y-%m-%d").date()
     if session_date:
-        return MovieSession.objects.all().filter(time=datetime.strptime)
+        return MovieSession.objects.all().filter(time=parsed_date)
     else:
         return MovieSession.objects.all()
 
@@ -26,7 +26,7 @@ def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
 
 def update_movie_session(session_id: int, show_time: str,
                          movie_id: int, cinema_hall_id: int) -> None:
-    new_obj = MovieSession.objects.all().filter(id=session_id)
+    new_obj = Movie.objects.filter(id=movie_id)
     new_obj.update(show_time=show_time, movie=movie_id,
                    cinema_hall=cinema_hall_id)
     return None
