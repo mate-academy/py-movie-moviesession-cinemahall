25841dc4d2764e8548982e34196d0aec3d1a527b
Ostap <ostap.burij@gmail.com>
2025-06-18 06:18:10 +0300

    Solution fixed flake8

--- services/movie.py
+++ services/movie.py
@@ -3,7 +3,10 @@ from django.db.models import QuerySet
 from db.models import Movie
 
 
-def get_movies(genres_ids: list[int] = None, actors_ids: list[int] = None) -> QuerySet:
+def get_movies(
+        genres_ids: list[int] = None,
+        actors_ids: list[int] = None
+) -> QuerySet:
     query = Movie.objects.all()
 
     if genres_ids:
