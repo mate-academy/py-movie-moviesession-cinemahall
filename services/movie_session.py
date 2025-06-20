81f4490946866062d0accfb78e8dbaa35337e758
Maksim Pliavinski <pliavinski.maksim@gmail.com>
2025-06-20 15:07:54 +0300

    refactor(movie_session): use get_movie_session_by_id in delete_movie_session_by_id

--- services/movie_session.py
+++ services/movie_session.py
@@ -50,4 +50,5 @@ def update_movie_session(
 
 
 def delete_movie_session_by_id(session_id: int) -> None:
-    MovieSession.objects.filter(pk=session_id).delete()
+    movie_session = get_movie_session_by_id(session_id)
+    movie_session.delete()
