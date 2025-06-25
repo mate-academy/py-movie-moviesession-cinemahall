c10db0a5d31d2e2e4ee64479d201a94f5e7c1c73
Alan <alanpires1223@gmail.com>
2025-06-25 11:26:29 -0300

    Adiciona models e serviços para Movie, CinemaHall e MovieSession

--- db/models.py
+++ db/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 
 class Actor(models.Model):
-    name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
 
     def __str__(self) -> str:
-        return self.name
+        return f"{self.first_name} {self.last_name}"
 
 
 class Genre(models.Model):
