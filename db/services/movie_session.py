964369a7b4705ecdfb480a111e6658caa5aef18d
kravetst <taraskravetc28@gmail.com>
2025-07-12 12:43:41 +0300

    implement movie, cinema hall, and movie session models and services

---
+++ db/services/movie_session.py
@@ -0,0 +1,52 @@
+from db.models import MovieSession
+from typing import Any
+from datetime import datetime
+
+
+def create_movie_session(
+        movie_show_time: datetime,
+        movie_id: int,
+        cinema_hall_id: int
+) -> Any:
+    session = MovieSession.objects.create(
+        movie_id=movie_id,
+        show_time=movie_show_time,
+        cinema_hall_id=cinema_hall_id,
+    )
+    return session
+
+
+def get_movies_sessions(session_date: str = None) -> Any:
+    queryset = MovieSession.objects.all()
+
+    if session_date is not None:
+        queryset = queryset.filter(show_time__date=session_date)
+
+    return queryset
+
+
+def get_movie_session_by_id(movie_session_id: int) -> Any:
+    return MovieSession.objects.get(id=movie_session_id)
+
+
+def update_movie_session(
+        session_id: int,
+        show_time: datetime = None,
+        movie_id: int = None,
+        cinema_hall_id: int = None
+) -> Any:
+    session = MovieSession.objects.get(id=session_id)
+
+    if show_time is not None:
+        session.show_time = show_time
+    if movie_id is not None:
+        session.movie_id = movie_id
+    if cinema_hall_id is not None:
+        session.cinema_hall_id = cinema_hall_id
+
+    session.save()
+    return session
+
+
+def delete_movie_session_by_id(session_id: int) -> Any:
+    MovieSession.objects.get(id=session_id).delete()
