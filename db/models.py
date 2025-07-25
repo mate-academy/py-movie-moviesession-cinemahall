fdb8c4347716793ca203d40bf47789df1ef258c3
valik <valentyn.rybinskyi@mail.octogames.studio>
2025-07-25 03:55:38 +0300

    Solution2

--- db/models.py
+++ db/models.py
@@ -19,8 +19,8 @@ class Actor(models.Model):
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    actors = models.ManyToManyField(Actor, related_name="movies", null=True)
-    genres = models.ManyToManyField(Genre, related_name="movies", null=True)
+    actors = models.ManyToManyField(Actor, related_name="movies")
+    genres = models.ManyToManyField(Genre, related_name="movies")
 
     def __str__(self) -> str:
         return str(self.title)
