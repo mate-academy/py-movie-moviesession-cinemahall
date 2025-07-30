fd3e4555ae54e75b03d9d0d7aadd425ef28955b4
reinvvay <reinvvay@gmail.com>
2025-07-30 18:44:18 +0300

    Solution

--- services/movie.py
+++ services/movie.py
@@ -18,7 +18,7 @@ def get_movies(
         return Movie.objects.filter(actors__in=actors_ids).distinct()
 
 
-def get_movie_by_id(movie_id: int):
+def get_movie_by_id(movie_id: int) -> Movie:
     return Movie.objects.get(id=movie_id)
 
 
