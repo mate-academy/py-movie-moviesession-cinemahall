1976a03771b15b2930e27d0c1b036a36d35a7469
Artcrafterrra <if.artcraft@gmail.com>
2025-07-30 14:04:47 +0300

    Solution

--- services/cinema_hall.py
+++ services/cinema_hall.py
@@ -7,7 +7,7 @@ def get_cinema_halls() -> QuerySet:
     return CinemaHall.objects.all()
 
 
-def create_cinema_halls(
+def create_cinema_hall(
         hall_name: str,
         hall_rows: int,
         hall_seats_in_row: int,
