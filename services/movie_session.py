26290b449cf62348fdb3f05c396fd32fc87f882e
Vladyslav Pihol <freepgvlad@gmail.com>
2025-08-01 11:09:41 +0300

    Solution4

--- services/movie_session.py
+++ services/movie_session.py
@@ -6,7 +6,7 @@ from db.models import MovieSession, Movie, CinemaHall
 
 
 def create_movie_session(
-        movie_show_time: datetime.time,
+        movie_show_time: datetime.datetime,
         movie_id: int,
         cinema_hall_id: int
 ) -> MovieSession:
@@ -36,7 +36,7 @@ def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
 
 def update_movie_session(
         session_id: int,
-        show_time: datetime.time = None,
+        show_time: datetime.datetime = None,
         movie_id: int = None,
         cinema_hall_id: int = None
 ) -> None:
