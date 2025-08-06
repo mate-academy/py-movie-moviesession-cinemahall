0e02f592f8fa9aaddf52b7fd5cb06b7ab452c1ad
Radeon <egorka22e40x@gmail.com>
2025-08-06 13:42:48 +0300

    changed database

--- db/models.py
+++ db/models.py
@@ -17,7 +17,7 @@ class Actor(models.Model):
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, unique=True)
     description = models.TextField()
     actors = models.ManyToManyField(
         Actor,
