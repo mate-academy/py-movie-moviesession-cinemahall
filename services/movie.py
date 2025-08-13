d9565fefa20fa4ece5f21b923249a3c78610f467
anton <anton@gmail.com>
2025-08-13 17:21:42 +0200

    Solutin

--- services/movie.py
+++ services/movie.py
@@ -1,23 +1,27 @@
-from typing import Optional
-from django.db.models import QuerySet
+from typing import Optional, List, TYPE_CHECKING
 from db.models import Movie
+
+if TYPE_CHECKING:
+    from django.db.models import QuerySet
 
 
 def get_movies(
-    genres_ids: Optional[list[int]] = None,
-    actors_ids: Optional[list[int]] = None
-) -> QuerySet[Movie]:
-    movies = Movie.objects.all()
+    genres_ids: Optional[List[int]] = None,
+    actors_ids: Optional[List[int]] = None,
+) -> "QuerySet[Movie]":
+    qs = Movie.objects.all()
+
     if genres_ids and actors_ids:
-        return movies.filter(
+        qs = qs.filter(
             genres__id__in=genres_ids,
-            actors__id__in=actors_ids
+            actors__id__in=actors_ids,
         ).distinct()
-    if genres_ids:
-        return movies.filter(genres__id__in=genres_ids).distinct()
-    if actors_ids:
-        return movies.filter(actors__id__in=actors_ids).distinct()
-    return movies
+    elif genres_ids:
+        qs = qs.filter(genres__id__in=genres_ids).distinct()
+    elif actors_ids:
+        qs = qs.filter(actors__id__in=actors_ids).distinct()
+
+    return qs
 
 
 def get_movie_by_id(movie_id: int) -> Movie:
@@ -27,15 +31,18 @@ def get_movie_by_id(movie_id: int) -> Movie:
 def create_movie(
     movie_title: str,
     movie_description: str,
-    genres_ids: Optional[list[int]] = None,
-    actors_ids: Optional[list[int]] = None
+    genres_ids: Optional[List[int]] = None,
+    actors_ids: Optional[List[int]] = None,
 ) -> Movie:
     movie = Movie.objects.create(
         title=movie_title,
-        description=movie_description
+        description=movie_description,
     )
+
     if genres_ids:
         movie.genres.set(genres_ids)
+
     if actors_ids:
         movie.actors.set(actors_ids)
+
     return movie
