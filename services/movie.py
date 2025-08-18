f5c98029e7615cf2c7b31f45458a468ba074f3ca
NoroiMusha37 <vo.melnyk37@gmail.com>
2025-08-19 02:52:51 +0300

    sol

--- services/movie.py
+++ services/movie.py
@@ -32,8 +32,8 @@ def create_movie(movie_title: str,
         description=movie_description,
     )
     if genres_ids:
-        movie.genres.set(genres_ids)  # type: ignore
+        movie.genres.add(*genres_ids)
     if actors_ids:
-        movie.actors.set(actors_ids)  # type: ignore
+        movie.actors.add(*actors_ids)
 
     return movie
