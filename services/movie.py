5827597cc600471aecea45e1d5814d9a07c10757
NoroiMusha37 <vo.melnyk37@gmail.com>
2025-08-19 02:34:41 +0300

    sol

--- services/movie.py
+++ services/movie.py
@@ -3,10 +3,9 @@ from django.db.models import QuerySet
 from db.models import Movie
 
 
-def get_movies(
-        genres_ids: list[int] = None,
-        actors_ids: list[int] = None
-) -> QuerySet:
+def get_movies(genres_ids: list[int] = None,
+               actors_ids: list[int] = None
+               ) -> QuerySet:
     movies = Movie.objects.all()
     if not genres_ids and not actors_ids:
         return movies
@@ -23,12 +22,11 @@ def get_movie_by_id(movie_id: int) -> Movie:
     return Movie.objects.get(id=movie_id)
 
 
-def create_movie(
-        movie_title: str,
-        movie_description: str,
-        genres_ids: list[int] = None,
-        actors_ids: list[int] = None
-) -> Movie:
+def create_movie(movie_title: str,
+                 movie_description: str,
+                 genres_ids: list[int] = None,
+                 actors_ids: list[int] = None
+                 ) -> Movie:
     movie = Movie.objects.create(
         title=movie_title,
         description=movie_description,
