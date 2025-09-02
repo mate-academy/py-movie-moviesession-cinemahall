dc403889446fc3b6f4bdcc085c2de3d606dbb146
Olha <olaviznak866@gmail.com>
2025-09-02 22:25:56 +0300

    Solution

---
+++ services/movie_session.py
@@ -0,0 +1,59 @@
+from datetime import datetime
+from typing import Optional
+from django.utils import timezone
+from django.db.models import QuerySet
+from db.models import MovieSession, Movie, CinemaHall
+
+
+def create_movie_session(
+        movie_show_time: datetime,
+        movie_id: int,
+        cinema_hall_id: int
+) -> MovieSession:
+    if timezone.is_naive(movie_show_time):
+        movie_show_time = timezone.make_aware(movie_show_time)
+    return MovieSession.objects.create(
+        show_time=movie_show_time,
+        movie=Movie.objects.get(id=movie_id),
+        cinema_hall=CinemaHall.objects.get(id=cinema_hall_id)
+    )
+
+
+def get_movies_sessions(
+        session_date: Optional[str] = None
+) -> QuerySet[MovieSession]:
+    qs = MovieSession.objects.all()
+    if session_date:
+        qs = qs.filter(
+            show_time__date=datetime.strptime(session_date, "%Y-%m-%d").date()
+        )
+    return qs
+
+
+def get_movie_session_by_id(movie_session_id: int) -> Optional[MovieSession]:
+    return MovieSession.objects.filter(id=movie_session_id).first()
+
+
+def update_movie_session(
+        session_id: int,
+        show_time: Optional[datetime] = None,
+        movie_id: Optional[int] = None,
+        cinema_hall_id: Optional[int] = None
+) -> Optional[MovieSession]:
+    session = MovieSession.objects.filter(id=session_id).first()
+    if not session:
+        return None
+    if show_time:
+        if timezone.is_naive(show_time):
+            show_time = timezone.make_aware(show_time)
+        session.show_time = show_time
+    if movie_id:
+        session.movie = Movie.objects.get(id=movie_id)
+    if cinema_hall_id:
+        session.cinema_hall = CinemaHall.objects.get(id=cinema_hall_id)
+    session.save()
+    return session
+
+
+def delete_movie_session_by_id(session_id: int) -> None:
+    MovieSession.objects.filter(id=session_id).delete()
