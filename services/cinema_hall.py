48608448c121e7433c0230bfa300df6c4ad820dc
Your Name <stas101203rakitin@gmail.com>
2025-09-04 22:37:11 +0300

    Solution

---
+++ services/cinema_hall.py
@@ -0,0 +1,15 @@
+from db.models import CinemaHall
+from django.db.models import QuerySet
+
+
+def get_cinema_halls() -> QuerySet:
+    cinema_hall = CinemaHall.objects.all()
+    return cinema_hall
+
+
+def create_cinema_hall(hall_name: str,
+                       hall_rows: int,
+                       hall_seats_in_row: int) -> None:
+    CinemaHall.objects.create(name=hall_name,
+                              rows=hall_rows,
+                              seats_in_row=hall_seats_in_row)
