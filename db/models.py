68e85a28f045610c370bd44d009b268b43b3181a
Evgeniya0208 <evgeniya.tyutyunik@gmail.com>
2025-09-08 20:18:57 +0300

    Solution

--- db/models.py
+++ db/models.py
@@ -13,7 +13,7 @@ class Actor(models.Model):
     last_name = models.CharField(max_length=255)
 
     def __str__(self) -> str:
-        return f"{self.first_name} {self.last_name}"
+        return self.first_name + " " + self.last_name
 
 
 class Movie(models.Model):
@@ -45,4 +45,4 @@ class MovieSession(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
-        return f"{self.movie.title} {self.show_time}"
+        return self.movie.title + " " + str(self.show_time)
--- services/movie.py
+++ services/movie.py
@@ -1,15 +1,17 @@
+from typing import Optional
+
 from django.db.models import QuerySet
 
 from db.models import Movie
 
 
-def get_movies(genres_ids: list[int] = None, actors_ids: list[int] = Non
+def get_movies(genres_ids: list[int] = None, actors_ids: list[int] = None
                ) -> QuerySet[Movie]:
     queryset = Movie.objects.all()
     if genres_ids:
-        queryset = Movie.objects.filter(genres__id__in=genres_ids)
+        queryset = queryset.filter(genres__id__in=genres_ids)
     if actors_ids:
-        queryset = Movie.objects.filter(actors__id__in=actors_ids)
+        queryset = queryset.filter(actors__id__in=actors_ids)
     return queryset
 
 
@@ -18,8 +20,8 @@ def get_movie_by_id(movie_id: int) -> Movie:
 
 
 def create_movie(movie_title: str, movie_description: str,
-                 genres_ids: list[int] = None,
-                 actors_ids: list[int] = None) -> Movie:
+                 genres_ids: Optional[list[int]] = None,
+                 actors_ids: Optional[list[int]] = None) -> Movie:
     new_movie = Movie.objects.create(
         title=movie_title,
         description=movie_description,
