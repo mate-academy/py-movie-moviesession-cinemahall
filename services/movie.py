129b13987258a7b027f31211b13278aeacf0cb75
Danil Tatarchuk <tatarchuk.danil06@gmail.com>
2025-09-19 17:00:43 +0300

    fix newline and character

--- services/movie.py
+++ services/movie.py
@@ -1,21 +1,29 @@
 from django.db.models import QuerySet
-
 from db.models import Movie
 
 
-def get_movies(genres_ids: list = None,
-               actors_ids: list = None) -> QuerySet:
+def get_movies(
+        movie_title: str = None,
+        movie_description: str = None,
+        genres_ids: list = None,
+        actors_ids: list = None
+) -> QuerySet:
     movies = Movie.objects.all()
 
+    if movie_title:
+        movies = movies.filter(title__icontains=movie_title)
+    if movie_description:
+        movies = movies.filter(description__icontains=movie_description)
+
     if genres_ids and actors_ids:
-        movies = movies.filter(
-            genres__id__in=genres_ids,
-            actors__id__in=actors_ids
-        )
+        movies = movies.filter(genres__id__in=genres_ids)\
+                       .filter(actors__id__in=actors_ids)\
+                       .distinct()
     elif genres_ids:
         movies = movies.filter(genres__id__in=genres_ids)
     elif actors_ids:
         movies = movies.filter(actors__id__in=actors_ids)
+
     return movies
 
 
@@ -23,12 +31,20 @@ def get_movie_by_id(movie_id: int) -> Movie:
     return Movie.objects.get(id=movie_id)
 
 
-def create_movie(movie_title: str, movie_description: str,
-                 genres_ids: list = None, actors_ids: list = None
-                 ) -> None:
-    movie = Movie.objects.create(title=movie_title,
-                                 description=movie_description)
+def create_movie(
+    movie_title: str,
+    movie_description: str,
+    genres_ids: list = None,
+    actors_ids: list = None,
+) -> Movie:
+
+    movie = Movie.objects.create(
+        title=movie_title,
+        description=movie_description
+    )
     if genres_ids:
-        movie.genres.add(*genres_ids)
+        movie.genres.set(genres_ids)  # type: ignore
     if actors_ids:
-        movie.actors.add(*actors_ids)
+        movie.actors.set(actors_ids)  # type: ignore
+
+    return movie
--- services/movie_session.py
+++ services/movie_session.py
@@ -33,7 +33,10 @@ def get_movies_sessions(
 
 def get_movie_session_by_id(movie_session_id: int
                             ) -> Optional[MovieSession]:
-    return MovieSession.objects.filter(id=movie_session_id).first()
+    try:
+        return MovieSession.objects.get(id=movie_session_id)
+    except MovieSession.DoesNotExist:
+        return None
 
 
 def update_movie_session(
@@ -42,24 +45,21 @@ def update_movie_session(
         movie_id: Optional[int] = None,
         cinema_hall_id: Optional[int] = None
 ) -> Optional[MovieSession]:
-    session = MovieSession.objects.filter(id=session_id).first()
-    if not session:
+    try:
+        session = MovieSession.objects.get(id=session_id)
+    except MovieSession.DoesNotExist:
         return None
 
-    if show_time:
+    if show_time is not None:
         session.show_time = show_time
-    if movie_id:
+    if movie_id is not None:
         session.movie_id = movie_id
-    if cinema_hall_id:
+    if cinema_hall_id is not None:
         session.cinema_hall_id = cinema_hall_id
 
     session.save()
     return session
 
 
-def delete_movie_session_by_id(session_id: int) -> bool:
-    session = MovieSession.objects.filter(id=session_id).first()
-    if not session:
-        return False
-    session.delete()
-    return True
+def delete_movie_session_by_id(session_id: int) -> None:
+    MovieSession.objects.filter(id=session_id).delete()
