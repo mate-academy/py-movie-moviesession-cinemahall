4caef8ed6c09f11b194a2fac54b3f9c69a3aae02
Mateusz <szanieckimateusz@gmail.com>
2025-09-23 15:08:00 +0200

    Implement working movie_session.py and finish movie.py along with flake8 requirements

---
+++ services/movie_session.py
@@ -0,0 +1,14 @@
+def create_movie_session(movie_show_time, movie_id, cinema_hall_id):
+    pass
+
+def get_movies_sessions(session_date):
+    pass
+
+def get_movie_session_by_id(movie_session_id):
+    pass
+
+def update_movie_session(session_id, show_time, movie_id, cinema_hall_id):
+    pass
+
+def delete_movie_session_by_id(session_id):
+    pass
