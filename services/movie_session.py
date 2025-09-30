d1e363faff4c5ae275b28fbae750e993a945080e
Samuel Yudi <toyotaa038@gmail.com>
2025-09-30 16:35:58 -0300

    aaaa

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,14 +1,16 @@
-from db.models import MovieSession, Movie, CinemaHall
+import datetime
+from db.models import MovieSession
 
 
-def create_movie_session(movie_show_time, movie_id: int, cinema_hall_id: int):
-    movie = Movie.objects.get(id=movie_id)
-    hall = CinemaHall.objects.get(id=cinema_hall_id)
-
+def create_movie_session(
+    movie_show_time: datetime.datetime,
+    movie_id: int,
+    cinema_hall_id: int
+) -> MovieSession:
     return MovieSession.objects.create(
         show_time=movie_show_time,
-        movie=movie,
-        cinema_hall=hall
+        movie_id=movie_id,
+        cinema_hall_id=cinema_hall_id,
     )
 
 
@@ -19,25 +21,29 @@ def get_movies_sessions(session_date: str = None):
     return queryset
 
 
-def get_movie_session_by_id(movie_session_id: int):
+def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
     return MovieSession.objects.get(id=movie_session_id)
 
 
-def update_movie_session(session_id: int, show_time=None,
-                         movie_id=None, cinema_hall_id=None):
+def update_movie_session(
+    session_id: int,
+    show_time: datetime.datetime = None,
+    movie_id: int = None,
+    cinema_hall_id: int = None
+) -> MovieSession:
     session = MovieSession.objects.get(id=session_id)
 
-    if show_time:
+    if show_time is not None:
         session.show_time = show_time
-    if movie_id:
+    if movie_id is not None:
         session.movie_id = movie_id
-    if cinema_hall_id:
+    if cinema_hall_id is not None:
         session.cinema_hall_id = cinema_hall_id
 
     session.save()
     return session
 
 
-def delete_movie_session_by_id(session_id: int):
+def delete_movie_session_by_id(session_id: int) -> None:
     session = MovieSession.objects.get(id=session_id)
     session.delete()
