10437f4b5da90a1987cee93ba5edbb9271bb61dc
Samuel Yudi <toyotaa038@gmail.com>
2025-09-30 16:27:11 -0300

    aaaaaa

--- db/models.py
+++ db/models.py
@@ -1,16 +1,52 @@
 from django.db import models
 
 
-class Genre(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+class Actor(models.Model):
+    first_name = models.CharField(max_length=63)
+    last_name = models.CharField(max_length=63)
 
-    def __str__(self) -> str:
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+
+    def __str__(self):
         return self.name
 
 
-class Actor(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    actors = models.ManyToManyField(Actor, related_name="movies")
+    genres = models.ManyToManyField(Genre, related_name="movies")
 
-    def __str__(self) -> str:
-        return f"{self.first_name} {self.last_name}"
+    def __str__(self):
+        return self.title
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+
+class MovieSession(models.Model):
+    show_time = models.DateTimeField()
+    cinema_hall = models.ForeignKey(
+        CinemaHall, on_delete=models.CASCADE, related_name="sessions"
+    )
+    movie = models.ForeignKey(
+        Movie, on_delete=models.CASCADE, related_name="sessions"
+    )
+
+    def __str__(self):
+        return f"{self.movie.title} {self.show_time}"
