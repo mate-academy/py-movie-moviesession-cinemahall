5fb0b6887d961400d962cc11f7d9cf3739081971
Vasyl <vasiliy.azarov.vl1@gmail.com>
2025-10-01 21:33:43 +0200

    Solution #7

--- services/movie.py
+++ services/movie.py
@@ -1,16 +1,15 @@
 from typing import Optional
-
 from django.db.models import QuerySet
 from db.models import Actor, Genre, Movie
 
 
-def get_movies(genre_ids: Optional[list[int]] = None,
-               actor_ids: Optional[list[int]] = None) -> QuerySet[Movie]:
+def get_movies(genres_ids: Optional[list[int]] = None,
+               actors_ids: Optional[list[int]] = None) -> QuerySet[Movie]:
     movies = Movie.objects.all()
-    if genre_ids:
-        movies = movies.filter(genres__id__in=genre_ids)
-    if actor_ids:
-        movies = movies.filter(actors__id__in=actor_ids)
+    if genres_ids:
+        movies = movies.filter(genres__id__in=genres_ids)
+    if actors_ids:
+        movies = movies.filter(actors__id__in=actors_ids)
     return movies.distinct()
 
 
@@ -20,13 +19,15 @@ def get_movie_by_id(movie_id: int) -> Movie:
 
 def create_movie(movie_title: str,
                  movie_description: str,
-                 genre_ids: Optional[list[int]] = None,
-                 actor_ids: Optional[list[int]] = None, ) -> Movie:
-    movie = Movie.objects.create(title=movie_title,
-                                 description=movie_description)
-    if genre_ids:
-        movie.genres.set(Genre.objects.filter(id__in=genre_ids))
-    if actor_ids:
-        movie.actors.set(Actor.objects.filter(id__in=actor_ids))
+                 genres_ids: Optional[list[int]] = None,
+                 actors_ids: Optional[list[int]] = None) -> Movie:
+    movie = Movie.objects.create(
+        title=movie_title,
+        description=movie_description)
+
+    if genres_ids:
+        movie.genres.set(Genre.objects.filter(id__in=genres_ids))
+    if actors_ids:
+        movie.actors.set(Actor.objects.filter(id__in=actors_ids))
 
     return movie
