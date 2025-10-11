4d07a27bbdee136bcafeae114386a62971925686
vasylhladyshev404 <vasyl@nextgear.nl>
2025-10-11 15:07:33 +0200

    1

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,6 +1,6 @@
 from typing import Optional
-from datetime import datetime, date
-from db.models import MovieSession, Movie, CinemaHall
+from datetime import datetime
+from db.models import MovieSession
 from django.db.models import QuerySet
 
 
@@ -9,17 +9,15 @@ def create_movie_session(
     movie_id: int,
     cinema_hall_id: int,
 ) -> MovieSession:
-    movie = Movie.objects.get(id=movie_id)
-    cinema_hall = CinemaHall.objects.get(id=cinema_hall_id)
     return MovieSession.objects.create(
         show_time=movie_show_time,
-        movie=movie,
-        cinema_hall=cinema_hall,
+        movie_id=movie_id,
+        cinema_hall_id=cinema_hall_id,
     )
 
 
 def get_movies_sessions(
-    session_date: Optional[date] = None,
+    session_date: Optional[str] = None,
 ) -> QuerySet[MovieSession]:
     if session_date:
         return MovieSession.objects.filter(show_time__date=session_date)
@@ -40,17 +38,14 @@ def update_movie_session(
 
     if show_time:
         session.show_time = show_time
-
     if movie_id:
-        session.movie = Movie.objects.get(id=movie_id)
-
+        session.movie_id = movie_id
     if cinema_hall_id:
-        session.cinema_hall = CinemaHall.objects.get(id=cinema_hall_id)
+        session.cinema_hall_id = cinema_hall_id
 
     session.save()
     return session
 
 
 def delete_movie_session_by_id(session_id: int) -> None:
-    session = MovieSession.objects.get(id=session_id)
-    session.delete()
+    MovieSession.objects.filter(id=session_id).delete()
