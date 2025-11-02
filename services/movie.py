b9b0a96dc2caea5c2c22a7eeb239e718b08ad499
acidcor <deadfoxua1337@gmail.com>
2025-11-02 17:05:38 +0100

    frelated name + set

--- services/movie.py
+++ services/movie.py
@@ -36,11 +36,9 @@ def create_movie(
     )
 
     if genres_ids:
-        genres = Genre.objects.filter(id__in=genres_ids)
-        movie.genres.set(genres)
+        movie.genres.set(genres_ids)
 
     if actors_ids:
-        actors = Actor.objects.filter(id__in=actors_ids)
-        movie.actors.set(actors)
+        movie.actors.set(actors_ids)
 
     return movie
