c9b30489e3ea963fa87399b6928f6f371303c04d
Gabriel <gfraycastilho@gmail.com>
2025-11-05 23:51:25 -0300

    solved

--- services/movie_session.py
+++ services/movie_session.py
@@ -4,16 +4,26 @@ from db.models import MovieSession
 def create_movie_session(movie_show_time, movie_id, cinema_hall_id):
     session = MovieSession.objects.create(
         show_time=movie_show_time,
-        cinema_hall=cinema_hall_id,
-        movie=movie_id,
+        cinema_hall_id=cinema_hall_id,
+        movie_id=movie_id,
     )
-
     return session
 
 
-def get_movie_session(session_id):
-    session = session_id
-    return session
+def get_movies_sessions(session_date=None):
+    if session_date is not None:
+        return MovieSession.objects.filter(show_time__date=session_date)
+    return MovieSession.objects.all()
+
+
+def get_movie_session_by_id(session_id):
+    return MovieSession.objects.get(id=session_id)
+
+
+def delete_movie_session_by_id(session_id):
+    session = MovieSession.objects.get(id=session_id)
+    session.delete()
+    return None
 
 
 def update_movie_session(session_id, show_time=None, movie_id=None, cinema_hall_id=None):
@@ -22,9 +32,10 @@ def update_movie_session(session_id, show_time=None, movie_id=None, cinema_hall_
     if show_time is not None:
         session.show_time = show_time
     if movie_id is not None:
-        session.movie = movie_id
+        session.movie_id = movie_id
     if cinema_hall_id is not None:
-        session.cinema_hall = cinema_hall_id
+        session.cinema_hall_id = cinema_hall_id
 
     session.save()
     return session
+
