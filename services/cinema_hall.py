17909e596e4cce92bd50697a6719f2d8bdaa8748
Gabriel <gfraycastilho@gmail.com>
2025-11-05 23:32:39 -0300

    solved

--- services/cinema_hall.py
+++ services/cinema_hall.py
@@ -4,6 +4,7 @@ from db.models import CinemaHall
 def get_cinema_halls():
     return CinemaHall.objects.all()
 
+
 def create_cinema_hall(hall_name, hall_rows, hall_seats_in_row):
     hall = CinemaHall.objects.create(
         name=hall_name,
