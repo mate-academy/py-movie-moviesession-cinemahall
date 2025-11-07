b099c87fbe9b68fa2acfd24b87fd01b9061551dd
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2025-11-07 14:11:18 +0100

    implementin create_movie

--- services/movie.py
+++ services/movie.py
@@ -1,6 +1,7 @@
 from django.db.models.query import QuerySet
 
-from db.models import Movie
+from db.models import Movie, Genre, Actor
+
 
 def get_movies(genres_ids: list = None, actors_ids: list = None) -> QuerySet:
     movies = Movie.objects.all()
@@ -13,3 +14,31 @@ def get_movies(genres_ids: list = None, actors_ids: list = None) -> QuerySet:
         movies = movies.filter(actors__id__in=actors_ids)
 
     return movies.distinct()
+
+
+def get_movie(movie_id: int) -> Movie:
+    movie = Movie.objects.get(id=movie_id)
+    return movie
+
+
+def create_movie(
+        movie_title: str,
+        movie_description: str,
+        genres_ids:list = None,
+        actors_ids: list = None
+) -> Movie:
+
+    movie = Movie.objects.create(
+        title=movie_title,
+        description=movie_description,
+    )
+
+    if genres_ids:
+        genres_qs = Genre.objects.filter(id__in=genres_ids)
+        movie.genres.set(genres_qs)
+
+    if actors_ids:
+        actors_qs = Actor.objects.filter(id__in=actors_ids)
+        movie.actors.set(actors_qs)
+
+    return movie
