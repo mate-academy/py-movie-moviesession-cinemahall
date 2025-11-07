fc491eba9a5fb1a83e011f69e989a19c93f92b20
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2025-11-07 11:16:59 +0100

    implementin models

--- db/models.py
+++ db/models.py
@@ -14,3 +14,34 @@ class Actor(models.Model):
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    genres = models.ManyToManyField(Genre)
+    actors = models.ManyToManyField(Actor)
+
+    def __str__(self) -> str:
+        return self.title
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.name} {self.capacity}"
+
+    @property
+    def capacity(self):
+        return self.rows * self.seats_in_row
+
+
+class MovieSession(models.Model):
+    show_time = models.DateTimeField()
+    cinema_hall = models.ForeignKey(CinemaHall, on_delete=models.DO_NOTHING)
+    movie = models.ForeignKey(Movie, on_delete=models.DO_NOTHING)
+
+    def __str__(self) -> str:
+        return f"{self.movie} {self.show_time}"
