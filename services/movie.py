669a009b7e33e5c83e0601242beb6481dfb30985
Vlad <vlad65404@gmail.com>
2025-11-14 23:47:13 +0200

    Solution

--- services/movie.py
+++ services/movie.py
@@ -30,7 +30,7 @@ def create_movie(
         description=movie_description,
     )
     if actors_ids:
-        movie.actors.add(*actors_ids)
+        movie.actors.set(actors_ids)
     if genres_ids:
-        movie.genres.add(*genres_ids)
+        movie.genres.set(genres_ids)
     return movie
--- services/movie_session.py
+++ services/movie_session.py
@@ -11,8 +11,8 @@ def create_movie_session(
 ) -> MovieSession:
     return MovieSession.objects.create(
         show_time=movie_show_time,
-        movie=Movie.objects.get(id=movie_id),
-        cinema_hall=CinemaHall.objects.get(id=cinema_hall_id),
+        movie_id=movie_id,
+        cinema_hall_id=cinema_hall_id,
     )
 
 
