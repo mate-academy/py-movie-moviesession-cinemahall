6a85c5489bc2e3353d274c34a82116bc5a0590b8
Wojciech Witkowski <witus85@gmail.com>
2025-11-18 21:02:21 +0100

    fix attribute issue

--- services/movie_session.py
+++ services/movie_session.py
@@ -12,8 +12,8 @@ def create_movie_session(
 ) -> None:
     MovieSession.objects.create(
         show_time=movie_show_time,
-        cinema_hall=cinema_hall_id,
-        movie=movie_id
+        cinema_hall_id=cinema_hall_id,
+        movie_id=movie_id
     )
 
 
@@ -41,9 +41,9 @@ def update_movie_session(
         session.show_time = show_time
 
     if movie_id:
-        session.movie = movie_id
+        setattr(session, "movie_id", movie_id)
     if cinema_hall_id:
-        session.cinema_hall = cinema_hall_id
+        setattr(session, "cinema_hall_id", cinema_hall_id)
 
     session.save()
 
