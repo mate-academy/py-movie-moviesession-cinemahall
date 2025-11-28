378abd8e8f7c867447a04cf7b322f8df5c530eb4
Ann Riabykina <anny.r20.1994@gmail.com>
2025-11-28 02:09:33 +0200

    Solution4

--- services/movie_session.py
+++ services/movie_session.py
@@ -37,9 +37,9 @@ def update_movie_session(session_id: int,
     if show_time:
         session.show_time = show_time
     if movie_id:
-        session.movie = Movie.objects.get(id=movie_id)
+        session.movie_id = movie_id
     if cinema_hall_id:
-        session.cinema_hall = CinemaHall.objects.get(id=cinema_hall_id)
+        session.cinema_hall_id = cinema_hall_id
     session.save()
     return session
 
