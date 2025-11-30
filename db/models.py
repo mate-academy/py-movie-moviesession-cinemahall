fbc354540f53c345b6e23b4027643c8ecb051c68
Artem <artemp2208@gmail.com>
2025-11-30 16:00:50 +0200

    flake

--- db/models.py
+++ db/models.py
@@ -5,14 +5,14 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
 
 
 class Genre(models.Model):
     name = models.CharField(max_length=255)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
 
@@ -22,7 +22,7 @@ class Movie(models.Model):
     actors = models.ManyToManyField("Actor", related_name="movies")
     genres = models.ManyToManyField("Genre", related_name="movies")
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.title
 
 
@@ -32,10 +32,10 @@ class CinemaHall(models.Model):
     seats_in_row = models.IntegerField()
 
     @property
-    def capacity(self):
+    def capacity(self) -> int:
         return self.rows * self.seats_in_row
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
 
@@ -52,5 +52,5 @@ class MovieSession(models.Model):
         related_name="movie_sessions"
     )
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.movie.title} {self.show_time}"
--- db/services/movie_session.py
+++ db/services/movie_session.py
@@ -56,4 +56,3 @@ def update_movie_session(
 def delete_movie_session_by_id(session_id: int) -> None:
     movie_session = MovieSession.objects.get(id=session_id)
     movie_session.delete()
-
