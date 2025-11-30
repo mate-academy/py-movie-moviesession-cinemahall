95ba12894acca657b5b2afff2a9f8a67c0cde235
Nikita Grinyov <nikita3grynov@gmail.com>
2025-12-01 00:24:20 +0200

    task

---
+++ services/movie.py
@@ -0,0 +1,20 @@
+from db.models import Movie
+from typing import Optional
+
+def get_movies(genres_ids: Optional[list[int]], actors_ids: Optional[list[int]]):
+    if not genres_ids and not actors_ids:
+        return Movie.objects.all()
+    if genres_ids and actors_ids:
+        return Movie.objects.filter(genres__id__in=genres_ids, actors__id__in=actors_ids)
+    if genres_ids and not actors_ids:
+        return Movie.objects.filter(genres__id__in=genres_ids)
+    if not genres_ids and actors_ids:
+        return Movie.objects.filter(actors__id__in=actors_ids)
+
+def get_movie_by_id(id: str):
+    return Movie.objects.get(id=id)
+
+def create_movie(movie_title: str, movie_description: str,
+                 genres_ids: Optional[list[int]], actors_ids: Optional[list[int]]):
+    return Movie.objects.create(title=movie_title, description=movie_description,
+                                genres__id__in=genres_ids, actors__id__in=actors_ids)
---
+++ services/movie_session.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+from typing import Optional
+
+from db.models import MovieSession
+
+
+def create_movie_session(movie_show_time: datetime, movie_id: int, cinema_hall_id: int):
+    return MovieSession.objects.create(movie_show_time=movie_show_time,
+                                       movie_id=movie_id, cinema_hall_id=cinema_hall_id)
+
+def get_movies_sessions(session_date: Optional[str]):
+    if session_date:
+        # конвертуємо рядок у datetime.date
+        try:
+            date_obj = datetime.strptime(session_date, "%Y-%m-%d").date()
+        except ValueError:
+            raise ValueError("session_date must be in 'YYYY-MM-DD' format")
+
+        return MovieSession.objects.filter(show_time__date=date_obj)
+
+    return MovieSession.objects.all()
+
+def get_movie_session_by_id(id: int):
+    return MovieSession.objects.get(id=id)
+
+def update_movie_session(session_id: int, show_time: Optional[datetime] = None,
+                         movie_id: Optional[int] = None, cinema_hall_id: Optional[int] = None):
+    update_fields = {}
+
+    if show_time is not None:
+        update_fields["show_time"] = show_time
+    if movie_id is not None:
+        update_fields["movie_id"] = movie_id
+    if cinema_hall_id is not None:
+        update_fields["cinema_hall_id"] = cinema_hall_id
+
+    if update_fields:
+        MovieSession.objects.filter(id=session_id).update(**update_fields)
+
+def delete_movie_session_by_id(id: int):
+    MovieSession.objects.filter(id=id).delete()
