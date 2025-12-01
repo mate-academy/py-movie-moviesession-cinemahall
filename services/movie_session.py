11a32746cdc9034b82edcb4d4925be658ca06031
TimoteoRJanzen <timoteojanzen123@gmail.com>
2025-12-01 11:51:36 -0300

    Solution

---
+++ services/movie_session.py
@@ -0,0 +1,54 @@
+from typing import Any
+
+from db.models import MovieSession, Movie, CinemaHall
+
+
+def create_movie_session(
+        movie_show_time: str,
+        movie_id: int,
+        cinema_hall_id: int
+) -> Any:
+
+    movie = Movie.objects.get(id=movie_id)
+    hall = CinemaHall.objects.get(id=cinema_hall_id)
+
+    return MovieSession.objects.create(
+        show_time=movie_show_time,
+        movie=movie,
+        cinema_hall=hall
+    )
+
+
+def get_movies_sessions(session_date: str = None) -> Any:
+    if session_date:
+        return MovieSession.objects.filter(show_time__date=session_date)
+    return MovieSession.objects.all()
+
+
+def get_movie_session_by_id(movie_session_id: int) -> Any:
+    return MovieSession.objects.get(id=movie_session_id)
+
+
+def update_movie_session(
+        session_id: int,
+        show_time: str = None,
+        movie_id: int = None,
+        cinema_hall_id: int = None
+) -> Any:
+    session = MovieSession.objects.get(id=session_id)
+
+    if show_time is not None:
+        session.show_time = show_time
+
+    if movie_id is not None:
+        session.movie = Movie.objects.get(id=movie_id)
+
+    if cinema_hall_id is not None:
+        session.cinema_hall = CinemaHall.objects.get(id=cinema_hall_id)
+
+    session.save()
+    return session
+
+
+def delete_movie_session_by_id(session_id: int) -> None:
+    MovieSession.objects.filter(id=session_id).delete()
