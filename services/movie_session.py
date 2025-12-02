6209abe599edd3e6ea59b4864378a5a81a9bab46
Oleksii Shcherbak <qksardas001@mail.com>
2025-12-02 07:58:55 +0200

    py-movie-moviesession-cinemahall fix

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date
 from django.db.models import QuerySet
 from db.models import MovieSession
 
@@ -16,13 +16,11 @@ def create_movie_session(
 
 
 def get_movies_sessions(
-    session_date: str | None = None
+    session_date: date | None = None
 ) -> QuerySet[MovieSession]:
     queryset = MovieSession.objects.all()
-
-    if session_date:
+    if session_date is not None:
         queryset = queryset.filter(show_time__date=session_date)
-
     return queryset
 
 
@@ -37,19 +35,16 @@ def update_movie_session(
     cinema_hall_id: int | None = None
 ) -> MovieSession:
     session = MovieSession.objects.get(id=session_id)
-
-    if show_time:
+    if show_time is not None:
         session.show_time = show_time
-
-    if movie_id:
+    if movie_id is not None:
         session.movie_id = movie_id
-
-    if cinema_hall_id:
+    if cinema_hall_id is not None:
         session.cinema_hall_id = cinema_hall_id
-
     session.save()
     return session
 
 
 def delete_movie_session_by_id(session_id: int) -> None:
-    MovieSession.objects.filter(id=session_id).delete()
+    session = MovieSession.objects.get(id=session_id)
+    session.delete()
