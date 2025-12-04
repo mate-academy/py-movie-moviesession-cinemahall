22141c56266630a5bdf5e64fc07035bfb4a08ed0
Anna Marchenko <marchenkoanna0464@gmail.com>
2025-12-04 22:54:21 +0100

    try

---
+++ services/movie_session.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+from db.models import MovieSession, Movie, CinemaHall
+
+
+def create_movie_session(
+    movie_show_time: datetime,
+    movie_id: int,
+    cinema_hall_id: int,
+):
+    return MovieSession.objects.create(
+        show_time=movie_show_time,
+        movie_id=movie_id,
+        cinema_hall_id=cinema_hall_id,
+    )
+
+
+def get_movie_sessions(session_date: datetime = None):
+    movie_sessions = MovieSession.objects.all()
+    if session_date:
+        movie_sessions = movie_sessions.filter(show_time__date=session_date)
+    return movie_sessions
+
+
+def get_movie_session_by_id(movie_session_id: int):
+    return MovieSession.objects.get(id=movie_session_id)
+
+
+def update_movie_session(
+    session_id: int,
+    show_time: datetime = None,
+    movie_id: int = None,
+    cinema_hall_id: int = None,
+):
+    session = MovieSession.objects.get(id=session_id)
+
+    if show_time:
+        session.show_time = show_time
+    if movie_id:
+        session.movie = Movie.objects.get(id=movie_id)
+    if cinema_hall_id:
+        session.cinema_hall = CinemaHall.objects.get(id=cinema_hall_id)
+
+    session.save()
+    return session
+
+
+def delete_movie_session_by_id(session_id: int):
+    MovieSession.objects.filter(id=session_id).delete()
